ada033ed87219a13c8a56f91189e21e50bdedca4
Skud <skud@infotrope.net>
2012-09-03 12:26:09 +0100

    added very basic tests for user model

---
+++ spec/model/user_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'user' do
+
+  before(:each) do
+    @user = User.new
+    @user.email = "example@example.com"
+    @user.password = "irrelevant"
+  end
+
+  it 'should save a basic user' do
+    @user.save.should be_true
+  end
+
+  it 'should be fetchable from the database' do
+    @user.save
+    @user2 = User.find_by_email('example@example.com')
+    @user2.email.should_not be_nil
+    @user2.encrypted_password.should_not be_nil
+  end
+
+end
