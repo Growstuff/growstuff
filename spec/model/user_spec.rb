b4ab40a2b325cb3aa9d6cf38c6aff3e561fdbe4a
Skud <skud@infotrope.net>
2012-09-03 12:27:57 +0100

    added failing tests for logging in by username

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -5,6 +5,7 @@ describe 'user' do
   before(:each) do
     @user = User.new
     @user.email = "example@example.com"
+    @user.username = "Someone"
     @user.password = "irrelevant"
   end
 
@@ -15,7 +16,8 @@ describe 'user' do
   it 'should be fetchable from the database' do
     @user.save
     @user2 = User.find_by_email('example@example.com')
-    @user2.email.should_not be_nil
+    @user2.email.should == "example@example.com"
+    @user2.username.should == "Someone"
     @user2.encrypted_password.should_not be_nil
   end
 
