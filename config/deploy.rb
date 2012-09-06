0d2630ae21129776d0445439836c3879fd285341
Skud <skud@infotrope.net>
2012-09-06 18:36:18 +0100

    tweaked bundler flags/paths/etc

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,8 @@ set :rvm_type, :user
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
+set :bundle_dir, "/home/deploy/.rvm/gems/ruby-1.9.3-p194@growstuffdev/"
+set :bundle_flags, "--deployment"
 require 'bundler/capistrano'
 
 namespace :deploy do
