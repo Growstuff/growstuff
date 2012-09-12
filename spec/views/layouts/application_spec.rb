ddc3ae1f01b736496f957c28b52c435fa2440309
Miles Gould <miles@assyrian.org.uk>
2012-09-12 19:18:37 +0100

    Don't repeat tests for all links in application spec.

--- spec/views/layouts/application_spec.rb
+++ spec/views/layouts/application_spec.rb
@@ -1,16 +1,8 @@
 require 'spec_helper'
 
 describe 'layouts/application.html.haml', :type => "view" do
-    
-    before(:each) do
-      render
-    end
-
     it 'should have links in footer' do
+        render
         rendered.should contain 'About'
-        rendered.should contain 'License'
-        rendered.should contain 'Github'
-        rendered.should contain 'Mailing list'
-        rendered.should contain 'Community Guidelines'
     end
 end
