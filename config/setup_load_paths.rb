75ca01188dce92d44a68e1de0af818e40ecaef2c
Skud <skud@infotrope.net>
2012-09-12 09:23:49 +0100

    commenting out 'unshift' line as recommended by error message from passenger'

--- config/setup_load_paths.rb
+++ config/setup_load_paths.rb
@@ -2,7 +2,7 @@ if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
   begin
     rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
     rvm_lib_path = File.join(rvm_path, 'lib')
-    $LOAD_PATH.unshift rvm_lib_path
+# $LOAD_PATH.unshift rvm_lib_path
     require 'rvm'
     RVM.use_from_path! File.dirname(File.dirname(__FILE__))
   rescue LoadError
