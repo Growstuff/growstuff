b85354f09987b771c1d316d9da84837a2ad36c6e
Miles Gould <miles@assyrian.org.uk>
2012-10-01 22:18:38 +0100

    Prettify views for crops.

--- spec/views/crops/index.html.haml_spec.rb
+++ spec/views/crops/index.html.haml_spec.rb
@@ -4,12 +4,12 @@ describe "crops/index" do
   before(:each) do
     assign(:crops, [
       stub_model(Crop,
-        :system_name => "System Name",
-        :en_wikipedia_url => "En Wikipedia Url"
+        :system_name => "Maize",
+        :en_wikipedia_url => "http://en.wikipedia.org/wiki/Maize"
       ),
       stub_model(Crop,
-        :system_name => "System Name",
-        :en_wikipedia_url => "En Wikipedia Url"
+        :system_name => "Tomato",
+        :en_wikipedia_url => "http://en.wikipedia.org/wiki/Tomato"
       )
     ])
   end
@@ -17,7 +17,7 @@ describe "crops/index" do
   it "renders a list of crops" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "System Name".to_s, :count => 2
-    assert_select "tr>td", :text => "En Wikipedia Url".to_s, :count => 2
+    assert_select "a", :text => "Maize"
+    assert_select "a", :text => "Tomato"
   end
 end
