6d7b04e7341e41e9faeda324f180c9b5cb927f73
Skud <skud@infotrope.net>
2012-11-07 13:46:01 +1100

    Only logged in users can add/edit scientific names Some small tweaks to crops tests along the way

--- spec/views/scientific_names/edit.html.haml_spec.rb
+++ spec/views/scientific_names/edit.html.haml_spec.rb
@@ -8,13 +8,29 @@ describe "scientific_names/edit" do
     ))
   end
 
-  it "renders the edit scientific_name form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => scientific_names_path(@scientific_name), :method => "post" do
-      assert_select "input#scientific_name_scientific_name", :name => "scientific_name[scientific_name]"
-      assert_select "input#scientific_name_crop_id", :name => "scientific_name[crop_id]"
+  context "logged out" do
+    it "doesn't show the SN form if logged out" do
+      render
+      rendered.should contain "Only logged in users can do this"
     end
   end
+
+  context "logged in" do
+    before(:each) do
+      @user = User.create(:email => "growstuff@example.com", :password => "irrelevant")
+      @user.confirm!
+      sign_in @user
+      render
+    end
+
+    it "renders the edit scientific_name form" do
+      render
+      # Run the generator again with the --webrat flag if you want to use webrat matchers
+      assert_select "form", :action => scientific_names_path(@scientific_name), :method => "post" do
+        assert_select "input#scientific_name_scientific_name", :name => "scientific_name[scientific_name]"
+        assert_select "input#scientific_name_crop_id", :name => "scientific_name[crop_id]"
+      end
+    end
+  end
+
 end
