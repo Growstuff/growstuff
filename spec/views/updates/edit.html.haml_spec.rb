e12bea06c3f55364fefbd7347a6d9055e99e7f52
Miles Gould <miles@assyrian.org.uk>
2012-11-08 12:53:53 +0000

    Defuglify the "Edit update" page.

--- spec/views/updates/edit.html.haml_spec.rb
+++ spec/views/updates/edit.html.haml_spec.rb
@@ -9,14 +9,29 @@ describe "updates/edit" do
     ))
   end
 
-  it "renders the edit update form" do
-    render
+  context "logged out" do
+    it "doesn't show the update form if logged out" do
+      render
+      rendered.should contain "Only logged in users can do this"
+    end
+  end
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => updates_path(@update), :method => "post" do
-      assert_select "input#update_user_id", :name => "update[user_id]"
-      assert_select "input#update_subject", :name => "update[subject]"
-      assert_select "textarea#update_body", :name => "update[body]"
+  context "logged in" do
+    before(:each) do
+      @user = User.create(:email => "growstuff@example.com",
+                          :password => "irrelevant")
+      @user.confirm!
+      sign_in @user
+      render
+    end
+
+    it "renders the edit update form" do
+      render
+
+      assert_select "form", :action => updates_path(@update), :method => "post" do
+        assert_select "input#update_subject", :name => "update[subject]"
+        assert_select "textarea#update_body", :name => "update[body]"
+      end
     end
   end
 end
