e6eb30fbbf7aea69b85ee5dc24ef65372a9b8461
Miles Gould <miles@assyrian.org.uk>
2012-11-09 10:13:16 +0000

    Fetch updates in reverse chronological order by default.

--- spec/models/update_spec.rb
+++ spec/models/update_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Update do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be sorted in reverse order" do
+    Update.create! :subject => "first entry", :body => "blah", :user_id => 1
+    Update.create! :subject => "second entry", :body => "blah", :user_id => 1
+    Update.first.subject.should == "second entry"
+  end
 end
