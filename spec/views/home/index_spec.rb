ae6ebebff9728dddee0e7ee85e4941dabe2d46c9
cephLpod <cephLpod@gmail.com>
2012-11-11 21:47:53 -0600

    An attempt to add counts to home page.
    This accesses the model directly, which is bad practice.
    What is a better way to do this?

--- spec/views/home/index_spec.rb
+++ spec/views/home/index_spec.rb
@@ -2,16 +2,37 @@ require 'spec_helper'
 
 describe 'home/index.html.haml', :type => "view" do
   before(:each) do
+    assign(:crops, [
+      stub_model(Crop,
+        :system_name => "Maize",
+        :en_wikipedia_url => "http://en.wikipedia.org/wiki/Maize"
+      ),
+      stub_model(Crop,
+        :system_name => "Tomato",
+        :en_wikipedia_url => "http://en.wikipedia.org/wiki/Tomato"
+      )
+    ])
     render
   end
+  
+
 
   it 'links to the crops page' do
     rendered.should contain 'Crops'
   end
-
+  
+  it 'counts the number of crops' do
+    rendered.should contain '(2)'
+  end
+  
   it 'links to the updates page' do
     rendered.should contain 'Recent updates'
   end
+  
+  it 'counts the number of updates' do
+     rendered.should contain '(0)'
+  end
+  
 
   it 'should have description' do
     render
