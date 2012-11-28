6c0e484741c90407d4d38fb5e57c9aeed8756869
Miles Gould <miles@assyrian.org.uk>
2012-11-28 12:22:40 +0000

    Specify UTC time zone for all events.

--- script/iteration_calendar.rb
+++ script/iteration_calendar.rb
@@ -16,7 +16,7 @@ cal = Calendar.new
 
 for i in first .. final do
   # Wednesday of week 1
-  iteration_start = Date.new(2012, 8, 8) + (i * 21)
+  iteration_start = DateTime.new(2012, 8, 8) + (i * 21)
   if i >= 4
     # We shifted to Thursday after iteration 3: see
     # http://lists.growstuff.org/pipermail/discuss/2012-October/000454.html
@@ -30,22 +30,22 @@ for i in first .. final do
     cleanup_end = cleanup_start + 3                 # 2400Z Tuesday of week 3
   end
   cal.event do
-    dtstart   iteration_start
-    dtend     doing_start
+    dtstart   iteration_start, { :TZID => "UTC" }
+    dtend     doing_start, { :TZID => "UTC" }
     summary   "Growstuff iteration #{i}: planning"
     description "Choose which stories we want to complete and who will do them"
     klass     "PUBLIC"
   end
   cal.event do
-    dtstart   doing_start
-    dtend     cleanup_start
+    dtstart   doing_start, { :TZID => "UTC" }
+    dtend     cleanup_start, { :TZID => "UTC" }
     summary   "Growstuff iteration #{i}: doing"
     description "Implement stories"
     klass     "PUBLIC"
   end
   cal.event do
-    dtstart   cleanup_start
-    dtend     cleanup_end
+    dtstart   cleanup_start, { :TZID => "UTC" }
+    dtend     cleanup_end, { :TZID => "UTC" }
     summary   "Growstuff iteration #{i}: integration and retrospective"
     description "Deploy completed stories to dev.growstuff.org and reflect on what went well and what went badly"
     klass     "PUBLIC"
