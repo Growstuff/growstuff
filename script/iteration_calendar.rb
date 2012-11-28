1dbbc62586bcf59ea3e14eea268d1fdcaf3c2405
Miles Gould <miles@assyrian.org.uk>
2012-11-28 11:57:13 +0000

    Fixes re Skud's comments.
     - Iterations after number 3 start on a Thursday
     - Each summary contains "Growstuff"

--- script/iteration_calendar.rb
+++ script/iteration_calendar.rb
@@ -15,30 +15,40 @@ final = ARGV[1].to_i
 cal = Calendar.new
 
 for i in first .. final do
-  iteration_start = Date.new(2012, 8, 8) + (i * 21) # Wednesday of week 1
-  doing_start = iteration_start + 3                 # Saturday of week 1
-  cleanup_start = doing_start + 15                  # Sunday of week 3
-  cleanup_end = cleanup_start + 3                   # Wednesday of week 3
+  # Wednesday of week 1
+  iteration_start = Date.new(2012, 8, 8) + (i * 21)
+  if i >= 4
+    # We shifted to Thursday after iteration 3: see
+    # http://lists.growstuff.org/pipermail/discuss/2012-October/000454.html
+    iteration_start += 1  
+    doing_start = iteration_start + 2               # Saturday of week 1
+    cleanup_start = doing_start + 15                # Sunday of week 3
+    cleanup_end = cleanup_start + 4                 # 2400Z Wednesday of week 3
+  else
+    doing_start = iteration_start + 3               # Saturday of week 1
+    cleanup_start = doing_start + 15                # Sunday of week 3
+    cleanup_end = cleanup_start + 3                 # 2400Z Tuesday of week 3
+  end
   cal.event do
     dtstart   iteration_start
     dtend     doing_start
-    summary   "Iteration #{i}: planning"
+    summary   "Growstuff iteration #{i}: planning"
     description "Choose which stories we want to complete and who will do them"
     klass     "PUBLIC"
   end
   cal.event do
     dtstart   doing_start
     dtend     cleanup_start
-    summary   "Iteration #{i}: doing"
+    summary   "Growstuff iteration #{i}: doing"
     description "Implement stories"
     klass     "PUBLIC"
   end
   cal.event do
     dtstart   cleanup_start
     dtend     cleanup_end
-    summary   "Iteration #{i}: integration and retrospective"
+    summary   "Growstuff iteration #{i}: integration and retrospective"
     description "Deploy completed stories to dev.growstuff.org and reflect on what went well and what went badly"
-    klass      "PUBLIC"
+    klass     "PUBLIC"
   end
 end
 
