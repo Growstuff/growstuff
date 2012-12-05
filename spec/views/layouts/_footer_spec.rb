7ed0a7578856badc0700782a15a57cd724bfadfe
Skud <skud@infotrope.net>
2012-12-05 16:43:45 +1100

    Added TOS link to footer

--- spec/views/layouts/_footer_spec.rb
+++ spec/views/layouts/_footer_spec.rb
@@ -8,10 +8,11 @@ describe 'layouts/_footer.html.haml', :type => "view" do
 
     it 'should have links in footer' do
         rendered.should contain 'About'
+        assert_select("a[href=#{'/policy/tos'}]", 'Terms of Service')
+        assert_select("a[href=#{'/policy/community'}]", 'Community Guidelines')
         rendered.should contain 'License'
         rendered.should contain 'Github'
         rendered.should contain 'Wiki'
         rendered.should contain 'Mailing list'
-        rendered.should contain 'Community Guidelines'
     end
 end
