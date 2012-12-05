3548d8f4eeaaa756012a6332a5f25e5835337d5b
Skud <skud@infotrope.net>
2012-12-05 21:54:29 +1100

    Added edit button to garden page

--- spec/views/gardens/show.html.haml_spec.rb
+++ spec/views/gardens/show.html.haml_spec.rb
@@ -2,12 +2,32 @@ require 'spec_helper'
 
 describe "gardens/show" do
   before(:each) do
+    @user = User.create(
+        :username => 'foo',
+        :email => 'foo@example.com',
+        :password => 'irrelevant',
+        :tos_agreement => true
+    )
+    @user.confirm!
     @garden = assign(:garden, stub_model(Garden,
-      :name => "Garden Name"
+      :name => "Garden Name",
+      :user_id => @user.id
     ))
   end
 
-  # garden name is shown in the header, so it's hard to test from here
-  # other garden attributes (eg. slug) aren't rendered on the page
+  context 'logged out' do
+    it 'should not show the edit button' do
+      render
+      rendered.should_not contain 'Edit'
+    end
+  end
+
+  context 'signed in' do
+    it 'should have an edit button' do
+      sign_in @user
+      render
+      rendered.should contain 'Edit'
+    end
+  end
 
 end
