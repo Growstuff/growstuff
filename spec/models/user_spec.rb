64724231e944ccdbf016fe4749403deda9f66ba0
Skud <skud@infotrope.net>
2012-12-05 16:13:08 +1100

    Fixed tests to work with TOS agreement

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,40 @@ require 'spec_helper'
 
 describe 'user' do
 
-  before(:each) do
-    @user = User.new
-    @user.email = "example@example.com"
-    @user.username = "someone"
-    @user.password = "irrelevant"
+  context 'valid user' do 
+    before(:each) do
+      @user = User.new
+      @user.email = "example@example.com"
+      @user.username = "someone"
+      @user.password = "irrelevant"
+      @user.tos_agreement = true
+    end
+
+    it 'should save a basic user' do
+      @user.save.should be_true
+    end
+
+    it 'should be fetchable from the database' do
+      @user.save
+      @user2 = User.find_by_email('example@example.com')
+      @user2.email.should == "example@example.com"
+      @user2.username.should == "someone"
+      @user2.slug.should == "someone"
+      @user2.encrypted_password.should_not be_nil
+    end
   end
 
-  it 'should save a basic user' do
-    @user.save.should be_true
-  end
+  context 'no TOS agreement' do
+    before(:each) do
+      @user = User.new
+      @user.email = "example@example.com"
+      @user.username = "someone"
+      @user.password = "irrelevant"
+    end
 
-  it 'should be fetchable from the database' do
-    @user.save
-    @user2 = User.find_by_email('example@example.com')
-    @user2.email.should == "example@example.com"
-    @user2.username.should == "someone"
-    @user2.slug.should == "someone"
-    @user2.encrypted_password.should_not be_nil
+    it "should refuse to save a user who hasn't agreed to the TOS" do
+      @user.save.should_not be_true
+    end
   end
 
 end
