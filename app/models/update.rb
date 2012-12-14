d2de7bc6f244d7130a765ede039f6dd184e28ba7
Miles Gould <miles@assyrian.org.uk>
2012-12-14 23:13:46 +0000

    Add slugs to updates.
     - add column to Update table
     - add a slug to all old updates
     - use slugs in URLs
     - make update slugs unique (we'll need them for delete URLs)

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,4 +1,6 @@
 class Update < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :subject, use: :slugged
   attr_accessible :body, :subject, :user_id
   belongs_to :user
   default_scope order("created_at desc")
--- spec/models/update_spec.rb
+++ spec/models/update_spec.rb
@@ -6,4 +6,10 @@ describe Update do
     Update.create! :subject => "second entry", :body => "blah", :user_id => 1
     Update.first.subject.should == "second entry"
   end
+
+  it "should have a slug" do
+    update = Update.create! :subject => "slugs are nasty",
+      :body => "They leave slime everywhere", :user_id => 1
+    update.slug.should == "slugs-are-nasty"
+  end
 end
