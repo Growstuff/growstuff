60db9e2e0569d527adaf71da6a8f7be92ca3237a
Skud <skud@infotrope.net>
2012-12-19 14:47:39 +1100

    only the owner can edit their plantings

--- spec/views/plantings/new.html.haml_spec.rb
+++ spec/views/plantings/new.html.haml_spec.rb
@@ -10,15 +10,32 @@ describe "plantings/new" do
     ).as_new_record)
   end
 
-  it "renders new planting form" do
-    render
+  context "logged out" do
+    it "doesn't show the planting form if logged out" do
+      render
+      rendered.should contain "Only logged in users can do this"
+    end
+  end
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => plantings_path, :method => "post" do
-      assert_select "select#planting_garden_id", :name => "planting[garden_id]"
-      assert_select "select#planting_crop_id", :name => "planting[crop_id]"
-      assert_select "input#planting_quantity", :name => "planting[quantity]"
-      assert_select "textarea#planting_description", :name => "planting[description]"
+  context "logged in" do
+    before(:each) do
+      @user = User.create(:email => "growstuff@example.com",
+                          :password => "irrelevant")
+      @user.confirm!
+      sign_in @user
+      render
+    end
+
+    it "renders new planting form" do
+      render
+
+      # Run the generator again with the --webrat flag if you want to use webrat matchers
+      assert_select "form", :action => plantings_path, :method => "post" do
+        assert_select "select#planting_garden_id", :name => "planting[garden_id]"
+        assert_select "select#planting_crop_id", :name => "planting[crop_id]"
+        assert_select "input#planting_quantity", :name => "planting[quantity]"
+        assert_select "textarea#planting_description", :name => "planting[description]"
+      end
     end
   end
 end
