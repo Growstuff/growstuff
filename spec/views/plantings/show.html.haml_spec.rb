69b7a14c79ded5bc4175bfa0d69b69583710f001
Skud <skud@infotrope.net>
2012-12-19 14:28:58 +1100

    prettified garden display, showed names instead of ids

--- spec/views/plantings/show.html.haml_spec.rb
+++ spec/views/plantings/show.html.haml_spec.rb
@@ -2,20 +2,43 @@ require 'spec_helper'
 
 describe "plantings/show" do
   before(:each) do
+    @user = assign(:user, stub_model(User,
+      :id => 23,
+      :username => 'blah',
+      :password => 'blahblah',
+      :tos_agreement => true
+    ))
+    @garden = assign(:garden, stub_model(Garden,
+      :id => 42,
+      :user => @user,
+      :name => "My Awesome Allotment"
+    ))
+    @crop = assign(:crop, stub_model(Crop,
+      :id => 99,
+      :system_name => "Tomato"
+    ))
     @planting = assign(:planting, stub_model(Planting,
-      :garden_id => 1,
-      :crop_id => 2,
-      :quantity => 3,
+      :garden => @garden,
+      :crop => @crop,
+      :quantity => 333,
       :description => "MyText"
     ))
+    render
   end
 
-  it "renders attributes in <p>" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/1/)
-    rendered.should match(/2/)
-    rendered.should match(/3/)
+  it "renders the name of the garden" do
+    rendered.should match(/#{@garden.name}/)
+  end
+
+  it "renders the name of the crop" do
+    rendered.should match(/#{@crop.system_name}/)
+  end
+
+  it "renders the quantity planted" do
+    rendered.should match(/333/)
+  end
+
+  it "renders the description" do
     rendered.should match(/MyText/)
   end
 end
