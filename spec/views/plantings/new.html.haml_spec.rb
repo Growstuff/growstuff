bf2b1d65968737539eecf4e8db60872c88b57b15
Skud <skud@infotrope.net>
2012-12-21 13:32:32 +1100

    select garden/crop based on params - BROKEN TESTSwq

--- spec/views/plantings/new.html.haml_spec.rb
+++ spec/views/plantings/new.html.haml_spec.rb
@@ -23,19 +23,59 @@ describe "plantings/new" do
                           :password => "irrelevant")
       @user.confirm!
       sign_in @user
-      render
+
+      # create gardens and crops to populate dropdowns
+      @garden1 = Garden.create!(
+        :id => 1,
+        :user_id => @user.id,
+        :name => 'Garden1'
+      )
+      @garden2 = Garden.create!(
+        :id => 2,
+        :user_id => @user.id,
+        :name => 'Garden2'
+      )
+
+      @crop1 = Crop.create!(
+        :id => 1,
+        :system_name => 'Tomato',
+        :en_wikipedia_url => 'http://blah'
+      )
+      @crop2 = Crop.create!(
+        :id => 2,
+        :system_name => 'Corn',
+        :en_wikipedia_url => 'http://blah'
+      )
+      @crop3 = Crop.create!(
+        :id => 3,
+        :system_name => 'Chard',
+        :en_wikipedia_url => 'http://blah'
+      )
+
+      # params to test default choice of crop and garden
+      # XXX this is very wrong! but we are still trying to figure out
+      # how to do it the right way. commiting as is, for now.
+      render :template => 'plantings/new',
+        :params => { :crop_id => 2, :garden_id => 2 }
     end
 
     it "renders new planting form" do
-      render
-
-      # Run the generator again with the --webrat flag if you want to use webrat matchers
       assert_select "form", :action => plantings_path, :method => "post" do
         assert_select "select#planting_garden_id", :name => "planting[garden_id]"
         assert_select "select#planting_crop_id", :name => "planting[crop_id]"
         assert_select "input#planting_quantity", :name => "planting[quantity]"
         assert_select "textarea#planting_description", :name => "planting[description]"
       end
+    end
+
+    it "selects a crop given in a param" do
+      assert_select "select#planting_crop_id",
+        :html => /option value="2" selected="selected"/
+    end
+
+    it "selects a garden given in a param" do
+      assert_select "select#planting_garden_id",
+        :html => /option value="2" selected="selected"/
     end
   end
 end
