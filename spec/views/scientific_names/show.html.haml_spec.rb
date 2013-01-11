74a11c5554c74c88b7c095bf0d14f48a140c0d78
gnattery <gnattitude@gmail.com>
2013-01-11 13:15:39 +1100

    Factory-girl-ified the scientific name tests

--- spec/views/scientific_names/show.html.haml_spec.rb
+++ spec/views/scientific_names/show.html.haml_spec.rb
@@ -2,16 +2,15 @@ require 'spec_helper'
 
 describe "scientific_names/show" do
   before(:each) do
-    @scientific_name = assign(:scientific_name, stub_model(ScientificName,
-      :scientific_name => "Scientific Name",
-      :crop_id => 1
-    ))
+    @scientific_name = assign(:scientific_name,
+      FactoryGirl.create(:zea_mays)
+    )
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Scientific Name/)
-    rendered.should match(/1/)
+    rendered.should match(/Zea mays/)
+    rendered.should match(@scientific_name.id.to_s)
   end
 end
