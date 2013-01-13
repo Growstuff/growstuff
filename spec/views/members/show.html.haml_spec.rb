7aac659749681379286bd7a848a0a4115053ab8c
Skud <skud@infotrope.net>
2013-01-13 11:38:54 +1100

    FGified and generally improved members/show tests

--- spec/views/members/show.html.haml_spec.rb
+++ spec/views/members/show.html.haml_spec.rb
@@ -1,15 +1,12 @@
 require 'spec_helper'
 
 describe "members/show" do
+
   before(:each) do
-    @member = User.create!(
-      :username => "pie",
-      :password => "steak&kidney",
-      :email => "steak-and-kidney@pie.com",
-      :tos_agreement => true
-    )
+    @member = FactoryGirl.create(:user)
     @time = @member.created_at
-    @member.gardens.create(:name => 'My Garden', :user_id => @member.id)
+
+    # need @garden to render the page
     @garden = Garden.new
     render
   end
@@ -21,6 +18,35 @@ describe "members/show" do
 
   it "contains a gravatar icon" do
     assert_select "img", :src => /gravatar\.com\/avatar/
+  end
+
+  context "no gardens" do
+    it "shouldn't mention the name of a garden" do
+      rendered.should_not contain "My Garden"
+    end
+  end
+
+  context "signed in user" do
+    before(:each) do
+      @member.confirm!
+      sign_in @member
+      render
+    end
+
+    it "contains a 'New Garden' link" do
+      assert_select "a[href=#garden_new]", :text => "New Garden"
+    end
+  end
+
+  context "user has a garden" do
+    before(:each) do
+      @member.gardens.create(:name => 'My Garden', :user_id => @member.id)
+      render
+    end
+
+    it "displays a garden, if the user has one" do
+      rendered.should contain "My Garden"
+    end
   end
 
 end
