ecd9cc19fccc7958caf2ad908027d63d3166492c
Skud <skud@infotrope.net>
2013-01-13 12:52:57 +1100

    FGified models/planting tests

--- spec/models/planting_spec.rb
+++ spec/models/planting_spec.rb
@@ -3,38 +3,20 @@ require 'spec_helper'
 describe Planting do
 
   before(:each) do
-    @crop = Crop.create(
-      :system_name => "Tomato",
-      :en_wikipedia_url => "http://en.wikipedia.org/wiki/Tomato"
-    )
-
-    @user = User.create!(
-      :username => 'foo',
-      :password => 'irrelevant',
-      :email => 'foo@example.com',
-      :tos_agreement => true
-    )
-    @user.confirm!
-
-    @garden = Garden.create(
-      :user_id => @user.id,
-      :name => 'bar'
-    )
-
-    @planting = Planting.create(
-      :garden_id => @garden.id,
-      :crop_id => @crop.id
-    )
-
+    @crop     = FactoryGirl.create(:tomato)
+    @user     = FactoryGirl.create(:confirmed_user)
+    @garden   = FactoryGirl.create(:garden, :user => @user)
+    @planting = FactoryGirl.create(:planting,
+        :crop => @crop, :garden => @garden)
   end
 
   it "generates an owner" do
     @planting.owner.should be_an_instance_of User
-    @planting.owner.username.should match /^foo$/
+    @planting.owner.username.should match /^user1$/
   end
 
   it "generates a location" do
-    @planting.location.should match /^foo's bar$/
+    @planting.location.should match /^user1's My Garden$/
   end
 
 end
