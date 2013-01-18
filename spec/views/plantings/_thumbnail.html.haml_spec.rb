46dd7ed34669c57a2313da2137145b3dadc34dd1
Skud <skud@infotrope.net>
2013-01-18 14:04:45 +1100

    Display thumbnail inside a .well, optionally hide description

--- spec/views/plantings/_thumbnail.html.haml_spec.rb
+++ spec/views/plantings/_thumbnail.html.haml_spec.rb
@@ -10,24 +10,43 @@ describe "plantings/_thumbnail" do
       :garden => @garden,
       :crop => @crop
     )
-
-    render :partial => "thumbnail", :locals => {
-      :planting => @planting,
-      :show_crop => true,
-      :show_location => true
-    }
   end
 
-  it "renders the quantity planted" do
-    rendered.should contain "33"
+  context "simple view" do
+    before(:each) do
+      render :partial => "thumbnail", :locals => {
+        :planting => @planting,
+      }
+    end
+
+    it "renders the quantity planted" do
+      rendered.should contain "33"
+    end
+
+    it "renders the date planted" do
+      rendered.should contain @planting.planted_at.to_s(:date)
+    end
+
+    it "shows the name of the crop" do
+      rendered.should contain "Tomato"
+    end
+
+    it "shows the description by default" do
+      rendered.should contain "This is a"
+    end
   end
 
-  it "renders the date planted" do
-    rendered.should contain @planting.planted_at.to_s(:date)
-  end
+  context "with complicated args" do
+    before(:each) do
+      render :partial => "thumbnail", :locals => {
+        :planting => @planting,
+        :hide_description => true
+      }
+    end
 
-  it "shows the name of the crop" do
-    rendered.should contain "Tomato"
+    it "hides the description if asked" do
+      rendered.should_not contain "This is a"
+    end
   end
 
 end
