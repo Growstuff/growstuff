ea8f30bd8dd9fd079f2d03c7394cbefaf8ff3032
Skud <skud@infotrope.net>
2013-02-01 10:25:54 +1100

    tweaked cancan defaults (we don't have current_user)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # tweak CanCan defaults because we don't have a "current_user" method
+  def current_ability
+    @current_ability ||= AccountAbility.new(current_member)
+  end
+
 end
