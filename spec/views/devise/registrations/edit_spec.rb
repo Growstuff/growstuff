4f64946de22c7be1dc4b2c5f79dfba228f7ac8ad
Skud <skud@infotrope.net>
2013-02-06 16:10:43 +1100

    Wrote some very basic tests for the improved settings page

--- spec/views/devise/registrations/edit_spec.rb
+++ spec/views/devise/registrations/edit_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
-describe 'devise/registrations/edit.html.erb', :type => "view" do
+describe 'devise/registrations/edit.html.haml', :type => "view" do
 
   context "logged in" do
     before(:each) do
-      @view.stub(:resource).and_return(Member.new)
+      controller.stub(:current_user) { nil }
+      @member = FactoryGirl.create(:member)
+      controller.stub(:current_member) { @member }
+      @view.stub(:resource).and_return(@member)
       @view.stub(:resource_name).and_return("member")
       @view.stub(:resource_class).and_return(Member)
       @view.stub(:devise_mapping).and_return(Devise.mappings[:member])
@@ -13,7 +16,15 @@ describe 'devise/registrations/edit.html.erb', :type => "view" do
 
     it 'should have some fields' do
         rendered.should contain 'Email'
-        rendered.should contain 'Cancel my account'
     end
+
+    it 'should have an email section' do
+      assert_select "h2", "Email address"
+    end
+
+    it 'should have a password section' do
+      assert_select "h2", "Change password"
+    end
+
   end
 end
