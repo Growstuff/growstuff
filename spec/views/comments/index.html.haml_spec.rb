979ad334f33a5b861c184511f5bce15b1bc73579
Skud <skud@infotrope.net>
2013-02-08 17:31:17 +1100

    made the index page not be ugly

--- spec/views/comments/index.html.haml_spec.rb
+++ spec/views/comments/index.html.haml_spec.rb
@@ -2,25 +2,16 @@ require 'spec_helper'
 
 describe "comments/index" do
   before(:each) do
+    controller.stub(:current_user) { nil }
     assign(:comments, [
-      stub_model(Comment,
-        :post_id => 1,
-        :author_id => 2,
-        :body => "MyText"
-      ),
-      stub_model(Comment,
-        :post_id => 1,
-        :author_id => 2,
-        :body => "MyText"
-      )
+      FactoryGirl.create(:comment),
+      FactoryGirl.create(:comment, :body => 'ROFL')
     ])
   end
 
   it "renders a list of comments" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    rendered.should contain 'OMG LOL'
+    rendered.should contain 'ROFL'
   end
 end
