e41f70db714e51f0191a7400befb1c4928438dd6
Skud <skud@infotrope.net>
2013-02-15 23:48:30 +1100

    a bit of test cleanup

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe "Notifications" do
   describe "GET /notifications" do
     it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get notifications_path
-      response.status.should be(200)
+      # can't see notifications because not logged in
+      # therefore redirect to homepage
+      response.status.should be(302)
     end
   end
 end
