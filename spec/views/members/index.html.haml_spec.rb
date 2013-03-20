793b25e3dddd67db66548a56b91522f517b5d524
gnattery <gnattitude@gmail.com>
2013-03-20 15:17:10 +1100

    Paginate and sort members.
    We've decided to remove tests for pagination display because we trust will_paginate

--- spec/views/members/index.html.haml_spec.rb
+++ spec/views/members/index.html.haml_spec.rb
@@ -2,15 +2,18 @@ require 'spec_helper'
 
 describe "members/index" do
   before(:each) do
-    assign(:members, [
-      FactoryGirl.create(:member),
-      FactoryGirl.create(:member)
-    ])
+    controller.stub(:current_user) { nil }
+    page = 1
+    per_page = 2
+    total_entries = 2
+    members = WillPaginate::Collection.create(page, per_page, total_entries) do |pager|
+      pager.replace([
+        FactoryGirl.create(:member),
+        FactoryGirl.create(:member)
+      ])
+    end
+    assign(:members, members)
     render
-  end
-
-  it "counts the number of members" do
-    rendered.should contain "Displaying 2 members"
   end
 
   it "contains two gravatar icons" do
