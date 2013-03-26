dcd7df7f4792375aec58bc587094dfd5f96ec2f4
Skud <skud@infotrope.net>
2013-03-26 16:17:04 +1100

    Made the password reset email friendlier.
    Also did the same for the unlock instructions, while I was in there.

--- spec/views/devise/mailer/unlock_instructions_spec.rb
+++ spec/views/devise/mailer/unlock_instructions_spec.rb
@@ -1,14 +1,16 @@
+require 'spec_helper'
 describe 'devise/mailer/unlock_instructions.html.haml', :type => "view" do
   context "logged in" do
     before(:each) do
-      @resource = mock_model(Member)
+#@resource = mock_model(Member)
+      @resource = FactoryGirl.create(:member)
       @resource.should_receive(:email).and_return("example@example.com")
       @resource.should_receive(:unlock_token).and_return("fred")
       render
     end
 
     it "should explain what's happened" do
-      rendered.should contain "Your account has been locked"
+      rendered.should contain "account has been locked"
     end
 
     it "should have an unlock link" do
