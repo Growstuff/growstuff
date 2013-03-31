a5f6931e1214ded60f6b7d1f877bfb9b3033b52a
Skud <skud@infotrope.net>
2013-03-31 21:00:57 +1100

    rewrote blurb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   skip_authorize_resource
+
   def index
+    @member_count   = Member.confirmed.count
+    @crop_count     = Crop.count
+    @planting_count = Planting.count
+    @garden_count   = Garden.count
+
+    respond_to do |format|
+      format.html # index.html.haml
+    end
   end
+
 end
--- spec/views/home/index_spec.rb
+++ spec/views/home/index_spec.rb
@@ -8,7 +8,6 @@ describe 'home/index.html.haml', :type => "view" do
 
     it 'should have description' do
       rendered.should contain 'is a community of food gardeners'
-      rendered.should contain 'We welcome you regardless of your experience, and invite you to be part of our development process.'
     end
   end
 
