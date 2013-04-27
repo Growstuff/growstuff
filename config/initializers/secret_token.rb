5b2dbc571e05ea762f34a4121f57ae48898592b4
Skud <skud@infotrope.net>
2013-04-27 10:23:32 +1000

    security fix: check ENV for rails secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Growstuff::Application.config.secret_token = '6b61c7a7c5f5684945c445d80ea3df725ee2bb3101a3a57a4bec349657e092a562aa714cb4706c038d7fb1bdbd497c4b103aa1247f3bdf79044e03363ea8ee50'
+Growstuff::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN'] || "this is not a real secret token but it's here to make life easier for developers"
