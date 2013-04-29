11f979313a7e3d2b30d55012a7fa133b0c70576a
Skud <skud@infotrope.net>
2013-04-29 18:55:13 +1000

    Added reply link to notification email.
    Note: this breaks DRY but we couldn't figure out how to put it in a
    helper and make it useable by both the controller and the mailer.
    Suggestions/help much appreciated!

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,6 +3,20 @@ class Notifier < ActionMailer::Base
 
   def notify(notification)
     @notification = notification
+
+    # by default, reply link sends a PM in return
+    @reply_link = new_notification_url(
+      :recipient_id => @notification.sender.id,
+      :subject => @notification.subject =~ /^Re: / ?
+        @notification.subject :
+        "Re: " + @notification.subject
+    )
+
+    if @notification.post
+      # comment on the post in question
+      @reply_link = new_comment_url(:post_id => @notification.post.id)
+    end
+
     mail(:to => @notification.recipient.email,
          :subject => @notification.subject)
   end
