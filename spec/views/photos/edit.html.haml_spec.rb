2fb8c380199f76b65af756255e5d0a114b0e7a9a
Miles Gould <miles@assyrian.org.uk>
2013-05-08 15:39:17 +0100

    Removed obsolete test from views/photos/edit
    We should consider just deleting this action entirely.

--- spec/views/photos/edit.html.haml_spec.rb
+++ spec/views/photos/edit.html.haml_spec.rb
@@ -10,15 +10,4 @@ describe "photos/edit" do
     ))
   end
 
-  it "renders the edit photo form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => photos_path(@photo), :method => "post" do
-      assert_select "input#photo_owner_id", :name => "photo[owner_id]"
-      assert_select "input#photo_flickr_photo_id", :name => "photo[flickr_photo_id]"
-      assert_select "input#photo_thumbnail_url", :name => "photo[thumbnail_url]"
-      assert_select "input#photo_fullsize_url", :name => "photo[fullsize_url]"
-    end
-  end
 end
