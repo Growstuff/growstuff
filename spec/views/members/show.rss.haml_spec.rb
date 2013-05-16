2d3b594ae160f80786d54de1af7c0e2348c8a444
Miles Gould <miles@assyrian.org.uk>
2013-05-16 18:24:22 +0100

    Test for expanded-markdown rendering.

--- spec/views/members/show.rss.haml_spec.rb
+++ spec/views/members/show.rss.haml_spec.rb
@@ -5,7 +5,7 @@ describe 'members/show.rss.haml', :type => "view" do
     @member = assign(:member, FactoryGirl.create(:member))
     assign(:posts, [
       FactoryGirl.build(:post, :id => 1, :author => @member),
-      FactoryGirl.build(:post, :id => 2, :author => @member)
+      FactoryGirl.build(:markdown_post, :id => 2, :author => @member)
     ])
     render
   end
@@ -16,6 +16,12 @@ describe 'members/show.rss.haml', :type => "view" do
 
   it 'shows content of posts' do
     rendered.should contain "This is some text."
+  end
+
+  it 'renders post bodies to HTML and XML-escapes them' do
+# The variable "rendered" has been entity-replaced and tag-stripped
+# The literal string output contains "&lt;strong&gt;" etc.
+    rendered.should contain "<strong>strong</strong>"
   end
 
 end
