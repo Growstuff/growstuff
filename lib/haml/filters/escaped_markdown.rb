d5c781a97ff6636556c0b24b42c28284eea02b1d
Miles Gould <miles@assyrian.org.uk>
2013-05-16 18:00:00 +0100

    Tests for Haml::Filters::EscapedMarkdown.

--- lib/haml/filters/escaped_markdown.rb
+++ lib/haml/filters/escaped_markdown.rb
@@ -1,10 +1,17 @@
-gem 'bluecloth'
+require 'bluecloth'
 
-module Haml::Filters::Escaped_Markdown
-  include Haml::Filters::Base
+module Haml::Filters
+  module EscapedMarkdown
+    include Haml::Filters::Base
 
-  def render(text)
-    bc = BlueCloth.new(text)
-    return Haml::Helpers.html_escape bc.to_html
+    def render(text)
+      bc = BlueCloth.new(text)
+      return Haml::Helpers.html_escape bc.to_html
+    end
   end
+
+# Register it as the handler for the :escaped_markdown HAML command.
+# The automatic system gives us :escapedmarkdown, which is ugly.
+defined['escaped_markdown'] = EscapedMarkdown
+
 end
