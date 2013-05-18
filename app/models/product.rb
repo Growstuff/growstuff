75e29132030f2e60b50d76900a7f75c1ee61de97
Skud <skud@infotrope.net>
2013-05-18 11:16:45 +1000

    added method to update paid acct details after product purchase

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,4 +11,16 @@ class Product < ActiveRecord::Base
   def to_s
     name
   end
+
+  # when purchasing a product that gives you a paid account, this method
+  # does all the messing around to actually make sure the account is
+  # updated correctly -- account type, paid until, etc.
+  def update_account(member)
+    member.account.account_type = account_type
+    if paid_months
+      start_date = member.account.paid_until || Time.zone.now
+      member.account.paid_until = start_date + paid_months.months
+    end
+  end
+
 end
