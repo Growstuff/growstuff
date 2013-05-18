865a1c705017e44886b57037da1f90c99f468aa9
Skud <skud@infotrope.net>
2013-05-18 10:29:18 +1000

    validate paid_months (must be integer)

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,9 @@ class Product < ActiveRecord::Base
   has_and_belongs_to_many :orders
   belongs_to :account_type
 
+  validates :paid_months, :numericality => { :only_integer => true,
+    :allow_nil => true }
+
   def to_s
     name
   end
