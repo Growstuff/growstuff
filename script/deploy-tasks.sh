660d70645e58bb3411e9a4d36364e6b2f4d71360
Skud <skud@infotrope.net>
2013-06-04 20:21:59 +1000

    Added oneoff task to set up shop/products/etc

--- script/deploy-tasks.sh
+++ script/deploy-tasks.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # tasks to run at deploy time, usually after 'rake db:migrate'
 
 # When adding tasks, do so in chronological order, and note the date
@@ -10,10 +12,32 @@
 # echo "2013-06-03 - replace empty garden names with 'Garden'"
 # rake growstuff:oneoff:empty_garden_names
 
+# you MUST set up the paypal API environment variables before doing
+# anything else, or things will fail all over the place. Stupid paypal :(
+
 echo "2013-06-04 - set up Paypal API"
-echo "You must run:"
-echo "  heroku config:set PAYPAL_USERNAME=..."
-echo "  heroku config:set PAYPAL_PASSWORD=..."
-echo "  heroku config:set PAYPAL_SIGNATURE=..."
+if [ ! $PAYPAL_USERNAME ]
+then
+  echo "You must run:"
+  echo "  heroku config:set PAYPAL_USERNAME=..."
+  exit
+fi
+
+if [ ! $PAYPAL_PASSWORD ]
+then
+  echo "You must run:"
+  echo "  heroku config:set PAYPAL_PASSWORD=..."
+  exit
+fi
+
+if [ ! $PAYPAL_SIGNATURE ]
+then
+  echo "You must run:"
+  echo "  heroku config:set PAYPAL_SIGNATURE=..."
+  exit
+fi
+
+echo "2013-06-04 - set up shop/products/etc"
+rake growstuff:oneoff:setup_shop
 
 
