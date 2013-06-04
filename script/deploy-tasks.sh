4919bc967046566a012e5636528dd366ba96ac0e
Skud <skud@infotrope.net>
2013-06-04 19:44:23 +1000

    added reminder to deploy tasks to set paypal details

--- script/deploy-tasks.sh
+++ script/deploy-tasks.sh
@@ -4,11 +4,16 @@
 # when it was added.  This will help us know which ones have been run
 # and can safely be commented out or removed.
 
-echo "2013-05-?? - Replacing empty post/notification subjects with '(no subject)'"
-rake growstuff:oneoff:empty_subjects
+# echo "2013-05-?? - Replacing empty post/notification subjects with '(no subject)'"
+# rake growstuff:oneoff:empty_subjects
 
-echo "2013-06-03 - replace empty garden names with 'Garden'"
-rake growstuff:oneoff:empty_garden_names
+# echo "2013-06-03 - replace empty garden names with 'Garden'"
+# rake growstuff:oneoff:empty_garden_names
 
+echo "2013-06-04 - set up Paypal API"
+echo "You must run:"
+echo "  heroku config:set PAYPAL_USERNAME=..."
+echo "  heroku config:set PAYPAL_PASSWORD=..."
+echo "  heroku config:set PAYPAL_SIGNATURE=..."
 
 
