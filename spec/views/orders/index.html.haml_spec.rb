6cf64694f469b8aa7c874f91fe8fa33b7fa42c66
Skud <skud@infotrope.net>
2013-06-06 21:07:54 +1000

    added account status info in various places

--- spec/views/orders/index.html.haml_spec.rb
+++ spec/views/orders/index.html.haml_spec.rb
@@ -4,9 +4,14 @@ describe "orders/index" do
   before(:each) do
     @member = FactoryGirl.create(:member)
     sign_in @member
-    @order1 = FactoryGirl.create(:order)
-    @order2 = FactoryGirl.create(:completed_order)
+    @order1 = FactoryGirl.create(:order, :member => @member)
+    @order2 = FactoryGirl.create(:completed_order, :member => @member)
     assign(:orders, [ @order1, @order2 ])
+  end
+
+  it "shows your current account status" do
+    render
+    rendered.should contain "Your current account status"
   end
 
   it "renders a list of orders" do
