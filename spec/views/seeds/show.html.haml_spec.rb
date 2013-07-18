610b100f7fb9580ba1ec226da3bdf75e20d7881c
Skud <skud@infotrope.net>
2013-07-18 23:54:51 +1000

    seed page: cleaned up views/tests for location

--- spec/views/seeds/show.html.haml_spec.rb
+++ spec/views/seeds/show.html.haml_spec.rb
@@ -15,25 +15,48 @@ describe "seeds/show" do
   context "tradable" do
     before(:each) do
       @owner = FactoryGirl.create(:london_member)
-      assign(:seed, FactoryGirl.create(:tradable_seed, 
+      assign(:seed, FactoryGirl.create(:tradable_seed,
         :owner => @owner))
       # note current_member is not the owner of this seed
       @member = FactoryGirl.create(:member)
       sign_in @member
       controller.stub(:current_user) { @member }
-      render
     end
 
     it "shows tradable attributes" do
+      render
       rendered.should contain "Tradable: Yes"
       rendered.should contain "Will trade to: locally"
     end
 
-    it "shows location of seed owner" do 
+    it "shows location of seed owner" do
+      render
       rendered.should contain @owner.location
     end
 
+    context 'with no location' do
+      before(:each) do
+        @owner = FactoryGirl.create(:member) # no location
+        sign_in @owner
+        controller.stub(:current_user) { @owner }
+        assign(:seed, FactoryGirl.create(:tradable_seed, :owner => @owner))
+      end
+
+      it 'says "from unspecified location"' do
+        render
+        rendered.should contain "(from unspecified location)"
+      end
+
+      it "links to profile to set location" do
+        render
+        assert_select "a[href=#{url_for(edit_member_registration_path)}]", :text => "Set Location"
+
+      end
+
+    end
+
     it "shows button to send message" do
+      render
       rendered.should contain "Request seeds"
     end
   end
