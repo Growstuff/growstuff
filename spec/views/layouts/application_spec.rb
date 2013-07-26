def6426e49d2f232d3ef620f275d7d06368e6abf
Skud <skud@infotrope.net>
2013-07-26 14:35:42 +1000

    Fixed broken test for clicky analytics

--- spec/views/layouts/application_spec.rb
+++ spec/views/layouts/application_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe 'layouts/application.html.haml', :type => "view" do
   before(:each) do
     controller.stub(:current_user) { nil }
-    render
   end
 
   it 'includes the analytics code' do
     Growstuff::Application.config.analytics_code = '<script>alert("foo!")</script>'
+    render
     assert_select "script", :text => 'alert("foo!")'
     rendered.should_not contain 'script'
   end
