f6fea51ac304508085feee7eb5e85a3943cf5953
Skud <skud@infotrope.net>
2013-08-14 12:08:27 +1000

    cache planting snippets on homepage

--- app/models/planting.rb
+++ app/models/planting.rb
@@ -67,6 +67,10 @@ class Planting < ActiveRecord::Base
     return photos.first
   end
 
+  def interesting?
+    return photos.present?
+  end
+
   # return a list of interesting plantings, for the homepage etc.
   # we can't do this via a scope (as far as we know) so sadly we have to
   # do it this way.
@@ -77,7 +81,7 @@ class Planting < ActiveRecord::Base
 
     Planting.all.each do |p|
       break if interesting_plantings.count == howmany # got enough yet?
-      next unless p.photos.present? # skip those that don't have photos
+      next unless p.interesting?    # skip those that don't have photos
       next if seen_owners[p.owner]  # skip if we already have one from this owner
 
       seen_owners[p.owner] = true   # we've seen this owner
--- app/models/planting_sweeper.rb
+++ app/models/planting_sweeper.rb
@@ -6,8 +6,13 @@ class PlantingSweeper < ActionController::Caching::Sweeper
     expire_fragment("member_thumbnail_#{planting.owner.slug}")
   end
 
+  def after_update(planting)
+    expire_fragment("planting_listitem_#{planting.id}")
+  end
+
   def after_destroy(planting)
     expire_fragment('homepage_stats')
+    Rails.cache.delete('interesting_plantings') if Planting.interesting.includes(planting)
   end
 
 end
