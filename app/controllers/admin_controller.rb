16f4e153f23ee9102857db40ed52d08b1fcbe95b
Skud <skud@infotrope.net>
2013-08-27 21:49:54 +1000

    Made admin/newsletter page with list of subscribers

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,4 +5,12 @@ class AdminController < ApplicationController
       format.html # index.html.haml
     end
   end
+
+  def newsletter
+    authorize! :manage, :all
+    @members = Member.confirmed.wants_newsletter.all
+    respond_to do |format|
+      format.html # index.html.haml
+    end
+  end
 end
