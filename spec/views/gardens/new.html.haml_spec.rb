d49a9e28e4cb8e9c58ecfdec439ab926c330092d
Skud <skud@infotrope.net>
2013-10-26 00:22:38 +1100

    Fixed broken test

--- spec/views/gardens/new.html.haml_spec.rb
+++ spec/views/gardens/new.html.haml_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe "gardens/new" do
   before(:each) do
-    controller.stub(:current_user) { Member.new }
-    assign(:garden, FactoryGirl.create(:garden))
+    @member = FactoryGirl.create(:member)
+    controller.stub(:current_member) { @member }
+    @garden = FactoryGirl.create(:garden, :owner => @member)
+    assign(:garden, @garden)
+    render
   end
 
   it "renders new garden form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => gardens_path, :method => "post" do
       assert_select "input#garden_name", :name => "garden[name]"
       assert_select "textarea#garden_description", :name => "garden[description]"
