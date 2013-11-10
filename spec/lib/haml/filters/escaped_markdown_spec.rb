9c899e80a3a07664a6d36c4b2416f15b53d727de
Skud <skud@infotrope.net>
2013-11-11 10:40:16 +1100

    Updated help text for markdown

--- spec/lib/haml/filters/escaped_markdown_spec.rb
+++ spec/lib/haml/filters/escaped_markdown_spec.rb
@@ -3,15 +3,6 @@ require 'haml/filters'
 require 'haml/filters/escaped_markdown'
 require 'haml/helpers'
 
-def input_link(name)
-  return "[#{name}](crop)"
-end
-
-def output_link(crop)
-  url = Rails.application.routes.url_helpers.crop_url(crop, :host => Growstuff::Application.config.host)
-  return Haml::Helpers.html_escape "<a href=\"#{url}\">#{crop.name}</a>"
-end
-
 describe 'Haml::Filters::Escaped_Markdown' do
   it 'is registered as the handler for :escaped_markdown' do
     Haml::Filters::defined['escaped_markdown'].should ==
@@ -25,8 +16,8 @@ describe 'Haml::Filters::Escaped_Markdown' do
 
   it 'converts quick crop links' do
     @crop = FactoryGirl.create(:crop)
-    rendered = Haml::Filters::EscapedMarkdown.render(input_link(@crop.name))
-    rendered.should match /#{output_link(@crop)}/
+    rendered = Haml::Filters::EscapedMarkdown.render("[#{@crop.name}](crop)")
+    rendered.should match /&lt;a href=&quot;/
   end
 
 end
