74e1e07567c53318e98174c6261f644768cdc407
Skud <skud@infotrope.net>
2014-04-28 12:42:23 +1000

    tweaking secret key settings to match what heroku already has

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "Growstuff <noreply@growstuff.org>"
 
-  config.secret_key = ENV['SECRET_KEY']
+  config.secret_key = ENV['RAILS_SECRET_TOKEN'] || 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
