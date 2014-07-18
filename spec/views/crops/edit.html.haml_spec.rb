7184a0d5d5bd2b279b4154503958d688407e4352
Skud <skud@infotrope.net>
2014-07-18 17:29:10 +1000

    Added scientific name field tests for edit form

--- spec/views/crops/edit.html.haml_spec.rb
+++ spec/views/crops/edit.html.haml_spec.rb
@@ -5,7 +5,11 @@ describe "crops/edit" do
     controller.stub(:current_user) {
       FactoryGirl.create(:crop_wrangling_member)
     }
-    @crop = assign(:crop, FactoryGirl.create(:maize))
+    @crop = FactoryGirl.create(:maize)
+    3.times do
+      @crop.scientific_names.build
+    end
+    assign(:crop, @crop)
     render
   end
 
@@ -18,5 +22,11 @@ describe "crops/edit" do
       assert_select "input#crop_name", :name => "crop[name]"
       assert_select "input#crop_en_wikipedia_url", :name => "crop[en_wikipedia_url]"
     end
+  end
+
+  it "shows three fields for scientific_name" do
+    assert_select "input#crop_scientific_names_attributes_0_scientific_name", :count => 1
+    assert_select "input#crop_scientific_names_attributes_1_scientific_name", :count => 1
+    assert_select "input#crop_scientific_names_attributes_2_scientific_name", :count => 1
   end
 end
