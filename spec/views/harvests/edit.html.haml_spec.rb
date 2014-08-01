63673112a756d8410c01bd6d96f7a2741ed6828e
Taylor Griffin <tygriffin@gmail.com>
2014-08-01 19:26:33 +1000

    repair failing view tests after implementing crop-suggest on harvest

--- spec/views/harvests/edit.html.haml_spec.rb
+++ spec/views/harvests/edit.html.haml_spec.rb
@@ -8,7 +8,8 @@ describe "harvests/edit" do
 
   it "renders new harvest form" do
     assert_select "form", :action => harvests_path, :method => "post" do
-      assert_select "select#harvest_crop_id", :name => "harvest[crop_id]"
+      assert_select "input#crop", :class => "ui-autocomplete-input"
+      assert_select "input#harvest_crop_id", :name => "harvest[crop_id]"
       assert_select "select#harvest_plant_part_id", :name => "harvest[plant_part_id]"
       assert_select "input#harvest_quantity", :name => "harvest[quantity]"
       assert_select "input#harvest_weight_quantity", :name => "harvest[quantity]"
