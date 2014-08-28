d8c7aba725d1339ba82127f1b472207735fb9ecd
Taylor Griffin <tygriffin@gmail.com>
2014-08-29 07:31:32 +1000

    pass options hash into auto_suggest helper to allow setting class

--- app/helpers/auto_suggest_helper.rb
+++ app/helpers/auto_suggest_helper.rb
@@ -1,13 +1,13 @@
 module AutoSuggestHelper
 
-  def auto_suggest(resource, source)
+  def auto_suggest(resource, source, options={})
     default = resource.send(source).nil? ? "" : resource.send(source).name
 
     resource = resource.class.name.downcase
     source_path = Rails.application.routes.url_helpers.send("#{source}s_search_path")
 
     %Q{
-      <input id="#{source}" class="auto-suggest" type="text" value="#{default}" data-source-url="#{source_path}">
+      <input id="#{source}" class="auto-suggest #{options[:class]}" type="text" value="#{default}" data-source-url="#{source_path}">
       <noscript>Warning: Javascript must be available to search and match crops</noscript>
       <input id="#{resource}_#{source}_id" class="auto-suggest-id" type="hidden" name="#{resource}[#{source}_id]">
     }.html_safe
