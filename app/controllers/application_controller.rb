965a3bf2a1815238f7591446893bcac39f6b1f1f
Skud <skud@infotrope.net>
2014-08-31 18:23:08 +1000

    Modernise redirection after signup to use devise's stored_location_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,19 @@ class ApplicationController < ActionController::Base
   after_filter :store_location
 
   def store_location
-    # store last url - this is needed for post-login redirect to whatever the user last visited.
-    if (request.fullpath != new_member_session_path && \
-      !request.xhr?) # don't store ajax calls
-      session[:previous_url] = request.fullpath
+    if (request.path != "/members/sign_in" &&
+        request.path != "/members/sign_up" &&
+        request.path != "/members/password/new" &&
+        request.path != "/members/password/edit" &&
+        request.path != "/members/confirmation" &&
+        request.path != "/members/sign_out" &&
+        !request.xhr?)
+        store_location_for(:member, request.fullpath)
     end
   end
 
   def after_sign_in_path_for(resource)
-    session[:previous_url] || root_path
+    stored_location_for(:member) || root_path
   end
 
   # tweak CanCan defaults because we don't have a "current_user" method
