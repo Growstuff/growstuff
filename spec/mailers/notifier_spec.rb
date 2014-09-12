2bbccfc44da1d8cb070c9cad5e9099915fb0ba18
Skud <skud@infotrope.net>
2014-09-12 12:30:04 +1000

    Added tests for planting reminder email

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -2,22 +2,45 @@ require "spec_helper"
 
 describe Notifier do
 
-  describe "notify" do
-    before(:each) do
-      @notification = FactoryGirl.create(:notification)
-      @mail = Notifier.notify(@notification)
-    end
+  describe "notifications" do
+    let(:notification) { FactoryGirl.create(:notification) }
+    let(:mail) { Notifier.notify(notification) }
 
     it 'sets the subject correctly' do
-      @mail.subject.should == @notification.subject
+      mail.subject.should == notification.subject
     end
 
     it 'comes from noreply@growstuff.org' do
-      @mail.from.should == ['noreply@growstuff.org']
+      mail.from.should == ['noreply@growstuff.org']
     end
 
     it 'sends the mail to the recipient of the notification' do
-      @mail.to.should == [@notification.recipient.email]
+      mail.to.should == [notification.recipient.email]
+    end
+  end
+
+  describe "planting reminders" do
+    let(:member) { FactoryGirl.create(:member) }
+    let(:mail) { Notifier.planting_reminder(member) }
+
+    it 'sets the subject correctly' do
+      mail.subject.should == "What have you planted lately?"
+    end
+
+    it 'comes from noreply@growstuff.org' do
+      mail.from.should == ['noreply@growstuff.org']
+    end
+
+    it 'sends the mail to the recipient of the notification' do
+      mail.to.should == [member.email]
+    end
+
+    it 'includes the new planting URL' do
+      mail.body.encoded.should match new_planting_path
+    end
+
+    it 'includes the new harvest URL' do
+      mail.body.encoded.should match new_harvest_path
     end
 
   end
