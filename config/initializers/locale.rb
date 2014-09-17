87df8661c7c6829549ef2e4bb5dc71d4af4527d2
Taylor Griffin <tygriffin@gmail.com>
2014-09-18 06:24:59 +1000

    raise exception on missing translations in test and dev environment

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,8 @@
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
+
+if Rails.env.development? || Rails.env.test?
+  I18n.exception_handler = lambda do |exception, locale, key, options|
+    raise "Missing translation: #{key}"
+  end
+end
  
