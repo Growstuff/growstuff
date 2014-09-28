d8b75eaa66d0f29e5c5d7f56bab8d47d5b8d8e94
Skud <skud@infotrope.net>
2014-09-28 15:55:55 +1000

    Fixed up db migration and tests for planting reminders

--- spec/features/planting_reminder_spec.rb
+++ spec/features/planting_reminder_spec.rb
@@ -3,18 +3,12 @@ require 'spec_helper'
 feature "planting reminder" do
   before :each do
     @member = FactoryGirl.create(:member)
-    visit root_path
-    click_link 'Sign in'
-    page.should have_content "Sign in"
-    fill_in 'Login', :with => @member.login_name
-    fill_in 'Password', :with => @member.password
-    click_button 'Sign in'
   end
 
   scenario "sends email" do
     expect {
       # stub for while we're working on this. remove!
-      visit send_planting_reminder_path
+      Notifier.planting_reminder(@member).deliver!
     }.to change { ActionMailer::Base.deliveries.count }.by(1)
   end
 end
