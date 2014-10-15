2515d34277a3420d71d25ab0553cc53b3e3c752b
Taylor Griffin <tygriffin@gmail.com>
2014-10-16 08:09:20 +1100

    add teardown to reset locale to default en

--- spec/features/locale_spec.rb
+++ spec/features/locale_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 feature "Changing locales" do
 
+  after do
+    I18n.locale = :en
+  end
+
   scenario "Locale can be set with a query param" do
     visit "/"
     expect(page).to have_content("a community of food gardeners.")
