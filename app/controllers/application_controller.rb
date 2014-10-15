6a7c935d43e8de4e818ff9435f4adb420cd1aa79
Taylor Griffin <tygriffin@gmail.com>
2014-10-16 06:37:02 +1100

    try setting locale from query parameter first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,10 +35,10 @@ class ApplicationController < ActionController::Base
   end
    
   def set_locale
-    I18n.locale = extract_locale_tld || I18n.default_locale
+    I18n.locale = params[:locale] || extract_locale_from_subdomain || I18n.default_locale
   end
 
-  def extract_locale_tld
+  def extract_locale_from_subdomain
     parsed_locale = request.subdomains.first
     I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
   end
