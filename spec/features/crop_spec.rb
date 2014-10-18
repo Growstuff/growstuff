c447c1cb3a4abb459e8be0f3f0e1bde001251f5c
Miles Gould <miles.gould@hogarthww.com>
2014-10-18 17:49:13 +0100

    Make edit-altname test more featurey.

--- spec/features/crop_spec.rb
+++ spec/features/crop_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 feature "Alternate names" do
   let(:alternate_eggplant) { FactoryGirl.create(:alternate_eggplant) }
+  let(:crop) { alternate_eggplant.crop }
 
   scenario "Display alternate names on crop page" do
     visit crop_path(alternate_eggplant.crop)
@@ -17,10 +18,15 @@ feature "Alternate names" do
     end
 
     scenario "Crop wranglers can edit alternate names" do
-      visit crop_path(alternate_eggplant.crop)
+      visit crop_path(crop)
       expect(page).to have_content "CROP WRANGLER"
       expect(page).to have_content alternate_eggplant.name
       expect(page).to have_link "Edit", :href => edit_alternate_name_path(alternate_eggplant)
+      within('.alternate_names') { click_on "Edit" }
+      expect(page).to have_css "option[value='#{crop.id}'][selected=selected]"
+      fill_in 'Name', with: "alternative aubergine"
+      click_on "Save"
+      expect(page).to have_content "alternative aubergine"
     end
 
     scenario "Crop wranglers can delete alternate names" do
@@ -30,7 +36,6 @@ feature "Alternate names" do
     end
 
     scenario "Crop wranglers can add alternate names" do
-      crop = alternate_eggplant.crop
       visit crop_path(crop)
       expect(page).to have_link "Add",
         href: new_alternate_name_path(crop_id: crop.id)
@@ -42,21 +47,11 @@ feature "Alternate names" do
     end
 
     scenario "The add-alternate-name page works" do
-      crop = alternate_eggplant.crop
       visit new_alternate_name_path(crop_id: crop.id)
       expect(page).to have_css "option[value='#{crop.id}'][selected=selected]"
       fill_in 'Name', with: "not an aubergine"
       click_on "Save"
       expect(page).to have_content "not an aubergine"
-    end
-
-    scenario "The edit-alternate-name page works" do
-      crop = alternate_eggplant.crop
-      visit edit_alternate_name_path(alternate_eggplant)
-      expect(page).to have_css "option[value='#{crop.id}'][selected=selected]"
-      fill_in 'Name', with: "alternative aubergine"
-      click_on "Save"
-      expect(page).to have_content "alternative aubergine"
     end
 
   end
