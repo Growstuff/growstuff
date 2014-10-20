c244da8b73c98945d49340ede84946bfbcbf291b
Taylor Griffin <tygriffin@gmail.com>
2014-10-21 07:24:08 +1100

    make English fallback language

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,12 @@ module Growstuff
     config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
+    I18n.default_locale = :en
+    # rails will fallback to config.i18n.default_locale translation
+    config.i18n.fallbacks = true
+    # rails will fallback to en, no matter what is set as config.i18n.default_locale
+    config.i18n.fallbacks = [:en]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
