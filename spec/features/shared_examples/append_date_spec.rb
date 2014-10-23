0ccd6e843a1b1e79de69e89ee00aab965b51423a
Taylor Griffin <tygriffin@gmail.com>
2014-10-23 22:43:38 +1100

    implement confirm without date functionality on appty on append date js

--- spec/features/shared_examples/append_date_spec.rb
+++ spec/features/shared_examples/append_date_spec.rb
@@ -1,6 +1,6 @@
 shared_examples "append date" do
 
-  scenario "Displaying a datepicker" do
+  scenario "Selecting a date with datepicker" do
     this_month = Date.today.strftime("%B")
     this_year  = Date.today.strftime("%Y")
     visit path
@@ -10,6 +10,13 @@ shared_examples "append date" do
       page.find(".datepicker-days td.day", text: "21").click
     end
     expect(page).to have_content "Finished: #{this_month} 21, #{this_year}"
+  end
+
+  scenario "Confirming without selecting date" do
+    visit path
+    click_link link_text
+    click_link "Confirm without date"
+    expect(page).to have_content("Finished: Yes (no date specified) ")
   end
 
 end
