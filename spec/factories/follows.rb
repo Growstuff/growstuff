e687bd43dc6bdd8c5052d95de01878673802416b
Yoong Kang Lim <yoongkang.lim@gmail.com>
2014-11-13 01:22:28 +1100

    Added model tests for follows

--- spec/factories/follows.rb
+++ spec/factories/follows.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :follow do
     member_id 1
-followed_id 1
+    followed_id 2
   end
 
 end
---
+++ spec/models/follow_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Follow do
+  
+  context "basic" do
+  	before(:each) do
+  	  @member1 = FactoryGirl.create(:member)
+  	  @member2 = FactoryGirl.create(:member)
+  	end
+
+  	it "sends a notification when a follow is created" do
+      expect {
+        Follow.create(:member_id => @member1.id, :followed_id => @member2.id)
+      }.to change(Notification, :count).by(1)
+    end
+  end
+
+end
