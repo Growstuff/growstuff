98c1680138e4d1955910eb69a67fab67e607da69
Taylor Griffin <tygriffin@gmail.com>
2014-11-17 07:16:02 +1100

    handle misc deprications

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,9 +8,6 @@ Growstuff::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
 
