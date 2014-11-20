8c55a6101d27ed76838b165c3ca77740564a6f83
Yoong Kang Lim <yoongkang.lim@gmail.com>
2014-11-21 01:14:12 +1100

    Formatting problem

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -7,7 +7,7 @@ describe Follow do
 	  @member2 = FactoryGirl.create(:member)
 	end
 
-	it "sends a notification when a follow is created" do
+  it "sends a notification when a follow is created" do
     expect {
       Follow.create(:follower_id => @member1.id, :followed_id => @member2.id)
     }.to change(Notification, :count).by(1)
