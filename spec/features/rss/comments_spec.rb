2111cddaef1dfd0130f2f1f1a7f23402dc31739c
Kevin Rio <kevinrio@gmail.com>
2014-11-23 21:02:56 -0500

    RSS feed tests

--- spec/features/rss/comments_spec.rb
+++ spec/features/rss/comments_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 feature 'Comments RSS feed' do
-  scenario 'This RSS feed exists' do
+  scenario 'The index feed exists' do
     visit comments_path(:format => 'rss')
     expect(page.status_code).to equal 200
   end
 
-  scenario 'The feed title is what we expect' do
+  scenario 'The index title is what we expect' do
     visit comments_path(:format => 'rss')
-    expect(page).to have_content "#{ENV['GROWSTUFF_SITE_NAME']} - Recent comments on all posts"
+    expect(page).to have_content "Recent comments on all posts (#{ENV['GROWSTUFF_SITE_NAME']})"
   end
 end
---
+++ spec/features/rss/members_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Members RSS feed' do
+  let(:member)   { FactoryGirl.create(:member) }
+
+  scenario 'The show action exists' do
+    visit member_path(member, :format => 'rss')
+    expect(page.status_code).to equal 200
+  end
+
+  scenario 'The show action title is what we expect' do
+    visit member_path(member, :format => 'rss')
+    expect(page).to have_content "#{member.login_name}'s recent posts (#{ENV['GROWSTUFF_SITE_NAME']})"
+  end
+end
