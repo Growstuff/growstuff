36ac0b75e7aba3747fbd55a4dea4759bd7c298e7
Taylor Griffin <tygriffin@gmail.com>
2015-01-15 22:23:27 +1100

    decoy commit to test if travis / phantomjs error was spurious

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -57,7 +57,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment.post, notice: "Comment was successfully created." }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
