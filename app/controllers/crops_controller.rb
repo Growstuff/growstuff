556ba33172704589c6c87fe3e353bafc395107aa
Shiho Takagi <shiho_taka@hotmail.com>
2015-01-17 00:09:39 +1100

    updated crop search view

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -51,18 +51,19 @@ class CropsController < ApplicationController
 
   # GET /crops/search
   def search
-    @search = params[:search]
-    @exact_match = Crop.find_by_name(params[:search])
+    query = params[:term] || params[:search]
+    @all_matches = Crop.search(query)
 
-    @partial_matches = Crop.search(params[:search])
-    # exclude exact match from partial match list
-    @partial_matches.reject!{ |r| @exact_match && r.eql?(@exact_match) }
-
-    @fuzzy = Crop.search(params[:term])
+    if exact_match = Crop.find_by_name(query)
+      @all_matches.delete(exact_match)
+      @all_matches.unshift(exact_match)
+    end
 
     respond_to do |format|
       format.html
-      format.json { render :json => @fuzzy }
+      format.json { 
+        render :json => @all_matches
+      }
     end
   end
 
