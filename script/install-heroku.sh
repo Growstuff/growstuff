0350ac6e26936f223a6ef990656f1116a4cbe4f2
Miles Gould <miles.gould@hogarthww.com>
2015-01-20 09:11:30 +0000

    Maintenance mode, attempt 4.

---
+++ script/install-heroku.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Install the Heroku toolbelt
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+if [ $TRAVIS_BRANCH == "dev" ]; then
+        APP_NAME=growstuff-staging
+elif [ $TRAVIS_BRANCH == "master" ]; then
+        APP_NAME=growstuff-prod
+elif [ $TRAVIS_BRANCH == "travis_deploy" ]; then
+        APP_NAME=tranquil-basin-3130
+else
+        echo "Couldn't find app corresponding to branch $TRAVIS_BRANCH"
+        exit 1
+fi
+
+git remote add heroku git@heroku.com:$APP_NAME.git
+echo >> ~/.ssh_config <<END_SSH_CONFIG
+Host heroku.com
+  StrictHostKeyChecking no
+  CheckHostIP no
+  UserKnownHostsFile=/dev/null
+END_SSH_CONFIG
+
+yes | heroku keys:add
