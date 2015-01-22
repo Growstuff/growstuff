542d978d9cc9ea0c3cd6768e87033a7c63ac3981
Cesy Avon <cesy.avon@gmail.com>
2015-01-22 14:24:03 +0000

    Adding extra tests for crop wrangling

--- spec/features/crops/crop_wranglers_spec.rb
+++ spec/features/crops/crop_wranglers_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 feature "crop wranglers" do
-  context "signed in member" do
+  context "signed in wrangler" do
     let!(:crop_wranglers) { FactoryGirl.create_list(:crop_wrangling_member, 3) }
-    let(:member){crop_wranglers.first}
+    let(:wrangler){crop_wranglers.first}
+    let!(:crops) { FactoryGirl.create_list(:crop, 2) }
 
     background do
-      login_as(member)
+      login_as(wrangler)
     end
-
-    scenario "crop wranglers are listed on the crop wrangler page" do
+    
+    scenario "sees crop wranglers listed on the crop wrangler page" do
       visit root_path
       click_link 'Crop Wrangling'
 
@@ -20,5 +21,36 @@ feature "crop wranglers" do
         end
       end
     end
+  
+    scenario "can see list of crops with extra detail of who created a crop" do
+      visit root_path
+      click_link 'Crop Wrangling'
+      within '.table' do
+        expect(page).to have_content "#{crops.first.creator.login_name}"
+      end
+    end
+      
+    scenario "visiting a crop can see wrangler links" do
+      visit crop_path(crops.first)
+      expect(page).to have_content 'You are a CROP WRANGLER'
+      expect(page).to have_link 'Edit crop'
+      expect(page).to have_link 'Delete crop'
+    end
+    
+  end
+  
+  context "signed in non-wrangler" do
+    let!(:crop_wranglers) { FactoryGirl.create_list(:crop_wrangling_member, 3) }
+    let(:member) { FactoryGirl.create(:member) }
+
+    background do
+      login_as(member)
+    end
+
+    scenario "can't see wrangling page" do
+      visit root_path
+      expect(page).not_to have_link "Crop Wrangling"
+    end
+    
   end
 end
