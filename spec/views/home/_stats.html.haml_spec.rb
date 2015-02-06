c1ab1f64ed421dbec11760e2806a0a39f0c239ed
Taylor Griffin <tygriffin@gmail.com>
2015-02-06 22:26:59 +1100

    removed extraneous tests

--- spec/views/home/_stats.html.haml_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe 'home/_stats.html.haml', :type => "view" do
-  it 'has activity stats' do
-    render
-    rendered.should have_content "So far, 0 members have planted 0 crops"
-  end
-end
