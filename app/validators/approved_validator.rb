2697fea24982c0b824db3139d7cc9062c3d0ee36
Taylor Griffin <tygriffin@gmail.com>
2015-02-09 22:41:35 +1100

    tests back to green

--- app/validators/approved_validator.rb
+++ app/validators/approved_validator.rb
@@ -1,6 +1,6 @@
 class ApprovedValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value.approved?
+    unless record.crop.try(:approved?)
       record.errors[attribute] << (options[:message] || 'must be approved')
     end
   end
