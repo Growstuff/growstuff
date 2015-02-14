200697dd96c64b8f7ac6864e882ae2b902154eea
borracciaBlu <marco.asdman@gmail.com>
2015-02-14 12:03:43 +1100

    improved interface gardens#index

--- spec/views/gardens/index.html.haml_spec.rb
+++ spec/views/gardens/index.html.haml_spec.rb
@@ -19,7 +19,6 @@ describe "gardens/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => @garden.name, :count => 2
     assert_select "tr>td>a", :text => @garden.location, :count => 2
-    assert_select "tr>td", :text => pluralize(@garden.area, @garden.area_unit), :count => 2
   end
 
 end
