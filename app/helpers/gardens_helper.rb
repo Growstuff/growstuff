f5ecd50a6ff155e205a15218be94698f031f705e
borracciaBlu <marco.asdman@gmail.com>
2015-02-16 20:26:12 +1100

    Fixed #517 tooltip and filter

--- app/helpers/gardens_helper.rb
+++ app/helpers/gardens_helper.rb
@@ -3,9 +3,9 @@ module GardensHelper
   def area_abbreviations(area_verbose)
     area_abbreviations = {
       'square metre' => 'm<sup>2</sup>',
-      'square foot'  => 'ft<sup>2</sup>',
-      'hectare'      => 'HA',
-      'acre'         => 'A',
+      'square foot'  => 'sqft',
+      'hectare'      => 'ha',
+      'acre'         => 'acre',
     }
     
     if area_abbreviations.include? area_verbose
