1b62c5cb97cb8feb0de3c9ef9cf29dc63cf55e3b
Mackenzie Morgan <macoafi@gmail.com>
2015-02-25 00:05:56 -0500

    remove pending/rejected crops from browse and test that, change links on wrangling page to go directly to crop edit and update tests for that

--- spec/features/crops/browse_crops_spec.rb
+++ spec/features/crops/browse_crops_spec.rb
@@ -4,6 +4,8 @@ feature "browse crops" do
 
   let(:tomato) { FactoryGirl.create(:tomato) }
   let(:maize)  { FactoryGirl.create(:maize) }
+  let(:pending_crop)  { FactoryGirl.create(:crop_request) }
+  let(:rejected_crop)  { FactoryGirl.create(:rejected_crop) }
 
   scenario "has a form for sorting by" do
     visit crops_path
@@ -16,4 +18,13 @@ feature "browse crops" do
     expect(page).to have_content crop1.name
   end
 
+  scenario "pending crops are not listed" do
+    visit crops_path
+    expect(page).not_to have_content pending_crop.name
+  end
+
+  scenario "rejected crops are not listed" do
+    visit crops_path
+    expect(page).not_to have_content rejected_crop.name
+  end
 end
