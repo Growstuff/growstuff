0d55b5437144f21e757e1fb5e918b7a840b2a4b1
Jake Yesbeck <jake@apartmentlist.com>
2015-05-05 09:05:00 -0700

    Use a constant for the default robots.txt filename

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,10 +1,13 @@
 class RobotsController < ApplicationController
+
+  DEFAULT_FILENAME = 'config/robots.txt'.freeze
+
   def robots
     filename = if subdomain && subdomain != 'www'
       "config/robots.#{ subdomain }.txt"
     end
 
-    file_to_render = File.exists?(filename.to_s) ? filename : 'config/robots.txt'
+    file_to_render = File.exists?(filename.to_s) ? filename : DEFAULT_FILENAME
 
     render file: file_to_render, layout: false, content_type: 'text/plain'
   end
