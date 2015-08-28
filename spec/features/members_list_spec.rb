f3579167790eb1377d53f851d72a11e6466abc45
Daniel O'Connor <daniel.oconnor@gmail.com>
2015-08-29 07:49:55 +0930

    For more accurate specs, explicitly target the login-name container

--- spec/features/members_list_spec.rb
+++ spec/features/members_list_spec.rb
@@ -11,7 +11,7 @@ feature "members list" do
       expect(page).to have_css "#sort"
       expect(page).to have_selector "form"
       click_button('Show')
-      all_links = page.all("#maincontainer p")
+      all_links = page.all("#maincontainer p.login-name")
       expect(all_links.first).to have_text member1.login_name
       expect(all_links.last).to have_text member2.login_name
     end
@@ -22,7 +22,7 @@ feature "members list" do
       expect(page).to have_selector "form"
       select("recently", :from => 'sort')
       click_button('Show')
-      all_links = page.all("#maincontainer p")
+      all_links = page.all("#maincontainer p.login-name")
       expect(all_links.first).to have_text member3.login_name
       expect(all_links.last).to have_text member1.login_name
     end
