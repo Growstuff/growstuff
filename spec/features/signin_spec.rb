00ca01dc33a2076fc1e697ae66b969ac5148098f
Daniel O'Connor <daniel.oconnor@gmail.com>
2015-09-15 11:20:33 +0930

    Add coverage of the most basic scenarios around sign up and login

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -50,4 +50,28 @@ feature "signin", :js => true do
     click_button 'Sign in'
     expect(current_path).to eq new_notification_path
   end
+
+  context "with facebook" do
+     scenario "sign in" do
+      # Ordinarily done by database_cleaner
+      Member.where(login_name: 'tdawg').delete_all
+
+      member = create :member, login_name: 'tdawg', email: 'example.oauth.facebook@example.com'
+
+      # Start the test
+      visit root_path 
+      first('.signup a').click
+
+      # Click the signup with facebook link
+
+      first('a[href="/members/auth/facebook"]').click
+      # Magic happens! 
+      # See config/environments/test.rb for the fake user
+      # that we pretended to auth as
+
+      # Signed up and logged in
+      expect(current_path).to eq root_path
+      expect(page.text).to include("Welcome to Growstuff (test), tdawg")
+    end
+  end
 end
