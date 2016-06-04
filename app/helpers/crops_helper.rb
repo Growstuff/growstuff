a8d22709e604165790dfe336c908d8d9526e0b50
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-06-04 20:51:20 +0930

    Add explict test coverage of the helper with nil seeds (page level tests already cover this rendering on crop detail in general)

--- app/helpers/crops_helper.rb
+++ app/helpers/crops_helper.rb
@@ -1,17 +1,21 @@
 module CropsHelper
   def display_seed_availability(member, crop)
     total_quantity = 0
-    member.seeds.each do |seed|
-      if seed.crop.name == crop.name && seed.quantity
-        total_quantity = total_quantity + seed.quantity
-      end
+
+    seeds = member.seeds.select {|seed| seed.crop.name == crop.name }
+
+    seeds.each do |seed|
+      total_quantity = total_quantity + seed.quantity if seed.quantity
+    end
+
+    if !seeds.any?
+      return "You don't have any seeds of this crop."
     end
 
     if (total_quantity != 0)
       "You have #{pluralize(total_quantity, "seed")} of this crop."
     else
-      "You don't have any seeds of this crop."
+      "You have an unknown quantity of seeds of this crop."
     end
-  
   end
 end
