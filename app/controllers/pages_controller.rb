7261fae9f26ce6052851855cfafa796dddd8061d
Cesy <cesy.avon@gmail.com>
2016-06-21 15:06:02 +0100

    Revert "challenge code"

--- app/controllers/pages_controller.rb
+++
@@ -1,6 +0,0 @@
-class PagesController < ApplicationController
-  def letsencrypt
-    # use your code here, not mine
-    render text: "B4-cHp8zD8RDI8ihZYvqzb2mV6RPSmrQ-clntqc-5Is.dlIPqFhMDCLyQEccczY3roHZ1UWu6UqVeyb9mkRxheU"
-  end
-end
