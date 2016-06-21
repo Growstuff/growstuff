1c3d0b16bd2ce595deaff69e300d0d80abe62047
Mackenzie Morgan <macoafi@gmail.com>
2016-06-21 11:33:10 -0400

    style fixes on gardens_helper

--- app/helpers/gardens_helper.rb
+++ app/helpers/gardens_helper.rb
@@ -1,21 +1,20 @@
 module GardensHelper
-
   def display_garden_description(garden)
     if garden.description.nil?
-      "no description provided."
+      'no description provided.'
     else
-      truncate(garden.description, length: 130, separator: ' ', omission: '... ') { link_to "Read more", garden_path(garden) }
+      truncate(garden.description, length: 130, separator: ' ', omission: '... ') { link_to 'Read more', garden_path(garden) }
     end
   end
 
   def display_garden_plantings(plantings)
     if plantings.blank?
-      "None"
+      'None'
     else
-      output = ""
+      output = ''
       plantings.first(2).each do |planting|
-        output += "<li>"
-        output += planting.quantity.nil? ? "0 " : "#{planting.quantity} "
+        output += '<li>'
+        output += planting.quantity.nil? ? '0 ' : "#{planting.quantity} "
         output += link_to planting.crop.name, planting.crop
         output += ", planted on #{planting.planted_at}</li>"
       end
