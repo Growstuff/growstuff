9942adbfeb4f4377c29edd937f94fee2fef84290
Brenda Wallace <brenda@wallace.net.nz>
2016-09-17 18:44:37 +1200

    attributes and relationships for our models

--- app/resources/api/v1/garden_resource.rb
+++ app/resources/api/v1/garden_resource.rb
@@ -1,4 +1,5 @@
 class Api::V1::GardenResource < JSONAPI::Resource
-  attributes :name, :slug, :description, :active, :area, :area_unit
+  attributes :name, :slug, :description, :active, :area, :area_unit, :location, :longitude, :latitude
   has_one :owner
+  has_many :plantings
 end
--- app/resources/api/v1/member_resource.rb
+++ app/resources/api/v1/member_resource.rb
@@ -1,2 +1,7 @@
 class Api::V1::MemberResource < JSONAPI::Resource
+  attributes :login_name, :bio, :preferred_avatar_uri
+  has_many :gardens
+  has_many :plantings
+  has_many :posts
+  has_many :seeds
 end
