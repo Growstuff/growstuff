ce2141ddb7d25682998900da8795b5cf965ee9ad
Miles Gould <mgould@redhat.com>
2016-10-07 14:49:09 +0100

    Only initialise Crop.__elasticsearch__ if needed
    The mere presence of this field was causing any test that ran
    `FactoryGirl.create :alternate_name` to try to connect to an
    ElasticSearch server, even if GROWSTUFF_ELASTICSEARCH was false.
    
    Fixes #1047

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -50,44 +50,46 @@ class Crop < ActiveRecord::Base
 
   ####################################
   # Elastic search configuration
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
-  # In order to avoid clashing between different environments,
-  # use Rails.env as a part of index name (eg. development_growstuff)
-  index_name [Rails.env, "growstuff"].join('_')
-  settings index: { number_of_shards: 1 },
-    analysis: {
-      tokenizer: {
-        gs_edgeNGram_tokenizer: {
-          type: "edgeNGram",  # edgeNGram: NGram match from the start of a token
-          min_gram: 3,
-          max_gram: 10,
-          # token_chars: Elasticsearch will split on characters
-          # that don’t belong to any of these classes
-          token_chars: [ "letter", "digit" ] 
-        }
-      },
-      analyzer: {
-        gs_edgeNGram_analyzer: {
-          tokenizer: "gs_edgeNGram_tokenizer",
-          filter: ["lowercase"]
-        }
-      },
-    } do
-    mappings dynamic: 'false' do
-      indexes :id, type: 'long'
-      indexes :name, type: 'string', analyzer: 'gs_edgeNGram_analyzer'
-      indexes :approval_status, type: 'string'
-      indexes :scientific_names do
-        indexes :scientific_name,
-          type: 'string',
-          analyzer: 'gs_edgeNGram_analyzer',
-          # Disabling field-length norm (norm). If the norm option is turned on(by default), 
-          # higher weigh would be given for shorter fields, which in our case is irrelevant.
-          norms: { enabled: false }
-      end
-      indexes :alternate_names do
+  if ENV["GROWSTUFF_ELASTICSEARCH"] == "true"
+    include Elasticsearch::Model
+    include Elasticsearch::Model::Callbacks
+    # In order to avoid clashing between different environments,
+    # use Rails.env as a part of index name (eg. development_growstuff)
+    index_name [Rails.env, "growstuff"].join('_')
+    settings index: { number_of_shards: 1 },
+      analysis: {
+        tokenizer: {
+          gs_edgeNGram_tokenizer: {
+            type: "edgeNGram",  # edgeNGram: NGram match from the start of a token
+            min_gram: 3,
+            max_gram: 10,
+            # token_chars: Elasticsearch will split on characters
+            # that don’t belong to any of these classes
+            token_chars: [ "letter", "digit" ]
+          }
+        },
+        analyzer: {
+          gs_edgeNGram_analyzer: {
+            tokenizer: "gs_edgeNGram_tokenizer",
+            filter: ["lowercase"]
+          }
+        },
+      } do
+      mappings dynamic: 'false' do
+        indexes :id, type: 'long'
         indexes :name, type: 'string', analyzer: 'gs_edgeNGram_analyzer'
+        indexes :approval_status, type: 'string'
+        indexes :scientific_names do
+          indexes :scientific_name,
+            type: 'string',
+            analyzer: 'gs_edgeNGram_analyzer',
+            # Disabling field-length norm (norm). If the norm option is turned on(by default),
+            # higher weigh would be given for shorter fields, which in our case is irrelevant.
+            norms: { enabled: false }
+        end
+        indexes :alternate_names do
+          indexes :name, type: 'string', analyzer: 'gs_edgeNGram_analyzer'
+        end
       end
     end
   end
