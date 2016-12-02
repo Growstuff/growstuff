3c93fcda91ad96aa004827c1146034e4a80f7ea3
Brenda Wallace <brenda@wallace.net.nz>
2016-12-02 20:07:46 +1300

    Wrapped long lines

--- app/resources/api/v1/seed_resource.rb
+++ app/resources/api/v1/seed_resource.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class Api::V1::SeedResource < JSONAPI::Resource
-  attributes :description, :quantity, :plant_before, :days_until_maturity_min, :days_until_maturity_max, :organic, :gmo, :heirloom
+  attributes :description, :quantity, :plant_before, :days_until_maturity_min,
+    :days_until_maturity_max, :organic, :gmo, :heirloom
   has_one :owner
   has_one :crop
 end
