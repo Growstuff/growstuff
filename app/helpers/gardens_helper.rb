f0110edc05e3327dea0afac073b694b74d70cd02
Brenda Wallace <brenda@wallace.net.nz>
2016-12-07 20:01:41 +1300

    Less busy gardens page

--- app/helpers/gardens_helper.rb
+++ app/helpers/gardens_helper.rb
@@ -1,26 +1,21 @@
 module GardensHelper
   def display_garden_description(garden)
-    if garden.description.nil?
-      "no description provided."
-    else
-      truncate(garden.description, length: 130, separator: ' ', omission: '... ') do
-        link_to "Read more", garden_path(garden)
-      end
+    return "" unless garden.description.present?
+    truncate(garden.description, length: 130, separator: ' ', omission: '... ') do
+      link_to "Read more", garden_path(garden)
     end
   end
 
   def display_garden_plantings(plantings)
-    if plantings.blank?
-      "None"
-    else
-      output = ""
-      plantings.first(2).each do |planting|
-        output += "<li>"
-        output += planting.quantity.nil? ? "0 " : "#{planting.quantity} "
-        output += link_to planting.crop.name, planting.crop
-        output += ", planted on #{planting.planted_at}</li>"
-      end
-      output.html_safe
+    return "" unless plantings.present?
+    output = '<ul class="garden-plantings">'
+    plantings.first(6).each do |planting|
+      output += "<li>"
+      output += "#{planting.quantity} " if planting.quantity.present?
+      output += link_to planting.crop.name, planting.crop
+      output += '</li>'
     end
+    output += '</ul>'
+    output.html_safe
   end
 end
