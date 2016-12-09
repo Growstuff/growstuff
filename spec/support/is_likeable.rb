2499db77f14933a976514869cb50975ba93fd685
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-12-09 11:59:56 +1030

    Rubocop

--- spec/support/is_likeable.rb
+++ spec/support/is_likeable.rb
@@ -1,13 +1,12 @@
 shared_examples "it is likeable" do
-  
   before(:each) do
     # Possibly a horrible hack.
     # Will fail if factory name does not match the model name..
     @likeable = FactoryGirl.create(described_class.to_s.underscore.to_sym)
     @member1 = FactoryGirl.create(:member)
     @member2 = FactoryGirl.create(:member)
-    @like1 = FactoryGirl.create(:like, :member => @member1, :likeable => @likeable)
-    @like2 = FactoryGirl.create(:like, :member => @member2, :likeable => @likeable)
+    @like1 = FactoryGirl.create(:like, member: @member1, likeable: @likeable)
+    @like2 = FactoryGirl.create(:like, member: @member2, likeable: @likeable)
   end
 
   it "has many likes" do
@@ -19,9 +18,7 @@ shared_examples "it is likeable" do
   end
 
   it 'should destroy the like when it is destroyed' do
-    like_count = @likeable.likes.count
-    expect {
-      @likeable.destroy
-    }.to change(Like, :count).by -like_count
+    like_count = -1 * @likeable.likes.count
+    expect { @likeable.destroy }.to change(Like, :count).by like_count
   end
 end
