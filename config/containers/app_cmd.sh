2819841d6c2bea4ef849cb37f89b37d6ded91c50
Miles Gould <mgould@redhat.com>
2017-02-02 18:43:26 +0000

    wip

--- config/containers/app_cmd.sh
+++ config/containers/app_cmd.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
+exec bundle exec rake db:create
+exec bundle exec rake db:seed
 exec bundle exec unicorn -c config/containers/unicorn.rb -E $RAILS_ENV;
