f40ff189497f6ffac4d3ef7915d1551ba045970b
Brenda Wallace <brenda@wallace.net.nz>
2017-02-12 22:14:55 +1300

    You can only reply to messages that were sent to you.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -27,7 +27,7 @@ class NotificationsController < ApplicationController
   # GET /notifications/1/reply
   def reply
     @notification = Notification.new
-    @sender_notification = Notification.find(params[:id])
+    @sender_notification = Notification.find_by(id: params[:id], recipient: current_member)
     @sender_notification.read = true
     @sender_notification.save
     @recipient = @sender_notification.sender
