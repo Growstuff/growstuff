d8d94d178c647177d0c42b943d87cbfe623362cf
Brenda Wallace <brenda@wallace.net.nz>
2017-02-12 15:13:34 +1300

    Moved some of likes#destroy out to new methods

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,36 +4,26 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.new(member: current_member, likeable: find_likeable)
-    unable_to_like(@like.likeable) unless @like.likeable && @like.save
+    unable_to_like(@like) unless @like.likeable && @like.save
 
     respond_to do |format|
       format.html { redirect_to @like.likeable }
-      format.json { render(json: render_json(@like), status: 201) }
+      format.json do
+        render(json: render_json(@like, liked_by_member: true),
+               status: 201)
+      end
     end
   end
 
   def destroy
-    like = Like.find(params[:id])
-    likeable = like.likeable
+    @like = Like.find_by(id: params[:id], member: current_member)
+    unable_to_unlike(@like) unless @like && @like.destroy
+
     respond_to do |format|
-      if like.destroy
-        format.html { redirect_to likeable }
-        format.json do
-          render(
-            json: {
-              id: likeable.id,
-              liked_by_member: false,
-              description: ActionController::Base.helpers.pluralize(likeable.likes.count, "like"),
-              url: likes_path(Like.new, "#{likeable.class.name.underscore}_id", likeable.id, format: :json)
-            },
-            status: 200
-          )
-        end
-      else
-        format.html do
-          flash[:error] = 'Unable to unlike'
-          redirect_to likeable
-        end
+      format.html { redirect_to @like.likeable }
+      format.json do
+        render(json: render_json(@like, liked_by_member: false),
+               status: 200)
       end
     end
   end
@@ -41,31 +31,37 @@ class LikesController < ApplicationController
   private
 
   def find_likeable
-    # params.each do |name, value|
-    #   return Regexp.last_match[1].classify.constantize.find(value) if name =~ /(.+)_id$/
-    # end
     Post.find(params[:post_id]) if params[:post_id]
   end
 
-  def like_params
-    params.require(:like).permit(:post_id)
-  end
-
-  def render_json(like)
+  def render_json(like, liked_by_member)
     {
       id: like.likeable.id,
-      liked_by_member: true,
+      liked_by_member: liked_by_member,
       description: ActionController::Base.helpers.pluralize(like.likeable.likes.count, "like"),
       url: like_path(like, format: :json)
     }
   end
 
-  def unable_to_like(likeable)
+  def unable_to_unlike(like)
+    respond_to do |format|
+      format.html do
+        flash[:error] = 'Unable to unlike'
+        if like.likeable
+          redirect_to like.likeable
+        else
+          redirect_to root_path
+        end
+      end
+    end
+  end
+
+  def unable_to_like(like)
     respond_to do |format|
       format.html do
         flash[:error] = 'Unable to like'
-        if likeable
-          redirect_to @like.likeable
+        if like.likeable
+          redirect_to like.likeable
         else
           redirect_to root_path
         end
