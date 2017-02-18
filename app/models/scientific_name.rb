3089c28233cf7443bec3905342c78ad43b8bb0db
Brenda Wallace <brenda@wallace.net.nz>
2017-02-18 14:38:21 +1300

    Adding validations to sci names and alt names

--- app/models/scientific_name.rb
+++ app/models/scientific_name.rb
@@ -2,4 +2,6 @@ class ScientificName < ActiveRecord::Base
   after_commit { |sn| sn.crop.__elasticsearch__.index_document if sn.crop && ENV['GROWSTUFF_ELASTICSEARCH'] == "true" }
   belongs_to :crop
   belongs_to :creator, class_name: 'Member'
+  validates :name, presence: true
+  validates :crop, presence: true
 end
