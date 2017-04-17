b617eb3140ccd2173a9972dd1b8c146091f09e36
Brenda Wallace <brenda@wallace.net.nz>
2017-04-17 12:42:30 +1200

    Deleting photo's association with another item

--- app/controllers/photo_associations_controller.rb
+++ app/controllers/photo_associations_controller.rb
@@ -4,10 +4,14 @@ class PhotoAssociationsController < ApplicationController
 
   def destroy
     @photo = Photo.find(params[:photo_id])
-    if params[:type] == 'planting'
-      @planting = Planting.find(params[:planting_id])
-      @photo.plantings.delete(@planting) if @planting.owner == current_member && @photo.owner == current_member
-    end
+    collection = Growstuff::Constants::PhotoModels.get_relation(@photo, params[:type])
+    item_class = Growstuff::Constants::PhotoModels.get_item(params[:type])
+    @item = item_class.find_by!(id: params[:id], owner_id: current_member.id)
+    collection.delete(@item) if owner_matches?
     respond_with(@photo)
+  end
+
+  def owner_matches?
+    @photo.owner == current_member && @item.owner == current_member
   end
 end
