16d88730ecf9ba22b53ecea063e2fca4eafaac36
Brenda Wallace <brenda@wallace.net.nz>
2017-04-17 13:37:56 +1200

    controller specs for PhotoAssociations

---
+++ spec/controllers/photo_associations_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe PhotoAssociationsController do
+  login_member
+
+  describe "destroy" do
+    let(:valid_params) do
+      {
+        id: harvest.id,
+        type: 'harvest',
+        photo_id: photo.id
+      }
+    end
+
+    before { photo.harvests << harvest }
+
+    describe "my harvest my photo" do
+      let(:harvest) { FactoryGirl.create :harvest, owner: member }
+      let(:photo) { FactoryGirl.create :photo, owner: member }
+
+      it "removes link" do
+        expect { delete :destroy, valid_params }.to change { photo.harvests.count }.by(-1)
+      end
+    end
+
+    describe "another member's harvest from another member's photo" do
+      let(:harvest) { FactoryGirl.create :harvest }
+      let(:photo) { FactoryGirl.create :photo }
+      it do
+        expect do
+          begin
+            delete :destroy, valid_params
+          rescue
+            nil
+          end
+        end.not_to change { photo.harvests.count }
+      end
+      it { expect { delete :destroy, valid_params }.to raise_error(ActiveRecord::RecordNotFound) }
+    end
+  end
+end
