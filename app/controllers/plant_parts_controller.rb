ba0a014a64fec16be26ab8734a93c9e715891322
Brenda Wallace <brenda@wallace.net.nz>
2017-05-28 10:48:08 +1200

    Removed route comments and extras on plant_parts controller

--- app/controllers/plant_parts_controller.rb
+++ app/controllers/plant_parts_controller.rb
@@ -3,46 +3,32 @@ class PlantPartsController < ApplicationController
   respond_to :html, :json
   responders :flash
 
-  # GET /plant_parts
-  # GET /plant_parts.json
   def index
     @plant_parts = PlantPart.all
     respond_with(@plant_parts)
   end
 
-  # GET /plant_parts/1
-  # GET /plant_parts/1.json
   def show
     respond_with(@plant_part)
   end
 
-  # GET /plant_parts/new
-  # GET /plant_parts/new.json
   def new
     @plant_part = PlantPart.new
     respond_with(@plant_part)
   end
 
-  # GET /plant_parts/1/edit
   def edit; end
 
-  # POST /plant_parts
-  # POST /plant_parts.json
   def create
-    @plant_part = PlantPart.new(plant_part_params)
-    @plant_part.save
+    @plant_part = PlantPart.create(plant_part_params)
     respond_with(@plant_part)
   end
 
-  # PUT /plant_parts/1
-  # PUT /plant_parts/1.json
   def update
     @plant_part.update(plant_part_params)
     respond_with(@plant_part)
   end
 
-  # DELETE /plant_parts/1
-  # DELETE /plant_parts/1.json
   def destroy
     @plant_part.destroy
     respond_with(@plant_part)
