aa2c4b0e9bf38f330efdbaa96d575b6fc25d3788
Brenda Wallace <brenda@wallace.net.nz>
2017-05-28 14:52:12 +1200

    DRY photos controller more

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,50 +3,41 @@ class PhotosController < ApplicationController
   after_action :expire_homepage, only: [:create, :delete]
   load_and_authorize_resource
   respond_to :html, :json
+  responders :flash
 
-  # GET /photos
-  # GET /photos.json
   def index
     @photos = Photo.paginate(page: params[:page])
     respond_with(@photos)
   end
 
-  # GET /photos/new
-  # GET /photos/new.json
   def new
     @type = params[:type]
     @id = params[:id]
 
     @photo = Photo.new
     retrieve_from_flickr
-    respond_with(@photo)
+    respond_with @photo
   end
 
-  # GET /photos/1/edit
-  def edit; end
+  def edit
+    respond_with @photo
+  end
 
-  # POST /photos
-  # POST /photos.json
   def create
     find_or_create_photo_from_flickr_photo
     add_photo_to_collection
-    flash[:notice] = 'Photo was successfully added.' if @photo.present? && @photo.save
-    respond_with(@photo)
+    @photo.save if @photo.present?
+    respond_with @photo
   end
 
-  # PUT /photos/1
-  # PUT /photos/1.json
   def update
-    flash[:notice] = 'Photo was successfully updated.' if @photo.update(photo_params)
-    respond_with(@photo)
+    @photo.update(photo_params)
+    respond_with @photo
   end
 
-  # DELETE /photos/1
-  # DELETE /photos/1.json
   def destroy
     @photo.destroy
-    flash[:alert] = "Photo successfully deleted."
-    respond_with(@photo)
+    respond_with @photo
   end
 
   private
