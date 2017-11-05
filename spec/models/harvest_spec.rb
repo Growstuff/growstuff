26537943e3d2df4df85c0a67878318dadd88bccb
Brenda Wallace <brenda@wallace.net.nz>
2017-11-05 18:00:49 +1300

    Removed pending spec

--- spec/models/harvest_spec.rb
+++ spec/models/harvest_spec.rb
@@ -302,10 +302,4 @@ describe Harvest do
     member.destroy
     expect(Harvest.joins(:owner).all).not_to include(harvest)
   end
-
-  pending 'sets planting harvested_at date' do
-    planting = FactoryGirl.create :planting
-    harvest = FactoryGirl.create :harvest, planting: planting, crop: planting.crop
-    expect(harvest.harvested_at).to eq(planting.harvested_at)
-  end
 end
