b1684b0156cddd7bc81fdce824bbdd93478f60b7
Brenda Wallace <brenda@wallace.net.nz>
2017-11-05 14:14:53 +1300

    Set prediction data for existing planting and crops

---
+++ db/migrate/20171105011017_set_prediction_data.rb
@@ -0,0 +1,11 @@
+class SetPredictionData < ActiveRecord::Migration
+  def change
+    say "Updating all plantings time to first harvest"
+    Planting.all.each(&:update_harvest_days)
+    say "Updating crop median time to first harvest, and lifespan"
+    Crop.all.each do |crop|
+      crop.update_lifespan_medians
+      crop.update_harvest_medians
+    end
+  end
+end
