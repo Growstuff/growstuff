c1836cc78e759663647339f6ca48b27056882f6d
Brenda Wallace <brenda@wallace.net.nz>
2017-11-17 21:23:28 +1300

    Removed un-used method, to reduce class size

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -102,16 +102,6 @@ class Crop < ActiveRecord::Base
     Photo.joins(:harvests).where("harvests.crop_id": id)
   end
 
-  def as_indexed_json(_options = {})
-    as_json(
-      only: [:id, :name, :approval_status],
-      include: {
-        scientific_names: { only: :name },
-        alternate_names: { only: :name }
-      }
-    )
-  end
-
   # update the Elasticsearch index (only if we're using it in this
   # environment)
   def update_index(_name_obj)
