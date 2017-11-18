9e98db423ed6fcdf8aded55ee806990056715104
Brenda Wallace <brenda@wallace.net.nz>
2017-11-18 15:44:45 +1300

    Tidy up photos logic, and test of signin/out on photos#new

--- spec/features/signout_spec.rb
+++ spec/features/signout_spec.rb
@@ -13,17 +13,27 @@ feature "signout" do
     expect(current_path).to eq crops_path
   end
 
-  scenario "after signout, redirect to signin page if page needs authentication" do
-    models = %w[plantings harvests posts photos gardens seeds]
-    models.each do |model|
-      visit "/#{model}/new"
+  shared_examples "sign-in redirects" do |path|
+    scenario "after signout, redirect to signin page if page needs authentication" do
+      visit path
       expect(current_path).to eq new_member_session_path
       fill_in 'Login', with: member.login_name
       fill_in 'Password', with: member.password
       click_button 'Sign in'
-      expect(current_path).to eq "/#{model}/new"
+      expect(current_path).to eq path
       click_link 'Sign out'
       expect(current_path).to eq new_member_session_path
     end
+  end
+
+  let(:path) {}
+  describe 'most models' do
+    let(:garden) { FactoryBot.create :garden, owner: member }
+    include_examples "sign-in redirects", "/plantings/new"
+    include_examples "sign-in redirects", "/harvests/new"
+    include_examples "sign-in redirects", "/posts/new"
+    include_examples "sign-in redirects", "/gardens/new"
+    include_examples "sign-in redirects", "/seeds/new"
+    include_examples "sign-in redirects", "/photos/new?type=garden&id=1"
   end
 end
