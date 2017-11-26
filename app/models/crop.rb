72fb70024f43fe3cb4721e658ae94a250ede7b8b
Brenda Wallace <brenda@wallace.net.nz>
2017-11-26 13:55:40 +1300

    Fix up crop.recent

--- app/models/crop.rb
+++ app/models/crop.rb
@@ -24,7 +24,7 @@ class Crop < ActiveRecord::Base
 
   ##
   ## Scopes
-  scope :recent, -> { approved.reorder("created_at desc") }
+  scope :recent, -> { approved.order(created_at: :desc) }
   scope :toplevel, -> { approved.where(parent_id: nil) }
   scope :popular, -> { approved.reorder("plantings_count desc, lower(name) asc") }
   # ok on sqlite and psql, but not on mysql
