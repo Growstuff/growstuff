37b6e47d105c9312f6c5b12f8bcee148b76f3711
Brenda Wallace <brenda@wallace.net.nz>
2017-11-27 19:12:46 +1300

    Updated shop controller specs

--- spec/controllers/shop_controller_spec.rb
+++ spec/controllers/shop_controller_spec.rb
@@ -1,33 +1,35 @@
 require 'rails_helper'
 
 describe ShopController do
-  before :each do
-    @product1 = FactoryBot.create(:product)
-    @product2 = FactoryBot.create(:product)
-  end
+  let!(:product1) { FactoryBot.create(:product, name: 'aaa') }
+  let!(:product2) { FactoryBot.create(:product, name: 'zzz') }
 
   describe "GET index" do
-    it "assigns all products as @products" do
-      get :index, {}
-      assigns(:products).should eq([@product1, @product2])
-    end
+    describe 'not logged in' do
+      before { get :index, {} }
 
-    it "assigns a new @order_item to build forms" do
-      get :index, {}
-      assigns(:order_item).should be_an_instance_of OrderItem
-    end
+      describe "assigns all products as @products ordered by name" do
+        it { expect(assigns(:products)).to eq([product1, product2]) }
+      end
 
-    it "assigns @order as nil if the user doesn't have one" do
-      get :index, {}
-      assigns(:order).should be_nil
-    end
+      describe "assigns a new @order_item to build forms" do
+        it { expect(assigns(:order_item)).to be_an_instance_of OrderItem }
+      end
 
-    it "assigns @order as current_order if there is one" do
-      @member = FactoryBot.create(:member)
-      sign_in @member
-      @order = FactoryBot.create(:order, member: @member)
-      get :index, {}
-      assigns(:order).should eq @order
+      describe "assigns @order as nil if the user doesn't have one" do
+        it { expect(assigns(:order)).to be_nil }
+      end
+    end
+    describe 'logged in' do
+      describe "assigns @order as current_order if there is one" do
+        let(:member) { FactoryBot.create(:member) }
+        let!(:order) { FactoryBot.create(:order, member: member) }
+        before do
+          sign_in member
+          get :index, {}
+        end
+        it { expect(assigns(:order)).to eq order }
+      end
     end
   end
 end
