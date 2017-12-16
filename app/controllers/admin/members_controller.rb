8a446ecbfc0cc459757a333feaf5338acaef8f34
Brenda Wallace <brenda@wallace.net.nz>
2017-12-17 12:45:19 +1300

    Member admin

---
+++ app/controllers/admin/members_controller.rb
@@ -0,0 +1,12 @@
+class Admin::MembersController < ApplicationController
+  before_action :auth!
+  def index
+    @members = Member.order(:login_name).paginate(page: params[:page])
+  end
+
+  private
+
+  def auth!
+    authorize! :manage, :all
+  end
+end
