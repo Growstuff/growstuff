1bf05f81ab20ffc5cab81e7b4dc0b688b2876375
Brenda Wallace <brenda@wallace.net.nz>
2018-01-07 17:25:16 +1300

    Convert to use chartkick gem
     Conflicts:
    	Gemfile
    	Gemfile.lock
    	config/initializers/assets.rb

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,7 @@ Growstuff::Application.routes.draw do
   get 'crops/search' => 'crops#search', as: 'crops_search'
   resources :crops do
     get 'photos' => 'photos#index'
+    get 'sunniness' => 'crops#sunniness'
   end
 
   resources :comments
