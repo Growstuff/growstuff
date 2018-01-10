5ea7ae8af08976b13ca785545cb890993661127b
Brenda Wallace <brenda@wallace.net.nz>
2018-01-11 09:03:13 +1300

    Added Harvested for chart

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -103,22 +103,28 @@ class CropsController < ApplicationController
   end
 
   def sunniness
-    @crop = Crop.find(params[:crop_id])
-    render json: Planting.where(crop: @crop)
-      .where.not(sunniness: nil)
-      .where.not(sunniness: '')
-      .group(:sunniness).count(:id)
+    pie_chart_query 'sunniness'
   end
 
   def planted_from
+    pie_chart_query 'planted_from'
+  end
+
+  def harvested_for
     @crop = Crop.find(params[:crop_id])
-    render json: Planting.where(crop: @crop)
-      .where.not(planted_from: nil)
-      .where.not(planted_from: '')
-      .group(:planted_from).count(:id)
+    render json: Harvest.joins(:plant_part).where(crop: @crop)
+      .group("plant_parts.name").count(:id)
   end
 
   private
+
+  def pie_chart_query(field)
+    @crop = Crop.find(params[:crop_id])
+    render json: Planting.where(crop: @crop)
+      .where.not(field.to_sym => nil)
+      .where.not(field.to_sym => '')
+      .group(field.to_sym).count(:id)
+  end
 
   def notifier
     case @crop.approval_status
