30f3f18e1c5dfdf085476bf02dbe06ee37f1ec30
Brenda Wallace <brenda@wallace.net.nz>
2018-02-05 12:26:36 +1300

    Don't update flickr meta data on db rollback

--- db/migrate/20180118112809_add_datetaken_to_photos.rb
+++ db/migrate/20180118112809_add_datetaken_to_photos.rb
@@ -1,6 +1,16 @@
 class AddDatetakenToPhotos < ActiveRecord::Migration
-  def change
+  def up
     add_column :photos, :date_taken, :datetime
+    update_flickr_metadata
+  end
+
+  def down
+    add_column :photos, :date_taken
+  end
+
+  private
+
+  def update_flickr_metadata
     # Fetch from flickr, the photos updated the longest ago will be fetched first
     Photo.all.order(:updated_at).each do |photo|
       say "Fetch flickr data for #{photo}"
