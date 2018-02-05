54c880a66cbe535563ad188700568c5b3937a439
Brenda Wallace <brenda@wallace.net.nz>
2018-02-05 15:36:32 +1300

    Removed active merchant

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,16 +91,5 @@ Growstuff::Application.configure do
   config.mapbox_map_id = 'growstuff.i3n2c4ie'
   config.mapbox_access_token = 'pk.eyJ1IjoiZ3Jvd3N0dWZmIiwiYSI6IkdxMkx4alUifQ.n0igaBsw97s14zMa0lwKCA'
 
-  config.after_initialize do
-    ActiveMerchant::Billing::Base.mode = :production
-    paypal_options = {
-      login: ENV['GROWSTUFF_PAYPAL_USERNAME'],
-      password: ENV['GROWSTUFF_PAYPAL_PASSWORD'],
-      signature: ENV['GROWSTUFF_PAYPAL_SIGNATURE']
-    }
-    ::STANDARD_GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(paypal_options)
-    ::EXPRESS_GATEWAY = ActiveMerchant::Billing::PaypalExpressGateway.new(paypal_options)
-  end
-
   config.active_job.queue_adapter = :sidekiq
 end
