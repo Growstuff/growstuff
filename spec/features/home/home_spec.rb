e2cd8dc435ef3c6071af1212fe3d1fb129626409
Brenda Wallace <brenda@wallace.net.nz>
2018-02-16 22:03:40 +1300

    Seeds appear for logged in or not

--- spec/features/home/home_spec.rb
+++ spec/features/home/home_spec.rb
@@ -9,27 +9,33 @@ feature "home page" do
   before { visit root_path }
   subject { page }
 
-  describe 'shows home page' do
+  shared_examples 'shows home page' do
     it { is_expected.to have_text 'View all crops' }
   end
 
-  describe 'shows seeds' do
+  shared_examples 'shows seeds' do
     let!(:tradable_seed) { FactoryBot.create :tradable_seed }
     let!(:finished_seed) { FactoryBot.create :tradable_seed, finished: true }
     let!(:untradable_seed) { FactoryBot.create :untradable_seed }
     describe 'shows tradeable seeds' do
-      it { is_expected.to have_text tradable_seed }
-      it { is_expected.not_to have_text finished_seed }
-      it { is_expected.not_to have_text untradable_seed }
+      it { is_expected.to have_link seed_path(tradable_seed) }
+    end
+    describe "doesn't show untradeable seed" do
+      it { is_expected.not_to have_link seed_path(finished_seed) }
+      it { is_expected.not_to have_link seed_path(untradable_seed) }
     end
     it { is_expected.to have_text 'View all seeds' }
   end
 
   context 'when anonymous' do
+    include_examples 'shows home page'
+    include_examples 'shows seeds'
+    it { is_expected.to have_text 'community of food gardeners' }
   end
 
   context "when signed in" do
     background { login_as member }
-    it { is_expected.to have_text 'Welcome' }
+    include_examples 'shows home page'
+    include_examples 'shows seeds'
   end
 end
