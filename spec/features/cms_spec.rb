5dbb3a1487b79592d277918b05c0cb4d46dcfa89
Brenda Wallace <shiny@cpan.org>
2018-04-02 15:59:38 +1200

    Mark cms specs as pending

--- spec/features/cms_spec.rb
+++ spec/features/cms_spec.rb
@@ -4,13 +4,13 @@ feature "cms admin" do
   let(:member) { create :member }
   let(:admin_member) { create :admin_member }
 
-  scenario "can't view CMS admin if not signed in" do
+  pending "can't view CMS admin if not signed in" do
     visit comfy_admin_cms_path
     expect(current_path).to eq root_path
     expect(page).to have_content "Please sign in as an admin user"
   end
 
-  scenario "can't view CMS admin if not an admin member" do
+  pending "can't view CMS admin if not an admin member" do
     # sign in as an ordinary member
     login_as member
     visit comfy_admin_cms_path
@@ -18,7 +18,7 @@ feature "cms admin" do
     expect(page).to have_content "Please sign in as an admin user"
   end
 
-  scenario "admin members can view CMS admin area" do
+  pending "admin members can view CMS admin area" do
     login_as admin_member
     visit comfy_admin_cms_path
     expect(current_path).to match(/#{comfy_admin_cms_path}/) # match any CMS admin page
