ed3ae8bbd6f45eb9527d36d029a8191c4ff9b456
Brenda Wallace <shiny@cpan.org>
2018-04-28 21:35:00 +1200

    Remove some old currency methods from application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,7 @@
 module ApplicationHelper
-  def price_in_dollars(price)
-    format('%.2f', price / 100.0)
-  end
-
-  # 999 cents becomes 9.99 AUD -- for products/orders/etc
-  def price_with_currency(price)
-    format('%.2f %s', price / 100.0, Growstuff::Application.config.currency)
-  end
-
   def parse_date(str)
     str ||= '' # Date.parse barfs on nil
     str == '' ? nil : Date.parse(str)
-  end
-
-  def forex_link(price)
-    pid = price_in_dollars(price)
-    currency = Growstuff::Application.config.currency
-    link = "http://www.wolframalpha.com/input/?i=#{pid}+#{currency}"
-
-    link_to "(convert)", link, target: "_blank", rel: "noopener noreferrer"
   end
 
   def build_alert_classes(alert_type = :info)
