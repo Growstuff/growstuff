f67e8ceb958ff16e3ffa2ba4e5a9cba63ba16612
jenkr55 <jenkr55@gmail.com>
2018-05-28 12:29:44 -0500

    Dont allow newlines

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -23,7 +23,7 @@ class Garden < ActiveRecord::Base
 
   validates :name,
     format: {
-      with: /\A[\p{L}\p{M}\d\s[-]]*\z/
+      with: /\A((?!\\n)[\p{L}\p{M}\d\s[:ascii:](?!\\n)])*\z/
     },
     allow_nil: false,
     allow_blank: false,
