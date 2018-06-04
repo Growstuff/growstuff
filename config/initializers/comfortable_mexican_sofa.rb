3399a794b2f739702c148015db5a5dbb44559efb
Brenda Wallace <shiny@cpan.org>
2018-06-04 16:55:29 +1200

    Ignore one non-ascii comment

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -55,9 +55,11 @@ ComfortableMexicanSofa.configure do |config|
   # object you want to keep. Set it to 0 if you wish to turn this feature off.
   #   config.revisions_limit = 25
 
+  # rubocop:disable Style/AsciiComments
   # Locale definitions. If you want to define your own locale merge
   # {:locale => 'Locale Title'} with this.
   #   config.locales = {:en => 'English', :es => 'Español'}
+  # rubocop:enable Style/AsciiComments
 
   # Admin interface will respect the locale of the site being managed. However you can
   # force it to English by setting this to `:en`
