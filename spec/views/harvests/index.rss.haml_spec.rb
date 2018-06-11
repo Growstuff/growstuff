f9bdae77d3fc670711de293aedc1beb19fd16f1c
robotscissors <cjbazin@gmail.com>
2018-06-11 10:38:26 -0700

    add tests for harvests/index.rss

--- spec/views/harvests/index.rss.haml_spec.rb
+++ spec/views/harvests/index.rss.haml_spec.rb
@@ -7,47 +7,36 @@ describe 'harvests/index.rss.haml' do
     @tomato = FactoryBot.create(:tomato)
     @maize  = FactoryBot.create(:maize)
     @pp = FactoryBot.create(:plant_part)
+    page = 1
+    per_page = 2
+    total_entries = 2
+    harvests = WillPaginate::Collection.create(page, per_page, total_entries) do |pager|
+      pager.replace([
+                      FactoryBot.create(:harvest,
+                        crop: @tomato,
+                        owner: @member),
+                      FactoryBot.create(:harvest,
+                        crop: @maize,
+                        plant_part: @pp,
+                        owner: @member,
+                        quantity: 2)
+                    ])
+    end
     assign(:harvests, harvests)
     render
   end
 
-  context 'all harvests' do
-    before(:each) do
-      #assign(:harvest, FactoryBot.create(:harvest))
-      render
-    end
-
-    it 'shows RSS feed title' do
-      rendered.should have_content "Recent harvests from all members"
-    end
-
-    it 'item title shows owner and crop' do
-      rendered.should have_content "#{@harvest.crop}"
-    end
-
-    # it 'shows formatted content of posts' do
-    #   rendered.should have_content "This is a <em>really</em> good plant."
-    # end
-    #
-    # it 'shows sunniness' do
-    #   rendered.should have_content 'Sunniness: sun'
-    # end
-    #
-    # it 'shows propagation method' do
-    #   rendered.should have_content 'Planted from: seedling'
-    # end
+  it 'shows RSS feed title' do
+    rendered.should have_content "Recent harvests from all members"
   end
 
-  # context "one person's plantings" do
-  #   before :each do
-  #     @planting = FactoryBot.create(:planting)
-  #     assign(:plantings, [@planting])
-  #     assign(:owner, @planting.owner)
-  #     render
-  #   end
-  #
-  #   it 'shows title for single member' do
-  #     rendered.should have_content "Recent plantings from #{@planting.owner}"
-  #   end
-  # end
+  it "displays crop's name in title" do
+    assign(:crop, @tomato)
+    render
+    rendered.should have_content "#{@tomato.name}"
+  end
+
+  it 'shows formatted content of harvest posts' do
+    rendered.should have_content "<p>Quantity: "
+  end
 end
