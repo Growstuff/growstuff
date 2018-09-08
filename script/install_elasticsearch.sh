a05152335471a3b5193a31393b904dbd0ee9ebc9
Brenda Wallace <brenda@wallace.net.nz>
2018-09-09 10:56:21 +1200

    Upgrade to elasticsearch 6.2.3

--- script/install_elasticsearch.sh
+++ script/install_elasticsearch.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 if [ "${GROWSTUFF_ELASTICSEARCH}" = "true" ]; then
+  VERSION="6.2.3"
   set -euv
   sudo dpkg -r elasticsearch
-  curl -O https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.4.0/elasticsearch-2.4.0.deb
-  sudo dpkg -i --force-confnew elasticsearch-2.4.0.deb
+  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${VERSION}.deb
+  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${VERSION}.deb.sha512
+  shasum -a 512 -c elasticsearch-${VERSION}.deb.sha512
+  sudo dpkg -i elasticsearch-${VERSION}.deb
+
   sudo service elasticsearch start
   sleep 10
   curl -v localhost:9200
