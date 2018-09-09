31744491ea46d1ca57a3dccd27c532101bf2464a
Awesome Code <team@xinminlabs.com>
2018-09-09 21:53:57 +0000

    Auto corrected by following Ruby FactoryBot/AttributeDefinedStatically

--- spec/factories/garden.rb
+++ spec/factories/garden.rb
@@ -1,24 +1,24 @@
 FactoryBot.define do
   factory :garden do
     name { Faker::Vehicle.vin }
-    description "This is a **totally** cool garden"
+    description { "This is a **totally** cool garden" }
     owner
-    active true
-    area 23
-    area_unit "acre"
-    location "Greenwich, UK"
+    active { true }
+    area { 23 }
+    area_unit { "acre" }
+    location { "Greenwich, UK" }
 
     factory :inactive_garden do
-      active false
+      active { false }
     end
 
     # the following are used for testing alphabetical ordering
     factory :garden_a do
-      name 'A garden starting with A'
+      name { 'A garden starting with A' }
     end
 
     factory :garden_z do
-      name 'Zzzz this garden makes me sleepy'
+      name { 'Zzzz this garden makes me sleepy' }
     end
   end
 end
--- spec/factories/planting.rb
+++ spec/factories/planting.rb
@@ -4,37 +4,37 @@ FactoryBot.define do
     garden { FactoryBot.create :garden, owner: owner }
     crop
     planted_at { Time.zone.local(2014, 7, 30) }
-    quantity 33
-    description "This is a *really* good plant."
-    finished false
-    finished_at nil
+    quantity { 33 }
+    description { "This is a *really* good plant." }
+    finished { false }
+    finished_at { nil }
 
     factory :seed_planting do
-      planted_from 'seed'
+      planted_from { 'seed' }
     end
 
     factory :seedling_planting do
-      planted_from 'seedling'
+      planted_from { 'seedling' }
     end
 
     factory :cutting_planting do
-      planted_from 'cutting'
+      planted_from { 'cutting' }
     end
 
     factory :sunny_planting do
-      sunniness 'sun'
+      sunniness { 'sun' }
     end
 
     factory :semi_shady_planting do
-      sunniness 'semi-shade'
+      sunniness { 'semi-shade' }
     end
 
     factory :shady_planting do
-      sunniness 'shade'
+      sunniness { 'shade' }
     end
 
     factory :finished_planting do
-      finished true
+      finished { true }
       planted_at { Time.zone.local(2014, 7, 30) }
       finished_at { Time.zone.local(2014, 8, 30) }
     end
