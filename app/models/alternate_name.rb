0d3588a2b25f4680aa51ca0d44c30b78d7a82fc0
Brenda Wallace <brenda@wallace.net.nz>
2018-10-16 12:47:05 +1300

    Add another inverse_of created_alternate_names

--- app/models/alternate_name.rb
+++ app/models/alternate_name.rb
@@ -1,7 +1,7 @@
 class AlternateName < ApplicationRecord
   after_commit { |an| an.crop.__elasticsearch__.index_document if an.crop && ENV['GROWSTUFF_ELASTICSEARCH'] == "true" }
   belongs_to :crop
-  belongs_to :creator, class_name: 'Member'
+  belongs_to :creator, class_name: 'Member', inverse_of: :created_alternate_names
   validates :name, presence: true
   validates :crop, presence: true
 end
