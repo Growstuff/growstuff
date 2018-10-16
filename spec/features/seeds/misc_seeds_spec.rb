fb156ec43894620d667cc127debaf92db1482497
Brenda Wallace <brenda@wallace.net.nz>
2018-10-16 14:20:15 +1300

    Updating edit seeds spec

--- spec/features/seeds/misc_seeds_spec.rb
+++ spec/features/seeds/misc_seeds_spec.rb
@@ -1,31 +1,38 @@
 require 'rails_helper'
 
 feature "seeds", js: true do
+  let(:member) { create :member }
+
   context "signed in user" do
-    let(:member) { create :member }
     let(:crop) { create :crop }
 
     background { login_as member }
 
-    scenario "button on index to edit seed" do
-      seed = create :seed, owner: member
-      visit seeds_path
-      click_link "edit_seed_glyphicon"
-      expect(current_path).to eq edit_seed_path(seed)
-      expect(page).to have_content 'Editing seeds'
+    describe "button on index to edit seed" do
+      let!(:seed) { create :seed, owner: member }
+      before do
+        visit seeds_path
+        click_link "edit_seed_glyphicon"
+      end
+      it { expect(current_path).to eq edit_seed_path(seed) }
+      it { expect(page).to have_content 'Editing seeds' }
     end
 
-    scenario "button on front page to add seeds" do
-      visit root_path
-      click_link "Add seeds"
-      expect(current_path).to eq new_seed_path
-      expect(page).to have_content 'Add seeds'
+    describe "button on front page to add seeds" do
+      before do
+        visit root_path
+        click_link "Add seeds"
+      end
+      it { expect(current_path).to eq new_seed_path }
+      it { expect(page).to have_content 'Add seeds' }
     end
 
-    scenario "Clicking link to owner's profile" do
-      visit seeds_by_owner_path(member)
-      click_link "View #{member}'s profile >>"
-      expect(current_path).to eq member_path(member)
+    describe "Clicking link to owner's profile" do
+      before do
+        visit seeds_by_owner_path(member)
+        click_link "View #{member}'s profile >>"
+      end
+      it { expect(current_path).to eq member_path(member) }
     end
 
     # actually adding seeds is in spec/features/seeds_new_spec.rb
@@ -40,35 +47,36 @@ feature "seeds", js: true do
       expect(current_path).to eq seed_path(seed)
     end
 
-    scenario "delete seeds" do
-      seed = create :seed, owner: member
-      visit seed_path(seed)
-      click_link 'Delete'
-      expect(current_path).to eq seeds_path
+    describe "delete seeds" do
+      let(:seed) { FactoryBot.create :seed, owner: member }
+      before do
+        visit seed_path(seed)
+        click_link 'Delete'
+      end
+      it { expect(current_path).to eq seeds_path }
     end
 
-    scenario "view seeds with max and min days until maturity" do
-      seed = create :seed, days_until_maturity_min: 5, days_until_maturity_max: 7
-      visit seed_path(seed)
-      expect(page).to have_content "Days until maturity:\n5–7"
-    end
+    describe '#show' do
+      before { visit seed_path(seed) }
+      describe "view seeds with max and min days until maturity" do
+        let(:seed) { FactoryBot.create :seed, days_until_maturity_min: 5, days_until_maturity_max: 7 }
+        it { expect(page).to have_content "Days until maturity:\n5–7" }
+      end
 
-    scenario "view seeds with only max days until maturity" do
-      seed = create :seed, days_until_maturity_max: 7
-      visit seed_path(seed)
-      expect(page).to have_content "Days until maturity: 7"
-    end
+      describe "view seeds with only max days until maturity" do
+        let(:seed) { FactoryBot.create :seed, days_until_maturity_max: 7 }
+        it { expect(page).to have_content "Days until maturity:\n7" }
+      end
 
-    scenario "view seeds with only min days until maturity" do
-      seed = create :seed, days_until_maturity_min: 5
-      visit seed_path(seed)
-      expect(page).to have_content "Days until maturity: 5"
-    end
+      describe "view seeds with only min days until maturity" do
+        let(:seed) { FactoryBot.create :seed, days_until_maturity_min: 5 }
+        it { expect(page).to have_content "Days until maturity:\n5" }
+      end
 
-    scenario "view seeds with neither max nor min days until maturity" do
-      seed = create :seed
-      visit seed_path(seed)
-      expect(page).to have_content "Days until maturity: unknown"
+      describe "view seeds with neither max nor min days until maturity" do
+        let(:seed) { FactoryBot.create :seed }
+        it { expect(page).to have_content "Days until maturity:\nunknown" }
+      end
     end
   end
 end
