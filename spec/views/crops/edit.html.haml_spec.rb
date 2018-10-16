918f73d0f029942e26f9baaf6da205285bec4808
Brenda Wallace <brenda@wallace.net.nz>
2018-10-16 14:29:01 +1300

    Update crop edit spec

--- spec/views/crops/edit.html.haml_spec.rb
+++ spec/views/crops/edit.html.haml_spec.rb
@@ -14,6 +14,6 @@ describe "crops/edit" do
   end
 
   it "shows the creator" do
-    rendered.should have_content "Added by #{@crop.creator} less than a minute ago."
+    expect(rendered).to have_content "Added by\n#{@crop.creator}\nless than a minute\nago."
   end
 end
