92939ca2043a63f0f50a1667b8ca2824b4dace98
Brenda Wallace <brenda@wallace.net.nz>
2018-10-16 14:08:37 +1300

    Spec updates

--- spec/views/scientific_names/edit.html.haml_spec.rb
+++ spec/views/scientific_names/edit.html.haml_spec.rb
@@ -2,21 +2,23 @@ require 'rails_helper'
 
 describe "scientific_names/edit" do
   context "logged in" do
+    let(:member) { FactoryBot.create(:member) }
+    let(:scientific_name) { FactoryBot.create(:zea_mays, creator: member) }
+
     before(:each) do
-      @member = FactoryBot.create(:member)
-      sign_in @member
-      controller.stub(:current_user) { @member }
-      @scientific_name = assign(:scientific_name,
-        FactoryBot.create(:zea_mays))
+      sign_in member
+      controller.stub(:current_user) { member }
+      assign(:scientific_name, scientific_name)
       render
     end
 
     it "shows the creator" do
-      rendered.should have_content "Added by #{@scientific_name.creator} less than a minute ago."
+      expect(rendered).to have_content "Added by\n#{member}\nless than a minute\nago."
     end
+    it { expect(rendered).to have_link member }
 
     it "renders the edit scientific_name form" do
-      assert_select "form", action: scientific_names_path(@scientific_name), method: "post" do
+      assert_select "form", action: scientific_names_path(scientific_name), method: "post" do
         assert_select "input#scientific_name_name", name: "scientific_name[scientific_name]"
         assert_select "select#scientific_name_crop_id", name: "scientific_name[crop_id]"
       end
