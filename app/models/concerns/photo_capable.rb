510124758d50c1278f59c933abda7f54e3a53151
Brenda Wallace <brenda@wallace.net.nz>
2018-10-16 13:26:54 +1300

    Inverse relation ships

--- app/models/concerns/photo_capable.rb
+++ app/models/concerns/photo_capable.rb
@@ -2,7 +2,7 @@ module PhotoCapable
   extend ActiveSupport::Concern
 
   included do
-    has_many :photographings, as: :photographable, dependent: :destroy
+    has_many :photographings, as: :photographable, dependent: :destroy, inverse_of: :photographable
     has_many :photos, through: :photographings, as: :photographable
 
     scope :has_photos, -> { includes(:photos).where.not(photos: { id: nil }) }
