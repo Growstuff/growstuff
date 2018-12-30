961f69d09535ad68c17a83f821ca1b066ad64426
Brenda Wallace <brenda@wallace.net.nz>
2018-12-30 22:49:41 +1300

    Further dry of scientific names controller
    and tests too

--- app/controllers/scientific_names_controller.rb
+++ app/controllers/scientific_names_controller.rb
@@ -4,49 +4,35 @@ class ScientificNamesController < ApplicationController
   respond_to :html, :json
   responders :flash
 
-  # GET /scientific_names
-  # GET /scientific_names.json
   def index
-    @scientific_names = ScientificName.all.order(:name)
+    @scientific_names = ScientificName.all.order(:name).paginate(page: params[:page])
     respond_with(@scientific_names)
   end
 
-  # GET /scientific_names/1
-  # GET /scientific_names/1.json
   def show
     respond_with(@scientific_name)
   end
 
-  # GET /scientific_names/new
-  # GET /scientific_names/new.json
   def new
-    @scientific_name = ScientificName.new
-    @crop = Crop.find_or_initialize_by(id: params[:crop_id])
+    @crop = Crop.find(params[:crop_id])
+
     respond_with(@scientific_name)
   end
 
-  # GET /scientific_names/1/edit
   def edit; end
 
-  # POST /scientific_names
-  # POST /scientific_names.json
   def create
-    @scientific_name = ScientificName.new(scientific_name_params)
     @scientific_name.creator = current_member
-
     @scientific_name.save
+
     respond_with(@scientific_name.crop)
   end
 
-  # PUT /scientific_names/1
-  # PUT /scientific_names/1.json
   def update
     @scientific_name.update(scientific_name_params)
     respond_with(@scientific_name.crop)
   end
 
-  # DELETE /scientific_names/1
-  # DELETE /scientific_names/1.json
   def destroy
     @crop = @scientific_name.crop
     @scientific_name.destroy
--- spec/controllers/scientific_names_controller_spec.rb
+++ spec/controllers/scientific_names_controller_spec.rb
@@ -4,15 +4,32 @@ describe ScientificNamesController do
   login_member(:crop_wrangling_member)
 
   let!(:crop) { FactoryBot.create(:tomato) }
-
-  def valid_attributes
-    { name: 'Solanum lycopersicum', crop_id: crop.id }
-  end
+  let!(:sci_name) { FactoryBot.create :scientific_name, name: 'aaa' }
 
   describe "GET new" do
-    it "assigns crop if specified" do
-      get :new, params: { crop_id: crop.id }
-      assigns(:crop).should be_an_instance_of Crop
+    before { get :new, params: { crop_id: crop.id } }
+    it { expect(assigns(:crop)).to eq crop }
+    it { expect(assigns(:scientific_name)).to be_an_instance_of ScientificName }
+  end
+
+  describe 'GET index' do
+    let!(:sci_name_last) { FactoryBot.create :scientific_name, name: 'zzz' }
+    before { get :index }
+
+    it "sorts by name" do
+      expect(assigns(:scientific_names)).to eq [sci_name, sci_name_last]
     end
+  end
+
+  describe 'POST create' do
+    subject { post :create, params: { scientific_name: { name: 'sciencey name', crop_id: crop.id } } }
+    it { expect { subject }.to change { ScientificName.count }.by(1) }
+    it { expect(subject.request.flash[:notice]).to_not be_nil }
+  end
+
+  describe 'DELETE destroy' do
+    subject { delete :destroy, params: { id: sci_name.id } }
+    it { expect { subject }.to change { ScientificName.count }.by(-1) }
+    it { expect(subject.request.flash[:notice]).to_not be_nil }
   end
 end
