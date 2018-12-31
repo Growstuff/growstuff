3e626a7a4292d642760c1d16052721cedf34c978
Brenda Wallace <brenda@wallace.net.nz>
2018-12-31 21:24:56 +1300

    Removing extra db fetches from posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,54 +4,40 @@ class PostsController < ApplicationController
   respond_to :html, :json
   respond_to :rss, only: %i(index show)
 
-  # GET /posts
-  # GET /posts.json
-  # GET /posts.rss
+  responders :flash
+
   def index
     @author = Member.find_by(slug: params[:author])
-    @posts = posts
+    @posts = @posts.where author: @author if @author.present?
+    @posts = @posts.order(created_at: :desc).includes(:author, comments: :author).paginate(page: params[:page])
     respond_with(@posts)
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
-  # GET /posts/1.rss
   def show
-    @post = Post.includes(:author, comments: :author).find(params[:id])
     respond_with(@post)
   end
 
-  # GET /posts/new
-  # GET /posts/new.json
   def new
-    @post = Post.new
-    @forum = Forum.find_by(id: params[:forum_id])
+    @forum = Forum.find(params[:forum_id])
     respond_with(@post)
   end
 
-  # GET /posts/1/edit
   def edit; end
 
-  # POST /posts
-  # POST /posts.json
   def create
-    params[:post][:author_id] = current_member.id
     @post = Post.new(post_params)
-    flash[:notice] = 'Post was successfully created.' if @post.save
+    @post.author = current_member
+    @post.save
     respond_with(@post)
   end
 
-  # PUT /posts/1
-  # PUT /posts/1.json
   def update
-    flash[:notice] = 'Post was successfully updated.' if @post.update(post_params)
+    @post.update(post_params)
     respond_with(@post)
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
-    flash[:notice] = 'Post was deleted.' if @post.destroy
+    @post.destroy
     respond_with(@post)
   end
 
@@ -59,13 +45,5 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:body, :subject, :author_id, :forum_id)
-  end
-
-  def posts
-    if @author
-      @author.posts
-    else
-      Post
-    end.order(created_at: :desc).includes(:author, comments: :author).paginate(page: params[:page])
   end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,29 +1,92 @@
 require 'rails_helper'
 
 describe PostsController do
-  login_member
+  let(:member) { FactoryBot.create :member }
+  shared_examples 'RSS feeds' do
+    describe "RSS feed" do
+      describe "returns an RSS feed" do
+        before { get :index, format: "rss" }
+        it { expect(response).to be_success }
+        it { expect(response).to render_template("posts/index") }
+        it { expect(response.content_type).to eq("application/rss+xml") }
+      end
+    end
 
-  def valid_attributes
-    member = FactoryBot.create(:member)
-    { author_id: member.id, subject: "blah", body: "blah blah" }
-  end
-
-  describe "GET RSS feed" do
-    it "returns an RSS feed" do
-      get :index, format: "rss"
-      expect(response).to be_success
-      expect(response).to render_template("posts/index")
-      expect(response.content_type).to eq("application/rss+xml")
+    describe "GET RSS feed for individual post" do
+      describe "returns an RSS feed" do
+        let(:post) { FactoryBot.create :post }
+        before { get :show, format: "rss", params: { id: post.slug } }
+        it { expect(response).to be_success }
+        it { expect(response).to render_template("posts/show") }
+        it { expect(response.content_type).to eq("application/rss+xml") }
+      end
     end
   end
 
-  describe "GET RSS feed for individual post" do
-    it "returns an RSS feed" do
-      post = Post.create! valid_attributes
-      get :show, format: "rss", params: { id: post.slug }
-      expect(response).to be_success
-      expect(response).to render_template("posts/show")
-      expect(response.content_type).to eq("application/rss+xml")
+  shared_examples 'view posts' do
+    describe 'view posts' do
+      let!(:post) { FactoryBot.create :post }
+      let!(:post_by_member) { FactoryBot.create :post, author: member }
+      describe '#index' do
+        describe 'default' do
+          before { get :index }
+          it { expect(assigns(:posts)).to eq [post_by_member, post] }
+        end
+        describe 'by author' do
+          before { get :index, params: { author: member.slug } }
+          it { expect(assigns(:posts)).to eq [post_by_member] }
+          it { expect(assigns(:author)).to eq member }
+        end
+      end
+      describe '#show' do
+        before { get :show, params: { id: post.id } }
+        it { expect(assigns(:post)).to eq post }
+      end
+    end
+  end
+
+  context 'not logged in' do
+    include_examples 'RSS feeds'
+    include_examples 'view posts'
+  end
+  context 'logged in' do
+    before(:each) do
+      sign_in member
+      # controller.stub(:current_member) { member }
+    end
+
+    include_examples 'RSS feeds'
+    include_examples 'view posts'
+
+    describe '#new' do
+      before { get :new }
+      it { expect(assigns(:post)).to be_an_instance_of Post }
+    end
+
+    describe '#create' do
+      subject { post :create, params: { post: { subject: 'cool story bro', body: 'then i found $10' } } }
+      it { expect { subject }.to change { Post.count }.by(1) }
+      it { expect(subject.request.flash[:notice]).to_not be_nil }
+    end
+
+    describe 'modifying data' do
+      let!(:post) { FactoryBot.create :post, author: member }
+      describe '#edit' do
+        before { get :edit, params: { id: post.id } }
+        it { expect(assigns(:post)).to eq post }
+      end
+
+      describe '#update' do
+        subject { put :update, params: { id: post.id, post: { subject: 'cool story bro', body: 'always blow on the pie' } } }
+        it { expect { subject }.to change { post.reload; post.body }.to 'always blow on the pie' }
+        it { expect(subject.request.flash[:notice]).to_not be_nil }
+      end
+
+      describe '#destroy' do
+        subject { delete :destroy, params: { id: post.id } }
+        it { expect { subject }.to change { Post.count }.by(-1) }
+        it { expect(subject.request.flash[:notice]).to_not be_nil }
+      end
     end
   end
 end
