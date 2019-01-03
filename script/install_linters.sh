12472d3762336bfc61bf1908d53a1bccce7300d2
Brenda Wallace <brenda@wallace.net.nz>
2019-01-03 14:41:11 +1300

    Return to previous version

--- script/install_linters.sh
+++ script/install_linters.sh
@@ -2,12 +2,9 @@
 
 if [ "${STATIC_CHECKS}" = "true" ]; then
   set -euv
-
-  rvm use 2.5.3; #overcommit doesn't support 2.6.0
   npm install;
 
   gem install --update overcommit haml-lint bundler-audit;
-  gem install childprocess 0.7.0
 
   pip install --upgrade pip;
   pip install yamllint --user;
