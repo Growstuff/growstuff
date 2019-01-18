656510855dee33c969bc02ccd4e36d928c1b463d
Brenda Wallace <brenda@wallace.net.nz>
2019-01-18 14:20:15 +1300

    Switch to new syntax for initing uglifier

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
