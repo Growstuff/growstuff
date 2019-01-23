001c0251b2237a74ba0a580cca6983a1e2cf4049
Brenda Wallace <brenda@wallace.net.nz>
2019-01-23 17:34:32 +1300

    DRY the icons

---
+++ app/helpers/icons_helper.rb
@@ -0,0 +1,17 @@
+module IconsHelper
+  def garden_icon
+    icon('far', 'square')
+  end
+
+  def planting_icon
+    icon('fas', 'seedling')
+  end
+
+  def harvest_icon
+    icon('fas', 'carrot')
+  end
+
+  def seed_icon
+    icon('fas', 'heart')
+  end
+end
