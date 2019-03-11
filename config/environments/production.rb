59b3a68fc34ebf1649616be4a22f7c03d9e0ca15
Brenda Wallace <brenda@wallace.net.nz>
2019-03-11 17:05:25 +1300

    Configure memcachier for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,15 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {username: ENV["MEMCACHIER_USERNAME"],
+                     password: ENV["MEMCACHIER_PASSWORD"],
+                     failover: true,
+                     socket_timeout: 1.5,
+                     socket_failure_delay: 0.2,
+                     down_retry_delay: 60
+                    }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
