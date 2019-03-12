3b9c9e0725b1f2880d0b31a2819f4d5a56538c2a
Brenda Wallace <brenda@wallace.net.nz>
2019-03-13 11:29:44 +1300

    More spec tidy

--- spec/features/unsubscribing_spec.rb
+++ spec/features/unsubscribing_spec.rb
@@ -7,21 +7,27 @@ describe "unsubscribe" do
 
   before { clear_emails }
 
-  it "from planting reminder mailing list" do
+  describe "from planting reminder mailing list" do
     # verifying the initial subscription status of the member
-    expect(member.send_planting_reminder).to eq(true)
-    expect(member.send_notification_email).to eq(true)
+    it { expect(member.send_planting_reminder).to eq(true) }
+    it { expect(member.send_notification_email).to eq(true) }
+    
+    context 'when i generate planting reminder email' do
+      before do
+        Notifier.planting_reminder(member).deliver_now
+        open_email(member.email)
+      end
 
-    # generate planting reminder email
-    Notifier.planting_reminder(member).deliver_now
-    open_email(member.email)
-
-    # clicking 'Unsubscribe' link will unsubscribe the member
-    current_email.click_link 'Unsubscribe from planting reminders'
-    expect(page).to have_content "You have been unsubscribed from planting reminders"
-    updated_member = Member.find(member.id) # reload the member
-    expect(updated_member.send_planting_reminder).to eq(false)
-    expect(updated_member.send_notification_email).to eq(true)
+      context 'and then click 'Unsubscribe' link to unsubscribe the member' do
+        before { current_email.click_link 'Unsubscribe from planting reminders' }
+        it { expect(page).to have_content "You have been unsubscribed from planting reminders"}
+        context 'then reload the member' do
+          before { updated_member = Member.find(member.id) }
+          it { expect(updated_member.send_planting_reminder).to eq(false) }
+          it { expect(updated_member.send_notification_email).to eq(true) }
+        end
+      end
+    end
   end
 
   it "from inbox notification mailing list" do
