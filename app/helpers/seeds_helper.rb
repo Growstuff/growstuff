2bc12570c422297958632daa1a9089dcc57d8e53
Brenda Wallace <brenda@wallace.net.nz>
2019-03-27 22:38:28 +1300

    Consistent display of seeds/harvests

--- app/helpers/seeds_helper.rb
+++ app/helpers/seeds_helper.rb
@@ -1,8 +1,13 @@
 module SeedsHelper
-  def display_seed_description(seed)
-    if seed.description.nil?
-      "no description provided."
+  def display_seed_quantity(seed)
+    if seed.quantity.nil?
+      'seeds'
     else
+      pluralize(seed.quantity, 'seed')
+    end
+  end
+  def display_seed_description(seed)
+    if seed.description.present?
       truncate(seed.description, length: 130, separator: ' ', omission: '... ') { link_to "Read more", seed_path(seed) }
     end
   end
