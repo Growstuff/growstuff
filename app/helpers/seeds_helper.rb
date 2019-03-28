e4d4cdc3fa73625c538ab53f5ec9aa4678f7ad5d
Brenda Wallace <brenda@wallace.net.nz>
2019-03-29 10:45:51 +1300

    Fix up seed description helper

--- app/helpers/seeds_helper.rb
+++ app/helpers/seeds_helper.rb
@@ -9,6 +9,8 @@ module SeedsHelper
   def display_seed_description(seed)
     if seed.description.present?
       truncate(seed.description, length: 130, separator: ' ', omission: '... ') { link_to "Read more", seed_path(seed) }
+    else
+      ''
     end
   end
 end
