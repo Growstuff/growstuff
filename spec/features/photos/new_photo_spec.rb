795283aedf1992eb21ec7fd974f4c74f45b96f47
Awesome Code <team@xinminlabs.com>
2019-03-30 20:25:15 +0000

    Auto corrected by following Lint Ruby Capybara/FeatureMethods

--- spec/features/photos/new_photo_spec.rb
+++ spec/features/photos/new_photo_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-feature "new photo page" do
+describe "new photo page" do
   let(:photo) { FactoryBot.create :photo }
 
   context "signed in member" do
     let(:member) { FactoryBot.create :member }
 
-    background { login_as member }
+    before { login_as member }
 
     context "viewing a planting" do
       let(:planting) { FactoryBot.create :planting, owner: member }
 
-      scenario "add photo" do
+      it "add photo" do
         visit planting_path(planting)
         within '.planting-actions' do
           click_link('Add photo')
@@ -23,7 +23,7 @@ feature "new photo page" do
     context "viewing a harvest" do
       let(:harvest) { FactoryBot.create :harvest, owner: member }
 
-      scenario "add photo" do
+      it "add photo" do
         visit harvest_path(harvest)
         within '.harvest-actions' do
           click_link "Add photo"
@@ -35,7 +35,7 @@ feature "new photo page" do
     context "viewing a garden" do
       let(:garden) { FactoryBot.create :garden, owner: member }
 
-      scenario "add photo" do
+      it "add photo" do
         visit garden_path(garden)
         within '.garden-actions' do
           click_link "Add photo"
@@ -47,7 +47,7 @@ feature "new photo page" do
     describe "viewing a seed" do
       let(:seed) { FactoryBot.create :seed, owner: member }
 
-      scenario "add photo" do
+      it "add photo" do
         visit seed_path(seed)
         first('.seed-actions').click_link('Add photo')
         expect(page).to have_text seed.to_s
--- spec/features/plantings/planting_a_crop_spec.rb
+++ spec/features/plantings/planting_a_crop_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 require 'custom_matchers'
 
-feature "Planting a crop", :js, :elasticsearch do
+describe "Planting a crop", :js, :elasticsearch do
   let(:member) { FactoryBot.create :member                }
   let!(:maize) { FactoryBot.create :maize                 }
   let(:garden) { FactoryBot.create :garden, owner: member }
@@ -9,7 +9,7 @@ feature "Planting a crop", :js, :elasticsearch do
     FactoryBot.create :planting, garden: garden, owner: member, planted_at: Date.parse("2013-03-10")
   end
 
-  background do
+  before do
     login_as member
     visit new_planting_path
     sync_elasticsearch [maize]
@@ -32,7 +32,7 @@ feature "Planting a crop", :js, :elasticsearch do
     it { expect(page).to have_optional 'input#planting_finished_at' }
   end
 
-  scenario "Creating a new planting" do
+  it "Creating a new planting" do
     fill_autocomplete "crop", with: "mai"
     select_from_autocomplete "maize"
     within "form#new_planting" do
@@ -48,7 +48,7 @@ feature "Planting a crop", :js, :elasticsearch do
     expect(page).to have_content "Not enough data"
   end
 
-  scenario "Clicking link to owner's profile" do
+  it "Clicking link to owner's profile" do
     visit member_plantings_path(member)
     click_link "View #{member}'s profile >>"
     expect(current_path).to eq member_path(member)
@@ -151,7 +151,7 @@ feature "Planting a crop", :js, :elasticsearch do
     end
   end
 
-  scenario "Planting from crop page" do
+  it "Planting from crop page" do
     visit crop_path(maize)
     within '.crop-actions' do
       click_link "Plant maize"
@@ -165,7 +165,7 @@ feature "Planting a crop", :js, :elasticsearch do
     expect(page).to have_content "maize"
   end
 
-  scenario "Editing a planting to add details" do
+  it "Editing a planting to add details" do
     visit planting_path(planting)
     click_link "Edit"
     fill_in "Tell us more about it", with: "Some extra notes"
@@ -173,7 +173,7 @@ feature "Planting a crop", :js, :elasticsearch do
     expect(page).to have_content "planting was successfully updated"
   end
 
-  scenario "Editing a planting to fill in the finished date" do
+  it "Editing a planting to fill in the finished date" do
     visit planting_path(planting)
     expect(page).to have_content "Not enough data"
     click_link "Edit"
@@ -184,7 +184,7 @@ feature "Planting a crop", :js, :elasticsearch do
     expect(page).not_to have_content "Not enough data"
   end
 
-  scenario "Marking a planting as finished" do
+  it "Marking a planting as finished" do
     fill_autocomplete "crop", with: "mai"
     select_from_autocomplete "maize"
     within "form#new_planting" do
