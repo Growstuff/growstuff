9a8ea7ee565c9ccab04379c3739f14498cc314ef
Awesome Code <team@xinminlabs.com>
2019-03-30 20:26:15 +0000

    Auto corrected by following Lint Ruby RSpec/AlignRightLetBrace

--- spec/controllers/garden_types_controller_spec.rb
+++ spec/controllers/garden_types_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe GardenTypesController, type: :controller do
   include Devise::Test::ControllerHelpers
   let(:valid_params) { { name: 'My second GardenType' } }
-  let(:garden_type)  { FactoryBot.create :garden_type }
+  let(:garden_type)  { FactoryBot.create :garden_type   }
 
   let(:member) { FactoryBot.create(:member) }
   let(:admin_member) { FactoryBot.create(:admin) }
