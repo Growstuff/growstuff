55e89e95fee3ea9814f50e1fc4f54463a57ac32d
Brenda Wallace <brenda@wallace.net.nz>
2019-04-08 13:21:54 +1200

    Fix a deprecation warning

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :photo do
     owner
     flickr_photo_id { 1 }
-    title { Faker::HarryPotter.quote }
+    title { Faker::Movies::HarryPotter.quote }
     license_name { "CC-BY" }
     license_url { "http://example.com/license.html" }
     thumbnail_url { "http://example.com/#{Faker::File.file_name}.jpg" }
