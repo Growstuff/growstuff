54146a3a9fca62a5215208398ddfdce7fec15d62
Brenda Wallace <brenda@wallace.net.nz>
2019-04-20 09:47:19 +1200

    javascript lint

--- app/assets/javascripts/plantings.js
+++ app/assets/javascripts/plantings.js
@@ -1,14 +1,8 @@
-// init Isotope
-var $grid = $('.planting-facts').isotope({
+$('.planting-facts').isotope({
   layoutMode: 'fitRows',
   percentPosition: true,
   itemSelector: '.fact',
   fitRows: {
-    gutter: 10
+    gutter: 10,
   }
 });
-// layout Isotope after each image loads
-// $grid.imagesLoaded().progress( function() {
-//   $grid.isotope('layout');
-// });
-
