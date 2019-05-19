dea6f36b0e22ac8f9c286cbf929a5fb7d8f8d326
Brenda Wallace <brenda@wallace.net.nz>
2019-05-19 15:34:26 +1200

    Fix filter posts by member

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -8,6 +8,22 @@ describe PostsController do
     { author_id: member.id, subject: "blah", body: "blah blah" }
   end
 
+  describe '#index' do
+    before do
+      FactoryBot.create_list :post, 100
+      FactoryBot.create_list :post, 5, author: member
+    end
+    describe "everyone's posts" do
+      before { get :index }
+      it {expect(assigns(:posts).size).to eq 12 }
+    end
+    describe "one member's posts" do
+      before { get :index, params: {member_slug: member.slug } }
+      it { expect(assigns(:posts).size).to eq 5 }
+      it { expect(assigns(:posts).first.author).to eq member }
+    end
+  end
+
   describe "GET RSS feed" do
     it "returns an RSS feed" do
       get :index, format: "rss"
