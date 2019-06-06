78ea7d6a53db8225eaca7a4ffbd14fb9fa2210ce
Brenda Wallace <brenda@wallace.net.nz>
2019-06-06 22:27:05 +1200

    Crop wrangling is now under the admin menu - update spec

--- spec/features/crops/crop_wranglers_spec.rb
+++ spec/features/crops/crop_wranglers_spec.rb
@@ -12,7 +12,7 @@ describe "crop wranglers", js: true do
 
     it "sees crop wranglers listed on the crop wrangler page" do
       visit root_path
-      click_link wrangler.login_name
+      click_link 'Admin'
       click_link 'Crop Wrangling'
 
       within '.crop_wranglers' do
@@ -25,7 +25,7 @@ describe "crop wranglers", js: true do
 
     it "can see list of crops with extra detail of who created a crop" do
       visit root_path
-      click_link wrangler.login_name
+      click_link 'Admin'
       click_link 'Crop Wrangling'
       within '#recently-added-crops' do
         expect(page).to have_content crops.first.creator.login_name.to_s
@@ -42,7 +42,7 @@ describe "crop wranglers", js: true do
 
     it "can create a new crop" do
       visit root_path
-      click_link wrangler.login_name
+      click_link 'Admin'
       click_link 'Crop Wrangling'
       click_link 'Add Crop'
       fill_in 'Name', with: "aubergine"
