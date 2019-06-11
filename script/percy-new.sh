accf549e58ad3db39a9a732167b6aa5b25a99f69
Brenda Wallace <brenda@wallace.net.nz>
2019-06-12 11:18:24 +1200

    Delete percy-new.sh

--- script/percy-new.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-bundle exec rails assets:precompile
-export PERCY_TOKEN=047f5b0ecf3ca0a2bc94a6cfc837e625ace93ef0d81cc8ac82e3fb3aebd5c17f
-PERCY_TARGET_BRANCH=dev npx percy exec -- bundle exec rspec spec/features/percy/
