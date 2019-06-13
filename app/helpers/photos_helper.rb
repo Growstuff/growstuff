798fff43cf2c2606c5ed8987843a7fd151f6200d
Brenda Wallace <brenda@wallace.net.nz>
2019-06-14 11:20:06 +1200

    DRY the photos helper

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,55 +1,38 @@
 module PhotosHelper
   def crop_image_path(crop, full_size: false)
-    if crop.default_photo.present?
-      photo = crop.default_photo
-      full_size ? photo.fullsize_url : photo.thumbnail_url
-    else
-      placeholder_image
-    end
+    photo_or_placeholder(crop, full_size: full_size)
   end
 
   def garden_image_path(garden, full_size: false)
-    if garden.default_photo.present?
-      photo = garden.default_photo
-      full_size ? photo.fullsize_url : photo.thumbnail_url
-    else
-      placeholder_image
-    end
+    photo_or_placeholder(garden, full_size: full_size)
   end
 
   def planting_image_path(planting, full_size: false)
-    if planting.photos.present?
-      photo = planting.photos.order(date_taken: :desc).first
-      full_size ? photo.fullsize_url : photo.thumbnail_url
-    else
-      placeholder_image
-    end
+    photo_or_placeholder(planting, full_size: full_size)
   end
 
   def harvest_image_path(harvest, full_size: false)
-    if harvest.photos.present?
-      photo = harvest.photos.order(date_taken: :desc).first
-      full_size ? photo.fullsize_url : photo.thumbnail_url
-    elsif harvest.planting.present?
-      planting_image_path(harvest.planting, full_size: full_size)
-    else
-      placeholder_image
-    end
+    photo_or_placeholder(harvest, full_size: full_size)
   end
 
   def seed_image_path(seed, full_size: false)
-    if seed.default_photo.present?
-      photo = seed.default_photo
-      full_size ? photo.fullsize_url : photo.thumbnail_url
-    elsif seed.crop.default_photo.present?
-      photo = seed.crop.default_photo
-      full_size ? photo.fullsize_url : photo.thumbnail_url
+    photo_or_placeholder(seed, full_size: full_size)
+  end
+
+  private
+
+  def photo_or_placeholder(item, full_size: false)
+    if item.default_photo.present?
+      item_photo(item, full_size: full_size)
     else
       placeholder_image
     end
   end
 
-  private
+  def item_photo(item, full_size:)
+    photo = item.default_photo
+    full_size ? photo.fullsize_url : photo.thumbnail_url
+  end
 
   def placeholder_image
     'placeholder_150.png'
