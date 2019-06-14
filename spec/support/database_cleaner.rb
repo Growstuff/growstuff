f73b260212ed4ae3c90a25f4b5920f600411dcc5
Brenda Wallace <brenda@wallace.net.nz>
2019-06-14 18:06:41 +1200

    replace existing database cleaner
    the old one doesn't play nice with capybara

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,21 +1,42 @@
 RSpec.configure do |config|
+
+  # database cleaner warning if you turn this off
   config.before(:suite) do
+    if config.use_transactional_fixtures?
+      raise(<<-MSG)
+        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+        (or set it to false) to prevent uncommitted transactions being used in
+        JavaScript-dependent specs.
+
+        During testing, the app-under-test that the browser driver connects to
+        uses a different database connection to the database connection used by
+        the spec. The app's database connection would not be able to access
+        uncommitted transaction data setup over the spec's database connection.
+      MSG
+    end
+
     DatabaseCleaner.clean_with(:truncation)
   end
-
-  config.before do
+  config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
+  # config.before(:suite) do
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
 
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
-  end
+  # config.before do
+  #   DatabaseCleaner.strategy = :transaction
+  # end
 
-  config.before do
-    DatabaseCleaner.start
-  end
+  # config.before(:each, js: true) do
+  #   DatabaseCleaner.strategy = :truncation
+  # end
 
-  config.after do
-    DatabaseCleaner.clean
-  end
+  # config.before do
+  #   DatabaseCleaner.start
+  # end
+
+  # config.after do
+  #   DatabaseCleaner.clean
+  # end
 end
