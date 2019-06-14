4fa4286670187213975f0977602f17d64a2e372b
Brenda Wallace <brenda@wallace.net.nz>
2019-06-14 20:32:12 +1200

    dismiss the alert when deleting crops

--- spec/features/crops/delete_crop_spec.rb
+++ spec/features/crops/delete_crop_spec.rb
@@ -11,16 +11,18 @@ describe "Delete crop spec" do
     it "Delete approved crop" do
       visit crop_path(approved_crop)
       click_link 'Actions'
-      click_link 'Delete'
-      dismiss_confirm {click_link 'OK'}
+      accept_confirm do
+        click_link 'Delete'
+      end
       expect(page).to have_content "crop was successfully destroyed"
     end
 
     it "Delete pending crop" do
       visit crop_path(pending_crop)
       click_link 'Actions'
-      click_link 'Delete'
-      dismiss_confirm {click_link 'OK'}
+      accept_confirm do
+        click_link 'Delete'
+      end
       expect(page).to have_content "crop was successfully destroyed"
     end
   end
