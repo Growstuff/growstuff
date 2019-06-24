926915aca038a05ed8f34603ac6bd4a52db56eca
Brenda Wallace <brenda@wallace.net.nz>
2019-06-24 18:29:18 +1200

    Update rss spec
    we can't check status codes anymore

--- spec/features/rss/members_spec.rb
+++ spec/features/rss/members_spec.rb
@@ -5,10 +5,6 @@ describe 'Members RSS feed' do
 
   before { visit member_path(member, format: 'rss') }
 
-  pending 'The show action exists' do
-    # expect(page.status_code).to equal 200
-  end
-
   it 'The show action title is what we expect' do
     expect(page).to have_content "#{member.login_name}'s recent posts (#{ENV['GROWSTUFF_SITE_NAME']})"
   end
