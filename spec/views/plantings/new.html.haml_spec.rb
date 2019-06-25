3d065c72ba1a82a0420c8952e98c11d611560485
Brenda Wallace <brenda@wallace.net.nz>
2019-06-25 15:54:50 +1200

    update new planting view spec

--- spec/views/plantings/new.html.haml_spec.rb
+++ spec/views/plantings/new.html.haml_spec.rb
@@ -20,15 +20,16 @@ describe "plantings/new" do
   context "logged in" do
     before do
       sign_in @member
-      assign(:planting, Planting.new)
+      planting = Planting.new(garden: @garden_z)
+      assign(:planting, planting)
       assign(:crop, @crop2)
-      assign(:garden, @garden_z)
+      assign(:gardens, [@garden_z])
       render
     end
 
     it "renders new planting form" do
       assert_select "form", action: plantings_path, method: "post" do
-        assert_select "select#planting_garden_id", name: "planting[garden_id]"
+        assert_select "input#planting_garden_id_#{@garden_z.id}", name: "planting[garden_id]"
         assert_select "input#crop", class: "ui-autocomplete-input"
         assert_select "input#planting_crop_id", name: "planting[crop_id]"
         assert_select "input#planting_quantity", name: "planting[quantity]"
@@ -43,8 +44,8 @@ describe "plantings/new" do
     end
 
     it "selects a garden given in a param" do
-      assert_select "select#planting_garden_id",
-        html: /option selected value="#{@garden_z.id}"/
+      assert_select "input", id: "planting_garden_id_#{@garden_z.id}",
+        type: 'radio', value: @garden_z.id, checked: "checked"
     end
   end
 end
