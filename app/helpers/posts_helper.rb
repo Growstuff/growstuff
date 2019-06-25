b176ba59dc16c9011cb896f71d7647a1a2fc4208
Brenda Wallace <brenda@wallace.net.nz>
2019-06-26 11:19:15 +1200

    removed byline method
    avoiding use of htmlseafe

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,25 +4,4 @@ module PostsHelper
     truncate(strip_tags(post.body), length: length,
         separator: ' ', omission: '... ') { link_to "Read more", post_path(post) }
   end
-
-  def post_byline(post)
-    byline = 'Posted by '
-
-    byline += if post.author
-                link_to post.author.login_name, member_path(post.author)
-              else
-                'Member Deleted'
-              end
-
-    if post.forum
-      byline += ' in '
-      byline += link_to post.forum, post.forum
-    end
-
-    byline += " on #{post.created_at}"
-
-    byline += " and edited at #{post.updated_at}" if post.updated_at > post.created_at
-
-    byline.html_safe
-  end
 end
