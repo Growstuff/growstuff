97028bdf1d7adf085b9cd9e08ac4d12918a0ce86
Brenda Wallace <brenda@wallace.net.nz>
2019-06-30 13:34:08 +1200

    Refactoring member profile spec to use shared example
    so we can test the page both signed in and not signed in

--- spec/features/members/profile_spec.rb
+++ spec/features/members/profile_spec.rb
@@ -1,21 +1,17 @@
 require 'rails_helper'
 
 describe "member profile", js: true do
-  context "signed out member" do
-    let(:member) { create :member }
+  let(:member) { create :member }
+  let(:other_member)  { create :member                }
+  let(:admin_member)  { create :admin_member          }
+  let(:crop_wrangler) { create :crop_wrangling_member }
 
+  shared_examples 'member details' do
     it "basic details on member profile page" do
       visit member_path(member)
       expect(page).to have_content("All about #{member.login_name}")
       expect(page).to have_content member.bio
       expect(page).to have_content "Member since #{member.created_at.to_s(:date)}"
-    end
-
-    it "no bio" do
-      member.bio = nil
-      member.save
-      visit member_path(member)
-      expect(page).to have_content "hasn't written a bio yet"
     end
 
     it "gravatar" do
@@ -37,18 +33,6 @@ describe "member profile", js: true do
         expect(page).not_to have_css("h1>small")
         expect(page).not_to have_css("#membermap")
         expect(page).not_to have_content "See other members"
-      end
-    end
-
-    context "email privacy" do
-      it "public email address" do
-        public_member = create :public_member
-        visit member_path(public_member)
-        expect(page).to have_content public_member.email
-      end
-      it "private email address" do
-        visit member_path(member)
-        expect(page).not_to have_content member.email
       end
     end
 
@@ -98,31 +82,41 @@ describe "member profile", js: true do
       visit member_path(member)
       expect(page).to have_link flickr_auth.name, href: "http://flickr.com/photos/#{flickr_auth.uid}"
     end
+
+    describe 'user role labels' do
+      describe "admin user's page" do
+        before { visit member_path(admin_member) }
+        it { expect(page).to have_text "Admin" }
+      end
+
+      it "crop wrangler's page" do
+        visit member_path(crop_wrangler)
+        expect(page).to have_text "Crop Wrangler"
+      end
+
+      it "ordinary user's page" do
+        visit member_path(other_member)
+        expect(page).not_to have_text "Crop Wrangler"
+        expect(page).not_to have_text "Admin"
+      end
+    end
+  end
+
+  context "not signed in" do
+    include_examples 'member details'
+
+    it "no bio" do
+      member.update! bio: nil
+      visit member_path(member)
+      expect(page).to have_content "hasn't written a bio yet"
+    end
   end
 
   context "signed in member" do
-    let(:member) { create :member }
-    let(:other_member)  { create :member                }
-    let(:admin_member)  { create :admin_member          }
-    let(:crop_wrangler) { create :crop_wrangling_member }
-
     before { login_as(member) }
 
-    it "admin user's page" do
-      visit member_path(admin_member)
-      expect(page).to have_text "Admin"
-    end
-
-    it "crop wrangler's page" do
-      visit member_path(crop_wrangler)
-      expect(page).to have_text "Crop Wrangler"
-    end
-
-    it "ordinary user's page" do
-      visit member_path(other_member)
-      expect(page).not_to have_text "Crop Wrangler"
-      expect(page).not_to have_text "Admin"
-    end
+    include_examples 'member details'
+    include_examples 'member activity'
 
     context "your own profile page" do
       before { visit member_path(member) }
@@ -133,23 +127,12 @@ describe "member profile", js: true do
     end
 
     context "someone else's profile page" do
-      before do
-        visit member_path(other_member)
-      end
+      before { visit member_path(other_member) }
 
       it "has a private message button" do
         expect(page).to have_link "Send message", href: new_notification_path(recipient_id: other_member.id)
       end
-    end
-
-    context "home page" do
-      before do
-        visit root_path
-      end
-
-      it "does not have a button to edit profile" do
-        expect(page).not_to have_link "Edit profile", href: edit_member_registration_path
-      end
+      it { expect(page).not_to have_link "Edit profile", href: edit_member_registration_path }
     end
   end
 end
