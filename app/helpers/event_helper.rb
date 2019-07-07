c61f13625ce618963f2317ae999963b2e7d9dd99
Brenda Wallace <brenda@wallace.net.nz>
2019-07-08 09:52:33 +1200

    Moved html into haml instead of html_safe in helper

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -1,19 +1,6 @@
 module EventHelper
   def event_description(event)
-    event_model = resolve_model(event)
-    if event.event_type == 'planting'
-      "planted #{link_to event_model.crop, event_model}"
-    elsif event.event_type == 'seed'
-      "saved #{link_to event_model.crop, event_model} seeds"
-    elsif event.event_type == 'harvest'
-      "harvested #{link_to event_model, event_model}"
-    elsif event.event_type == 'comment'
-      "#{link_to 'commented', event_model} on #{link_to event_model.post, event_model.post}"
-    elsif event.event_type == 'post'
-      "wrote a post about #{link_to event_model, event_model}"
-    elsif event.event_type == 'photo'
-      "took a photo #{link_to event_model.title, event_model}"
-    end.html_safe
+    render "#{event.event_type.pluralize}/description", event_model: resolve_model(event)
   end
 
   def resolve_model(event)
