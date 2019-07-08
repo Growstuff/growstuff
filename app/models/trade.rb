40857b1353ef5200b607629a86384e46536f6520
Brenda Wallace <brenda@wallace.net.nz>
2019-07-09 10:37:18 +1200

    More on the trades screen

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,8 +1,8 @@
 class Trade < ApplicationRecord
   belongs_to :seed
   belongs_to :requested_by, class_name: 'Member', inverse_of: :trades
-  delegate :owner, to: :seed
-  delegate :crop, to: :seed
+  has_one :owner, through: :seed
+  has_one :crop, through: :seed
 
   after_create do
     Notification.create(
@@ -11,5 +11,9 @@ class Trade < ApplicationRecord
       subject:   "#{requested_by.login_name} requests your #{seed.crop.name} seeds",
       body:      "View your trades on #{ENV['GROWSTUFF_SITE_NAME']} to accept or decline"
     )
+  end
+
+  def to_s
+    "#{crop} seeds requested by #{requested_by}"
   end
 end
