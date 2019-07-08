f638d0c2d33f5cfc8d12f644dcca1bee126482d7
Brenda Wallace <brenda@wallace.net.nz>
2019-07-08 20:03:27 +1200

    rubocop lint

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -41,7 +41,7 @@ class LikesController < ApplicationController
       format.json do
         render(json:   render_json(like,
               liked_by_member: liked_by_member),
-              status: status_code)
+               status: status_code)
       end
     end
   end
