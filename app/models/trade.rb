9e9b4bf89d8651bf49c740da5fee01f493e27e24
Brenda Wallace <brenda@wallace.net.nz>
2019-07-09 09:09:48 +1200

    Send a message after a trade is requested

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,4 +3,13 @@ class Trade < ApplicationRecord
   belongs_to :requested_by, class_name: 'Member', inverse_of: :trades
   delegate :owner, to: :seed
   delegate :crop, to: :seed
+
+  after_create do
+    Notification.create(
+      recipient: seed.owner,
+      sender:    requested_by,
+      subject:   "#{requested_by.login_name} requests your #{seed.crop.name} seeds",
+      body:      "View your trades on #{ENV['GROWSTUFF_SITE_NAME']} to accept or decline"
+    )
+  end
 end
