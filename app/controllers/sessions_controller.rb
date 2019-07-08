cbfe6690b83b0084f361361253715e1630c4b42f
Brenda Wallace <brenda@wallace.net.nz>
2019-07-09 09:07:47 +1200

    Add flash message onlogin if there's trades waiting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,12 @@ class SessionsController < Devise::SessionsController
 
   def create
     super do |_resource|
-      flash[:alert] = "There are crops waiting to be wrangled." if Crop.pending_approval.present? && current_member.role?(:crop_wrangler)
+      if Crop.pending_approval.present? && current_member.role?(:crop_wrangler)
+        flash[:alert] = "There are crops waiting to be wrangled."
+      end
+      if Trade.where(owner: current_member)
+        flash[:info] = "Someone has requested a seed trade."
+      end
     end
   end
 end
