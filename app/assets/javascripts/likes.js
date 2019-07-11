96863058574a920a7f2b27957779b44fde070be9
Brenda Wallace <brenda@wallace.net.nz>
2019-07-12 10:13:24 +1200

    The like icons are now the buttons

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -2,24 +2,26 @@ $(document).ready(function() {
   $('.like-btn').show();
 
   $('.post-like').on('ajax:success', function(event, data) {
-    var likeControl = $('#post-' + data.id + ' .post-like');
+    var like_button = $('#post-' + data.id + ' .post-like');
+    var like_badge = $('#post-'+ data.id + ' .like-badge');
 
     $('#post-' + data.id + ' .like-count').text(data.like_count);
     if (data.liked_by_member) {
-      likeControl.data('method', 'delete');
-      likeControl.attr('href', data.url);
-      likeControl.text('Unlike');
+      like_badge.addClass('liked');
+      like_button.data('method', 'delete');
+      like_button.attr('href', data.url);
+      like_button.text('Unlike');
     } else {
-      likeControl.data('method', 'post');
-      likeControl.attr('href', '/likes.json?post_id=' + data.id);
-      likeControl.text('Like');
+      like_badge.removeClass('liked');
+      like_button.data('method', 'post');
+      like_button.attr('href', '/likes.json?post_id=' + data.id);
+      like_button.text('Like');
     }
   });
 
 
   $('.photo-like').on('ajax:success', function(event, data) {
     var like_badge = $('#photo-'+ data.id + ' .like-badge');
-    var like_count = $('#photo-'+ data.id + ' .like-count');
     var like_button = $('#photo-'+ data.id + ' .like-btn');
 
     $('#photo-' + data.id + ' .like-count').text(data.like_count);
@@ -28,13 +30,11 @@ $(document).ready(function() {
       // Turn the button into an unlike button
       like_button.data('method', 'delete');
       like_button.attr('href', data.url);
-      like_button.text('Unlike');
     } else {
       like_badge.removeClass('liked');
       // Turn the button into an *like* button
       like_button.data('method', 'post');
       like_button.attr('href', '/likes.json?photo_id=' + data.id);
-      like_button.text('Like');
     }
   });
 });
