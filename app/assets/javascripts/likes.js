1b87026df4716bb114cbf00aef4023d9ca319b93
Brenda Wallace <brenda@wallace.net.nz>
2019-07-12 11:59:48 +1200

    Use camel case in js

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -2,39 +2,39 @@ $(document).ready(function() {
   $('.like-btn').show();
 
   $('.post-like').on('ajax:success', function(event, data) {
-    var like_button = $('#post-' + data.id + ' .post-like');
-    var like_badge = $('#post-'+ data.id + ' .like-badge');
+    var likeButton = $('#post-' + data.id + ' .post-like');
+    var likeBadge = $('#post-'+ data.id + ' .like-badge');
 
     $('#post-' + data.id + ' .like-count').text(data.like_count);
     if (data.liked_by_member) {
-      like_badge.addClass('liked');
-      like_button.data('method', 'delete');
-      like_button.attr('href', data.url);
-      like_button.text('Unlike');
+      likeBadge.addClass('liked');
+      likeButton.data('method', 'delete');
+      likeButton.attr('href', data.url);
+      likeButton.text('Unlike');
     } else {
-      like_badge.removeClass('liked');
-      like_button.data('method', 'post');
-      like_button.attr('href', '/likes.json?post_id=' + data.id);
-      like_button.text('Like');
+      likeBadge.removeClass('liked');
+      likeButton.data('method', 'post');
+      likeButton.attr('href', '/likes.json?post_id=' + data.id);
+      likeButton.text('Like');
     }
   });
 
 
   $('.photo-like').on('ajax:success', function(event, data) {
-    var like_badge = $('#photo-'+ data.id + ' .like-badge');
-    var like_button = $('#photo-'+ data.id + ' .like-btn');
+    var likeBadge = $('#photo-'+ data.id + ' .like-badge');
+    var likeButton = $('#photo-'+ data.id + ' .like-btn');
 
     $('#photo-' + data.id + ' .like-count').text(data.like_count);
     if (data.liked_by_member) {
-      like_badge.addClass('liked');
+      likeBadge.addClass('liked');
       // Turn the button into an unlike button
-      like_button.data('method', 'delete');
-      like_button.attr('href', data.url);
+      likeButton.data('method', 'delete');
+      likeButton.attr('href', data.url);
     } else {
-      like_badge.removeClass('liked');
+      likeBadge.removeClass('liked');
       // Turn the button into an *like* button
-      like_button.data('method', 'post');
-      like_button.attr('href', '/likes.json?photo_id=' + data.id);
+      likeButton.data('method', 'post');
+      likeButton.attr('href', '/likes.json?photo_id=' + data.id);
     }
   });
 });
