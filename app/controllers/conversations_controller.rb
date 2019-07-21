dab9a96486076a925521e339c08d0c364d344072
Brenda Wallace <brenda@wallace.net.nz>
2019-07-21 14:47:32 +1200

    better handling of deleted members

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,12 +1,13 @@
 class ConversationsController < ApplicationController
   respond_to :html
   before_action :authenticate_member!
+  before_action :set_box
   before_action :check_current_subject_in_conversation, only: %i(show update destroy)
 
   def index
-    @conversations = if box.eql? "inbox"
+    @conversations = if @box.eql? "inbox"
                        mailbox.inbox.paginate(page: params[:page])
-                     elsif box.eql? "sentbox"
+                     elsif @box.eql? "sent"
                        mailbox.sentbox.paginate(page: params[:page])
                      else
                        mailbox.trash.paginate(page: params[:page])
@@ -21,25 +22,25 @@ class ConversationsController < ApplicationController
   end
 
   def update
-    @conversation.untrash(@actor) if params[:untrash].present?
+  #   @conversation.untrash(@actor) if params[:untrash].present?
 
-    if params[:reply_all].present?
-      last_receipt = mailbox.receipts_for(@conversation).last
-      @receipt = @actor.reply_to_all(last_receipt, params[:body])
-    end
+  #   if params[:reply_all].present?
+  #     last_receipt = mailbox.receipts_for(@conversation).last
+  #     @receipt = @actor.reply_to_all(last_receipt, params[:body])
+  #   end
 
-    @receipts = if box.eql? 'trash'
-                  mailbox.receipts_for(@conversation).trash
-                else
-                  mailbox.receipts_for(@conversation).not_trash
-                end
-    redirect_to action: :show
-    @receipts.mark_as_read
+  #   @receipts = if box.eql? 'trash'
+  #                 mailbox.receipts_for(@conversation).trash
+  #               else
+  #                 mailbox.receipts_for(@conversation).not_trash
+  #               end
+  #   redirect_to action: :show
+  #   @receipts.mark_as_read
   end
 
   def destroy
-    @conversation = Mailboxer::Conversation.find params[:id]
-    current_member.mark_as_deleted @conversation
+    @conversation = Mailboxer::Conversation.find(params[:id])
+    @conversation.move_to_trash(current_member)
     redirect_to conversations_path
   end
 
@@ -49,8 +50,9 @@ class ConversationsController < ApplicationController
     current_member.mailbox
   end
 
-  def box
-    if params[:box].blank? || !%w(inbox sentbox trash).include?(params[:box])
+  def set_box
+    @boxes = %W[inbox sent trash]
+    @box = if params[:box].blank? || !@boxes.include?(params[:box])
       'inbox'
     else
       params[:box]
