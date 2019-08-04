0dc17207113875db7a9efc3419ac1c6a75c71abc
Brenda Wallace <brenda@wallace.net.nz>
2019-08-04 16:24:09 +1200

    Double quote to prevent globbing and word splitting.

--- script/install_elasticsearch.sh
+++ script/install_elasticsearch.sh
@@ -5,10 +5,10 @@ if [[ -z "$ELASTIC_SEARCH_VERSION" ]]; then
 else
   set -euv
   sudo dpkg -r elasticsearch
-  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTIC_SEARCH_VERSION}.deb
-  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTIC_SEARCH_VERSION}.deb.sha512
-  shasum -a 512 -c elasticsearch-${ELASTIC_SEARCH_VERSION}.deb.sha512
-  sudo dpkg -i --force-confnew elasticsearch-${ELASTIC_SEARCH_VERSION}.deb
+  wget "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTIC_SEARCH_VERSION}.deb"
+  wget "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTIC_SEARCH_VERSION}.deb.sha512"
+  shasum -a 512 -c "elasticsearch-${ELASTIC_SEARCH_VERSION}.deb.sha512"
+  sudo dpkg -i --force-confnew "elasticsearch-${ELASTIC_SEARCH_VERSION}.deb"
 
   sudo service elasticsearch start
   sleep 10
