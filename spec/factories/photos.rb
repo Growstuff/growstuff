d89d49d1bfa069ee45954efe67e6d49e323827f2
Brenda Wallace <brenda@wallace.net.nz>
2019-09-11 08:29:57 +1200

    Fix factory for photos

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -3,7 +3,8 @@
 FactoryBot.define do
   factory :photo do
     owner
-    flickr_photo_id { 1 }
+    source { 'flickr' }
+    source_id { 1 }
     title { Faker::Movies::HarryPotter.quote }
     license_name { "CC-BY" }
     license_url { "http://example.com/license.html" }
