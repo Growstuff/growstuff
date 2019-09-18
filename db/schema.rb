ed56b3910d47d7fd6bf5129c4884bac62e2f935d
Brenda Wallace <brenda@wallace.net.nz>
2019-09-18 14:14:08 +1200

    fixing specs

--- db/schema.rb
+++ db/schema.rb
@@ -16,30 +16,30 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   enable_extension "plpgsql"
 
   create_table "alternate_names", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255, null: false
+    t.string "name", null: false
     t.integer "crop_id", null: false
     t.integer "creator_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "authentications", id: :serial, force: :cascade do |t|
     t.integer "member_id", null: false
-    t.string "provider", limit: 255, null: false
-    t.string "uid", limit: 255
-    t.string "token", limit: 255
-    t.string "secret", limit: 255
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name", limit: 255
+    t.string "provider", null: false
+    t.string "uid"
+    t.string "token"
+    t.string "secret"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "name"
     t.index ["member_id"], name: "index_authentications_on_member_id"
   end
 
   create_table "comfy_cms_blocks", id: :serial, force: :cascade do |t|
-    t.string "identifier", limit: 255, null: false
+    t.string "identifier", null: false
     t.text "content"
+    t.string "blockable_type"
     t.integer "blockable_id"
-    t.string "blockable_type", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["blockable_id", "blockable_type"], name: "index_comfy_cms_blocks_on_blockable_id_and_blockable_type"
@@ -48,14 +48,14 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
 
   create_table "comfy_cms_categories", id: :serial, force: :cascade do |t|
     t.integer "site_id", null: false
-    t.string "label", limit: 255, null: false
-    t.string "categorized_type", limit: 255, null: false
+    t.string "label", null: false
+    t.string "categorized_type", null: false
     t.index ["site_id", "categorized_type", "label"], name: "index_cms_categories_on_site_id_and_cat_type_and_label", unique: true
   end
 
   create_table "comfy_cms_categorizations", id: :serial, force: :cascade do |t|
     t.integer "category_id", null: false
-    t.string "categorized_type", limit: 255, null: false
+    t.string "categorized_type", null: false
     t.integer "categorized_id", null: false
     t.index ["category_id", "categorized_type", "categorized_id"], name: "index_cms_categorizations_on_cat_id_and_catd_type_and_catd_id", unique: true
   end
@@ -63,9 +63,9 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   create_table "comfy_cms_files", id: :serial, force: :cascade do |t|
     t.integer "site_id", null: false
     t.integer "block_id"
-    t.string "label", limit: 255, null: false
-    t.string "file_file_name", limit: 255, null: false
-    t.string "file_content_type", limit: 255, null: false
+    t.string "label", null: false
+    t.string "file_file_name", null: false
+    t.string "file_content_type", null: false
     t.integer "file_file_size", null: false
     t.string "description", limit: 2048
     t.integer "position", default: 0, null: false
@@ -80,9 +80,9 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   create_table "comfy_cms_layouts", id: :serial, force: :cascade do |t|
     t.integer "site_id", null: false
     t.integer "parent_id"
-    t.string "app_layout", limit: 255
-    t.string "label", limit: 255, null: false
-    t.string "identifier", limit: 255, null: false
+    t.string "app_layout"
+    t.string "label", null: false
+    t.string "identifier", null: false
     t.text "content"
     t.text "css"
     t.text "js"
@@ -99,9 +99,9 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
     t.integer "layout_id"
     t.integer "parent_id"
     t.integer "target_page_id"
-    t.string "label", limit: 255, null: false
-    t.string "slug", limit: 255
-    t.string "full_path", limit: 255, null: false
+    t.string "label", null: false
+    t.string "slug"
+    t.string "full_path", null: false
     t.text "content_cache"
     t.integer "position", default: 0, null: false
     t.integer "children_count", default: 0, null: false
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   end
 
   create_table "comfy_cms_revisions", id: :serial, force: :cascade do |t|
-    t.string "record_type", limit: 255, null: false
+    t.string "record_type", null: false
     t.integer "record_id", null: false
     t.text "data"
     t.datetime "created_at"
@@ -122,11 +122,11 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   end
 
   create_table "comfy_cms_sites", id: :serial, force: :cascade do |t|
-    t.string "label", limit: 255, null: false
-    t.string "identifier", limit: 255, null: false
-    t.string "hostname", limit: 255, null: false
-    t.string "path", limit: 255
-    t.string "locale", limit: 255, default: "en", null: false
+    t.string "label", null: false
+    t.string "identifier", null: false
+    t.string "hostname", null: false
+    t.string "path"
+    t.string "locale", default: "en", null: false
     t.boolean "is_mirrored", default: false, null: false
     t.index ["hostname"], name: "index_comfy_cms_sites_on_hostname"
     t.index ["is_mirrored"], name: "index_comfy_cms_sites_on_is_mirrored"
@@ -134,8 +134,8 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
 
   create_table "comfy_cms_snippets", id: :serial, force: :cascade do |t|
     t.integer "site_id", null: false
-    t.string "label", limit: 255, null: false
-    t.string "identifier", limit: 255, null: false
+    t.string "label", null: false
+    t.string "identifier", null: false
     t.text "content"
     t.integer "position", default: 0, null: false
     t.boolean "is_shared", default: false, null: false
@@ -149,8 +149,8 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
     t.integer "post_id", null: false
     t.integer "author_id", null: false
     t.text "body", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "crop_companions", force: :cascade do |t|
@@ -159,16 +159,16 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   end
 
   create_table "crops", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255, null: false
-    t.string "en_wikipedia_url", limit: 255
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug", limit: 255
+    t.string "name", null: false
+    t.string "en_wikipedia_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "slug"
     t.integer "parent_id"
     t.integer "plantings_count", default: 0
     t.integer "creator_id"
     t.integer "requester_id"
-    t.string "approval_status", limit: 255, default: "approved"
+    t.string "approval_status", default: "approved"
     t.text "reason_for_rejection"
     t.text "request_notes"
     t.text "rejection_notes"
@@ -192,17 +192,17 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   create_table "follows", id: :serial, force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "forums", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255, null: false
+    t.string "name", null: false
     t.text "description", null: false
     t.integer "owner_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "slug"
     t.index ["slug"], name: "index_forums_on_slug", unique: true
   end
 
@@ -214,21 +214,21 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   end
 
   create_table "gardens", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255, null: false
+    t.string "name", null: false
     t.integer "owner_id"
-    t.string "slug", limit: 255, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "slug", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text "description"
     t.boolean "active", default: true
-    t.string "location", limit: 255
+    t.string "location"
     t.float "latitude"
     t.float "longitude"
     t.decimal "area"
-    t.string "area_unit", limit: 255
+    t.string "area_unit"
     t.integer "garden_type_id"
     t.index ["garden_type_id"], name: "index_gardens_on_garden_type_id"
-    t.index ["owner_id"], name: "index_gardens_on_user_id"
+    t.index ["owner_id"], name: "index_gardens_on_owner_id"
     t.index ["slug"], name: "index_gardens_on_slug", unique: true
   end
 
@@ -243,13 +243,13 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
     t.integer "owner_id", null: false
     t.date "harvested_at"
     t.decimal "quantity"
-    t.string "unit", limit: 255
+    t.string "unit"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "slug"
     t.decimal "weight_quantity"
-    t.string "weight_unit", limit: 255
+    t.string "weight_unit"
     t.integer "plant_part_id"
     t.float "si_weight"
     t.integer "planting_id"
@@ -264,8 +264,8 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
 
   create_table "likes", id: :serial, force: :cascade do |t|
     t.integer "member_id"
-    t.integer "likeable_id"
     t.string "likeable_type"
+    t.integer "likeable_id"
     t.string "categories", array: true
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -334,30 +334,30 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   end
 
   create_table "members", id: :serial, force: :cascade do |t|
-    t.string "email", limit: 255, default: "", null: false
-    t.string "encrypted_password", limit: 255, default: "", null: false
-    t.string "reset_password_token", limit: 255
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip", limit: 255
-    t.string "last_sign_in_ip", limit: 255
-    t.string "confirmation_token", limit: 255
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email", limit: 255
+    t.string "unconfirmed_email"
     t.integer "failed_attempts", default: 0
-    t.string "unlock_token", limit: 255
+    t.string "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "login_name", limit: 255
-    t.string "slug", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "login_name"
+    t.string "slug"
     t.boolean "tos_agreement"
     t.boolean "show_email"
-    t.string "location", limit: 255
+    t.string "location"
     t.float "latitude"
     t.float "longitude"
     t.boolean "send_notification_email", default: true
@@ -365,19 +365,19 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
     t.integer "plantings_count"
     t.boolean "newsletter"
     t.boolean "send_planting_reminder", default: true
-    t.string "preferred_avatar_uri", limit: 255
+    t.string "preferred_avatar_uri"
     t.integer "gardens_count"
     t.integer "harvests_count"
     t.integer "seeds_count"
     t.datetime "discarded_at"
     t.integer "photos_count"
     t.integer "forums_count"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["confirmation_token"], name: "index_members_on_confirmation_token", unique: true
     t.index ["discarded_at"], name: "index_members_on_discarded_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["slug"], name: "index_users_on_slug", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+    t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_members_on_slug", unique: true
+    t.index ["unlock_token"], name: "index_members_on_unlock_token", unique: true
   end
 
   create_table "members_roles", id: false, force: :cascade do |t|
@@ -388,12 +388,12 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   create_table "notifications", id: :serial, force: :cascade do |t|
     t.integer "sender_id"
     t.integer "recipient_id", null: false
-    t.string "subject", limit: 255
+    t.string "subject"
     t.text "body"
     t.boolean "read", default: false
     t.integer "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "orders_products", id: false, force: :cascade do |t|
@@ -414,15 +414,15 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
 
   create_table "photos", id: :serial, force: :cascade do |t|
     t.integer "owner_id", null: false
-    t.string "source_id", limit: 255, null: false
-    t.string "thumbnail_url", limit: 255, null: false
-    t.string "fullsize_url", limit: 255, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "title", limit: 255, null: false
-    t.string "license_name", limit: 255, null: false
-    t.string "license_url", limit: 255
-    t.string "link_url", limit: 255, null: false
+    t.string "thumbnail_url", null: false
+    t.string "fullsize_url", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "title", null: false
+    t.string "license_name", null: false
+    t.string "license_url"
+    t.string "link_url", null: false
+    t.string "source_id"
     t.datetime "date_taken"
     t.integer "likes_count", default: 0
     t.string "source"
@@ -440,10 +440,10 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   end
 
   create_table "plant_parts", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug", limit: 255
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "slug"
   end
 
   create_table "plantings", id: :serial, force: :cascade do |t|
@@ -452,11 +452,11 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
     t.date "planted_at"
     t.integer "quantity"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug", limit: 255
-    t.string "sunniness", limit: 255
-    t.string "planted_from", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "slug"
+    t.string "sunniness"
+    t.string "planted_from"
     t.integer "owner_id"
     t.boolean "finished", default: false, null: false
     t.date "finished_at"
@@ -470,31 +470,31 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
 
   create_table "posts", id: :serial, force: :cascade do |t|
     t.integer "author_id", null: false
-    t.string "subject", limit: 255, null: false
+    t.string "subject", null: false
     t.text "body", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "slug"
     t.integer "forum_id"
     t.integer "likes_count", default: 0
-    t.index ["created_at", "author_id"], name: "index_updates_on_created_at_and_user_id"
-    t.index ["slug"], name: "index_updates_on_slug", unique: true
+    t.index ["created_at", "author_id"], name: "index_posts_on_created_at_and_author_id"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   create_table "roles", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255, null: false
+    t.string "name", null: false
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "slug"
     t.index ["slug"], name: "index_roles_on_slug", unique: true
   end
 
   create_table "scientific_names", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255, null: false
+    t.string "name", null: false
     t.integer "crop_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer "creator_id"
   end
 
@@ -504,10 +504,10 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
     t.text "description"
     t.integer "quantity"
     t.date "plant_before"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "tradable_to", limit: 255, default: "nowhere"
-    t.string "slug", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string "tradable_to", default: "nowhere"
+    t.string "slug"
     t.integer "days_until_maturity_min"
     t.integer "days_until_maturity_max"
     t.text "organic", default: "unknown"
@@ -519,19 +519,6 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
     t.index ["slug"], name: "index_seeds_on_slug", unique: true
   end
 
-  create_table "trades", force: :cascade do |t|
-    t.bigint "seed_id"
-    t.bigint "requested_by_id"
-    t.boolean "accepted"
-    t.text "message"
-    t.text "address_for_delivery"
-    t.text "rejection_reason"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["requested_by_id"], name: "index_trades_on_requested_by_id"
-    t.index ["seed_id"], name: "index_trades_on_seed_id"
-  end
-
   add_foreign_key "harvests", "plantings"
   add_foreign_key "mailboxer_conversation_opt_outs", "mailboxer_conversations", column: "conversation_id", name: "mb_opt_outs_on_conversations_id"
   add_foreign_key "mailboxer_notifications", "mailboxer_conversations", column: "conversation_id", name: "notifications_on_conversation_id"
@@ -540,6 +527,4 @@ ActiveRecord::Schema.define(version: 2019_09_15_065209) do
   add_foreign_key "photo_associations", "photos"
   add_foreign_key "plantings", "seeds", column: "parent_seed_id", name: "parent_seed", on_delete: :nullify
   add_foreign_key "seeds", "plantings", column: "parent_planting_id", name: "parent_planting", on_delete: :nullify
-  add_foreign_key "trades", "members", column: "requested_by_id"
-  add_foreign_key "trades", "seeds"
 end
