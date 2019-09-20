b2ab1d8538dc76db560265edb1428b608c33f5f6
Brenda Wallace <brenda@wallace.net.nz>
2019-09-21 10:22:55 +1200

    data base schema for crop companions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_21_042146) do
+ActiveRecord::Schema.define(version: 2019_09_15_065209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -151,6 +151,11 @@ ActiveRecord::Schema.define(version: 2019_07_21_042146) do
     t.text "body", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "crop_companions", force: :cascade do |t|
+    t.integer "crop_a_id", null: false
+    t.integer "crop_b_id", null: false
   end
 
   create_table "crops", id: :serial, force: :cascade do |t|
