fc07571ee107d19dfe8dccf86d29d9f94686ae54
Brenda Wallace <brenda@wallace.net.nz>
2019-10-19 10:25:12 +1300

    Rename `is_admin?` to `admin?`.

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,6 +1,6 @@
 module Admin
   class MembersController < ApplicationController
-    before_action :is_admin?
+    before_action :admin?
     load_and_authorize_resource
     respond_to :html
     responders :flash
@@ -34,7 +34,7 @@ module Admin
       params[:q]
     end
 
-    def is_admin?
+    def admin?
       authorize! :manage, :all
     end
   end
--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -1,6 +1,6 @@
 module Admin
   class RolesController < ApplicationController
-    before_action :is_admin?
+    before_action :admin?
     load_and_authorize_resource
     respond_to :html
     responders :flash
@@ -36,7 +36,7 @@ module Admin
 
     private
 
-    def is_admin?
+    def admin?
       authorize! :manage, :all
     end
 
