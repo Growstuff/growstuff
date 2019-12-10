c7e8efb1f1709eacfc1f9f122058ce45a41a1b25
codefactor-io <support@codefactor.io>
2019-12-10 00:37:06 +0000

    [CodeFactor] Apply fixes to commit 5afe3e1

--- app/models/concerns/planting_search.rb
+++ app/models/concerns/planting_search.rb
@@ -8,30 +8,29 @@ module PlantingSearch
 
     def search_data
       {
-        slug:          slug,
-        crop_slug:     crop.slug,
-        crop_name:     crop.name,
-        crop_id:       crop_id,
-        owner_id:      owner_id,
-        owner_name:    owner.login_name,
-        planted_from:  planted_from,
-        photos_count:  photos.size,
-        harvests_count:  photos.size,
-        has_photos:    photos.size.positive?,
-        thumbnail_url: default_photo&.thumbnail_url,
-        created_at:    created_at.to_i
+        slug:           slug,
+        crop_slug:      crop.slug,
+        crop_name:      crop.name,
+        crop_id:        crop_id,
+        owner_id:       owner_id,
+        owner_name:     owner.login_name,
+        planted_from:   planted_from,
+        photos_count:   photos.size,
+        harvests_count: photos.size,
+        has_photos:     photos.size.positive?,
+        thumbnail_url:  default_photo&.thumbnail_url,
+        created_at:     created_at.to_i
       }
     end
 
     def self.homepage_records(limit)
       self.search('*',
-        limit: limit,
-          where: {
-            photos_count: {gt: 0},
-          },
-          boost_by: [:created_at],
-          load: false
-        )
+        limit:    limit,
+        where:    {
+          photos_count: { gt: 0 }
+        },
+        boost_by: [:created_at],
+        load:     false)
     end
   end
 end
