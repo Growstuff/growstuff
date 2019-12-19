0c338e771c31130eb763830b943e8b9111d46e87
codefactor-io <support@codefactor.io>
2019-12-19 20:33:55 +0000

    [CodeFactor] Apply fixes to commit 18cb218

--- app/models/concerns/crop_search.rb
+++ app/models/concerns/crop_search.rb
@@ -4,15 +4,15 @@ module CropSearch
   included do
     ####################################
     # Elastic search configuration
-    searchkick word_start:     %i(name description alternate_names scientific_names),
+    searchkick word_start: %i(name description alternate_names scientific_names),
                case_sensitive: false,
                merge_mappings: true,
-               mappings:       {
+               mappings: {
                  properties: {
-                   created_at:      { type: :integer },
+                   created_at: { type: :integer },
                    plantings_count: { type: :integer },
-                   harvests_count:  { type: :integer },
-                   photos_count:    { type: :integer }
+                   harvests_count: { type: :integer },
+                   photos_count: { type: :integer }
                  }
                }
 
@@ -25,21 +25,21 @@ module CropSearch
 
     def search_data
       {
-        name:             name,
-        description:      description,
-        slug:             slug,
-        alternate_names:  alternate_names.pluck(:name),
+        name: name,
+        description: description,
+        slug: slug,
+        alternate_names: alternate_names.pluck(:name),
         scientific_names: scientific_names.pluck(:name),
-        photos_count:     photo_associations_count,
+        photos_count: photo_associations_count,
         # boost the crops that are planted the most
-        plantings_count:  plantings_count,
-        harvests_count:   harvests_count,
+        plantings_count: plantings_count,
+        harvests_count: harvests_count,
         # boost this crop for these members
-        planters_ids:     plantings.pluck(:owner_id),
-        has_photos:       photos.size.positive?,
-        thumbnail_url:    default_photo&.thumbnail_url,
-        scientific_name:  default_scientific_name&.name,
-        created_at:       created_at.to_i
+        planters_ids: plantings.pluck(:owner_id),
+        has_photos: photos.size.positive?,
+        thumbnail_url: default_photo&.thumbnail_url,
+        scientific_name: default_scientific_name&.name,
+        created_at: created_at.to_i
       }
     end
   end
