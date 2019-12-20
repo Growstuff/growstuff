9a0f22a4b9e65e830740bbcb6806aaa195415f3c
codefactor-io <support@codefactor.io>
2019-12-20 04:33:33 +0000

    [CodeFactor] Apply fixes to commit fc1c507

--- app/models/concerns/planting_search.rb
+++ app/models/concerns/planting_search.rb
@@ -5,11 +5,11 @@ module PlantingSearch
 
   included do
     searchkick merge_mappings: true,
-               mappings:       {
+               mappings: {
                  properties: {
-                   created_at:     { type: :integer },
+                   created_at: { type: :integer },
                    harvests_count: { type: :integer },
-                   photos_count:   { type: :integer },
+                   photos_count: { type: :integer },
                    owner_location: { type: :text }
                  }
                }
@@ -18,38 +18,38 @@ module PlantingSearch
 
     def search_data
       {
-        slug:             slug,
-        active:           active?,
-        crop_id:          crop_id,
-        crop_name:        crop.name,
-        crop_slug:        crop.slug,
-        finished:         finished?,
-        harvests_count:   harvests.size,
-        has_photos:       photos.size.positive?,
-        location:         location,
-        owner_id:         owner_id,
-        owner_location:   owner.location,
-        owner_name:       owner.login_name,
-        owner_slug:       owner.slug,
+        slug: slug,
+        active: active?,
+        crop_id: crop_id,
+        crop_name: crop.name,
+        crop_slug: crop.slug,
+        finished: finished?,
+        harvests_count: harvests.size,
+        has_photos: photos.size.positive?,
+        location: location,
+        owner_id: owner_id,
+        owner_location: owner.location,
+        owner_name: owner.login_name,
+        owner_slug: owner.slug,
         percentage_grown: percentage_grown.to_i,
-        photos_count:     photos.size,
-        planted_at:       planted_at,
-        planted_from:     planted_from,
-        quantity:         quantity,
-        sunniness:        sunniness,
-        thumbnail_url:    default_photo&.thumbnail_url,
-        created_at:       created_at.to_i
+        photos_count: photos.size,
+        planted_at: planted_at,
+        planted_from: planted_from,
+        quantity: quantity,
+        sunniness: sunniness,
+        thumbnail_url: default_photo&.thumbnail_url,
+        created_at: created_at.to_i
       }
     end
 
     def self.homepage_records(limit)
       search('*',
-             limit:    limit,
-             where:    {
+             limit: limit,
+             where: {
                photos_count: { gt: 0 }
              },
              boost_by: [:created_at],
-             load:     false)
+             load: false)
     end
   end
 end
