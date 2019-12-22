8fe5e4435b53d82ea4cfef202eab287d9d231ded
codefactor-io <support@codefactor.io>
2019-12-22 20:46:53 +0000

    [CodeFactor] Apply fixes

--- app/controllers/plantings_controller.rb
+++ app/controllers/plantings_controller.rb
@@ -29,11 +29,11 @@ class PlantingsController < ApplicationController
     end
 
     @plantings = Planting.search(
-      where:    where,
-      page:     params[:page],
-      limit:    30,
+      where: where,
+      page: params[:page],
+      limit: 30,
       boost_by: [:created_at],
-      load:     false
+      load: false
     )
 
     @filename = "Growstuff-#{specifics}Plantings-#{Time.zone.now.to_s(:number)}.csv"
@@ -59,15 +59,15 @@ class PlantingsController < ApplicationController
   def new
     @planting = Planting.new(
       planted_at: Time.zone.today,
-      owner:      current_member,
-      garden:     current_member.gardens.first
+      owner: current_member,
+      garden: current_member.gardens.first
     )
     @seed = Seed.find_by(slug: params[:seed_id]) if params[:seed_id]
     @crop = Crop.approved.find_by(id: params[:crop_id]) || Crop.new
     if params[:garden_id]
       @planting.garden = Garden.find_by(
         owner: current_member,
-        id:    params[:garden_id]
+        id: params[:garden_id]
       )
     end
 
