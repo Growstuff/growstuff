6622aaf8860fd75d1c6ee5ce4b92075b81c6f5c4
Brenda Wallace <brenda@brenda.nz>
2019-12-24 09:25:15 +1300

    used expect syntax

--- spec/requests/harvests_spec.rb
+++ spec/requests/harvests_spec.rb
@@ -5,9 +5,8 @@ require 'rails_helper'
 describe "Harvests" do
   describe "GET /harvests" do
     it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get harvests_path
-      response.status.should be(200)
+      expect(response.status).to be 200
     end
   end
 end
