5962081c9e1b07d9492df9635ccfd09e21be7f32
Brenda Wallace <brenda@brenda.nz>
2019-12-25 11:42:29 +1300

    fix specs fpr seeds rss feed

--- spec/views/seeds/index.rss.haml_spec.rb
+++ spec/views/seeds/index.rss.haml_spec.rb
@@ -7,21 +7,9 @@ describe 'seeds/index.rss.haml', :search do
     controller.stub(:current_user) { nil }
   end
 
-  context 'all seeds' do
-    let(:seed) { FactoryBot.create(:seed) }
-    before do
-      @tradable = FactoryBot.create(:tradable_seed)
-      Seed.searchkick_index.refresh
-      assign(:seeds, Seed.search(load: false))
-      render
-    end
-
-    it 'shows RSS feed title' do
-      expect(rendered).to have_content "Recent seeds from all members"
-    end
-
+  shared_examples 'displays seed in rss feed' do
     it 'has a useful item title' do
-      expect(rendered).to have_content "#{seed.owner.login_name}'s #{seed.crop} seeds"
+      expect(rendered).to have_content "#{seed.owner.login_name}'s #{seed.crop.name} seeds"
     end
 
     it 'shows the seed count' do
@@ -31,23 +19,47 @@ describe 'seeds/index.rss.haml', :search do
     it 'shows the plant_before date' do
       expect(rendered).to have_content "Plant before: #{seed.plant_before.to_s(:ymd)}"
     end
+  end
+
+  context 'all seeds' do
+    let!(:seed) { FactoryBot.create(:seed) }
+    let!(:tradable) { FactoryBot.create(:tradable_seed) }
+    before do
+      Seed.searchkick_index.refresh
+      assign(:seeds, Seed.search(load: false))
+      render
+    end
+
+    include_examples 'displays seed in rss feed'
+
+    it 'shows RSS feed title' do
+      expect(rendered).to have_content "Recent seeds from all members"
+    end
+
 
     it 'mentions that one seed is tradable' do
-      expect(rendered).to have_content "Will trade #{@tradable.tradable_to} from #{@tradable.owner.location}"
+      expect(rendered).to have_content "Will trade #{tradable.tradable_to} from #{tradable.owner.location}"
+    end
+
+    it "does not offer untradable seed as tradeable" do
+      expect(rendered).not_to have_content "Will trade #{seed.tradable_to} from #{seed.owner.location}"
     end
   end
 
   context "one member's seeds" do
+    let!(:seed) { FactoryBot.create(:seed) }
+
     before do
-      seed = FactoryBot.create(:seed)
-      assign(:seeds, [seed])
       assign(:owner, seed.owner)
-      Seed.reindex
+      Seed.searchkick_index.refresh
+      assign(:seeds, Seed.search(load: false))
       render
     end
 
     it 'shows RSS feed title' do
       expect(rendered).to have_content "Recent seeds from #{seed.owner}"
     end
+    
+    include_examples 'displays seed in rss feed'
   end
 end
