06241842700817b491d1a18a57cf7b9baf2b1ed2
Brenda Wallace <brenda@brenda.nz>
2019-12-28 09:06:13 +1300

    tidying up matching attributes for seeds rss feed

--- app/models/concerns/search_seeds.rb
+++ app/models/concerns/search_seeds.rb
@@ -4,54 +4,52 @@ module SearchSeeds
   extend ActiveSupport::Concern
 
   included do
-    searchkick merge_mappings: true,
-               mappings: {
-                 properties: {
-                   created_at: { type: :integer },
-                   plant_before: { type: :text },
-                   photos_count: { type: :integer },
-                   tradable_to: { type: :text }
-                 }
-               }
+    searchkick merge_mappings: true, mappings: {
+      properties: {
+        created_at:   { type: :integer },
+        plant_before: { type: :text },
+        photos_count: { type: :integer },
+        tradable_to:  { type: :text }
+      }
+    }
 
     scope :search_import, -> { includes(:owner, :crop, :parent_planting) }
 
     def search_data
       {
-        slug: slug,
-        crop_id: crop_id,
-        crop_name: crop.name,
-        crop_slug: crop.slug,
-        gmo: gmo,
-        has_photos: photos.size.positive?,
-        heirloom: heirloom,
-        organic: organic,
-        owner_id: owner_id,
+        slug:             slug,
+        crop_id:          crop_id,
+        crop_name:        crop.name,
+        crop_slug:        crop.slug,
+        finished:         finished?,
+        gmo:              gmo,
+        has_photos:       photos.size.positive?,
+        heirloom:         heirloom,
+        location:         owner.location,
+        organic:          organic,
+        owner_id:         owner_id,
+        owner_location:   owner_location,
         owner_login_name: owner_login_name,
-        owner_location: owner_location,
-        owner_slug: owner_slug,
-        parent_planting: parent_planting,
-        photos_count: photos.size,
-        plant_before: plant_before&.to_s(:ymd),
-        quantity: quantity,
-        thumbnail_url: default_photo&.thumbnail_url || crop.default_photo&.thumbnail_url,
-        tradable_to: tradable_to,
-        tradable: tradable?,
-        finished: finished?,
-        location: owner.location,
-        created_at: created_at.to_i
+        owner_slug:       owner_slug,
+        parent_planting:  parent_planting,
+        photos_count:     photos.size,
+        plant_before:     plant_before&.to_s(:ymd),
+        quantity:         quantity,
+        thumbnail_url:    default_photo&.thumbnail_url || crop.default_photo&.thumbnail_url,
+        tradable_to:      tradable_to,
+        tradable:         tradable?,
+        created_at:       created_at.to_i
       }
     end
 
     def self.homepage_records(limit)
-      search('*',
-             limit: limit,
-             where: {
-               finished: false,
-               tradable: true
-             },
-             boost_by: [:created_at],
-             load: false)
+      search('*', limit:    limit,
+                  where:    {
+                    finished: false,
+                    tradable: true
+                  },
+                  boost_by: [:created_at],
+                  load:     false)
     end
   end
 end
