96f0866f39f50b3501c49293bc0dd4b2fc74587c
Brenda Wallace <brenda@brenda.nz>
2020-01-06 21:49:00 +1300

    rubocop fix up

--- app/models/concerns/search_plantings.rb
+++ app/models/concerns/search_plantings.rb
@@ -6,10 +6,10 @@ module SearchPlantings
   included do
     searchkick merge_mappings: true, mappings: {
       properties: {
-        active: { type: :boolean },
-        created_at: { type: :integer },
+        active:         { type: :boolean },
+        created_at:     { type: :integer },
         harvests_count: { type: :integer },
-        photos_count: { type: :integer },
+        photos_count:   { type: :integer },
         owner_location: { type: :text }
       }
     }
@@ -18,46 +18,46 @@ module SearchPlantings
 
     def search_data
       {
-        slug: slug,
-        active: active,
-        finished: finished?,
-        has_photos: photos.size.positive?,
-        location: location,
+        slug:             slug,
+        active:           active,
+        finished:         finished?,
+        has_photos:       photos.size.positive?,
+        location:         location,
         percentage_grown: percentage_grown.to_i,
-        planted_at: planted_at,
-        planted_from: planted_from,
-        quantity: quantity,
-        sunniness: sunniness,
+        planted_at:       planted_at,
+        planted_from:     planted_from,
+        quantity:         quantity,
+        sunniness:        sunniness,
 
         # crops
-        crop_id: crop_id,
-        crop_name: crop.name,
-        crop_slug: crop.slug,
+        crop_id:          crop_id,
+        crop_name:        crop.name,
+        crop_slug:        crop.slug,
 
         # owner
-        owner_id: owner_id,
-        owner_location: owner_location,
+        owner_id:         owner_id,
+        owner_location:   owner_location,
         owner_login_name: owner_login_name,
-        owner_slug: owner_slug,
+        owner_slug:       owner_slug,
 
         # photos
-        thumbnail_url: default_photo&.thumbnail_url || crop.default_photo&.thumbnail_url,
+        thumbnail_url:    default_photo&.thumbnail_url || crop.default_photo&.thumbnail_url,
         # counts
-        photos_count: photos.size,
-        harvests_count: harvests_count,
+        photos_count:     photos.size,
+        harvests_count:   harvests_count,
 
         # timestamps
-        created_at: created_at.to_i
+        created_at:       created_at.to_i
       }
     end
 
     def self.homepage_records(limit)
-      search('*', limit: limit,
-                  where: {
+      search('*', limit:    limit,
+                  where:    {
                     photos_count: { gt: 0 }
                   },
                   boost_by: [:created_at],
-                  load: false)
+                  load:     false)
     end
   end
 end
