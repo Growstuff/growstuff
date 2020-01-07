8dfb8ff954aa1d5e00c224565525c8cc2e6c75eb
Brenda Wallace <brenda@brenda.nz>
2020-01-08 08:10:16 +1300

    Update params in seeds controller spec

--- spec/controllers/seeds_controller_spec.rb
+++ spec/controllers/seeds_controller_spec.rb
@@ -6,10 +6,11 @@ describe SeedsController, :search do
   let(:owner) { FactoryBot.create(:member) }
 
   describe "GET index" do
-    let(:owner) { FactoryBot.create(:member) }
-
     describe "picks up owner from params" do
-      before { get :index, params: { member_slug: owner.slug } }
+      before do
+        Seed.reindex
+        get :index, params: { member_slug: owner.slug }
+      end
 
       it { expect(assigns(:owner)).to eq(owner) }
     end
@@ -25,9 +26,12 @@ describe SeedsController, :search do
     end
 
     context 'with parent planting' do
-      let(:planting) { FactoryBot.create :planting, owner: owner }
+      let!(:planting) { FactoryBot.create :planting, owner: owner }
 
-      before { get :new, params: { planting_id: planting.to_param } }
+      before do
+        Seed.reindex
+        get :new, params: { planting_slug: planting.to_param }
+      end
 
       it { expect(assigns(:planting)).to eq(planting) }
     end
