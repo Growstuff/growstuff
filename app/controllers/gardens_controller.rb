c933665e7d8b990905a74ee56dee27741c187d62
Martina Simicic <922964+simicic@users.noreply.github.com>
2020-01-08 08:59:50 +0100

    Name variable to somthing more descriptive

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -4,7 +4,7 @@ class GardensController < DataController
   def index
     @owner = Member.find_by(slug: params[:member_slug])
     @show_all = params[:all] == '1'
-    @my_garden = params[:member_slug].present? ? true : false
+    @show_jump_to = params[:member_slug].present? ? true : false
 
     @gardens = @gardens.includes(:owner)
     @gardens = @gardens.active unless @show_all
