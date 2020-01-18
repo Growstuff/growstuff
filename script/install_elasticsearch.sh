973609e21e559060c26214803233c313035c5a27
Brenda Wallace <brenda@brenda.nz>
2020-01-18 13:40:35 +1300

    More echoes in the elastic search installer

--- script/install_elasticsearch.sh
+++ script/install_elasticsearch.sh
@@ -3,13 +3,16 @@
 if [[ -z "$ELASTIC_SEARCH_VERSION" ]]; then
   echo "ELASTIC_SEARCH_VERSION variable not set"
 else
+  echo "Downloading Elasticsearch ${ELASTIC_SEARCH_VERSION}"
   sudo dpkg -r elasticsearch
   wget "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTIC_SEARCH_VERSION}.deb"
   wget "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTIC_SEARCH_VERSION}.deb.sha512"
   shasum -a 512 -c "elasticsearch-${ELASTIC_SEARCH_VERSION}.deb.sha512"
+
+  echo "Installing Elasticsearch ${ELASTIC_SEARCH_VERSION}"
   sudo dpkg -i --force-confnew "elasticsearch-${ELASTIC_SEARCH_VERSION}.deb"
 
-  # echo "Telling Elasticsearch to start."
+  echo "Starting Elasticsearch ${ELASTIC_SEARCH_VERSION}"
   # sudo service elasticsearch start
   sudo systemctl start elasticsearch
 
