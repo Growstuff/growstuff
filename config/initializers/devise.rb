5816b13244f272e28f07bb5c6cd7edae0ca06237
Awesome Code <team@xinminlabs.com>
2020-04-12 23:45:53 +0000

    Auto corrected by following Lint Ruby Lint/RedundantCopDisableDirective

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-
-# rubocop:disable Layout/LineLength
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
