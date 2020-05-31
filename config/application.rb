5600670fef46ef11d80285c2d67e1b3d70ddb243
Brenda Wallace <brenda@brenda.nz>
2020-05-31 12:58:26 +1200

    Only allow open CORS on the api

--- config/application.rb
+++ config/application.rb
@@ -108,7 +108,7 @@ module Growstuff
     config.middleware.insert_before 0, Rack::Cors do
       allow do
          origins '*'
-         resource '*', headers: :any, methods: [:get, :post, :options]
+         resource '/api/v1/*', headers: :any, methods: [:get, :post, :options]
        end
     end
   end
