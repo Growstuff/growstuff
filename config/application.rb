32972a90fd8cffc5a5a8cbf5856ea0673fb80836
Brenda Wallace <brenda@brenda.nz>
2020-05-31 12:54:01 +1200

    Removed commented out code

--- config/application.rb
+++ config/application.rb
@@ -111,14 +111,5 @@ module Growstuff
          resource '*', headers: :any, methods: [:get, :post, :options]
        end
     end
-    # config.middleware.insert_before 0, "Rack::Cors" do
-    #   allow do
-    #     # In development, we don't care about the origin.
-    #     origins '*'
-    #     # Reminder: On the following line, the 'methods' refer to the 'Access-
-    #     # Control-Request-Method', not the normal Request Method.
-    #     resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put, :patch], credentials: true
-    #   end
-    # end
   end
 end
