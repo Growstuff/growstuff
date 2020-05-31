748209589456a4ea94e5211fd61eb79905269e40
Brenda Wallace <brenda@brenda.nz>
2020-05-31 12:03:04 +1200

    CORS headers for an open api

--- config/application.rb
+++ config/application.rb
@@ -105,5 +105,20 @@ module Growstuff
     config.cloudmade_key = '29a2d9e3cb3d429490a8f338b2388b1d'
 
     # config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+         origins '*'
+         resource '*', headers: :any, methods: [:get, :post, :options]
+       end
+    end
+    # config.middleware.insert_before 0, "Rack::Cors" do
+    #   allow do
+    #     # In development, we don't care about the origin.
+    #     origins '*'
+    #     # Reminder: On the following line, the 'methods' refer to the 'Access-
+    #     # Control-Request-Method', not the normal Request Method.
+    #     resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put, :patch], credentials: true
+    #   end
+    # end
   end
 end
