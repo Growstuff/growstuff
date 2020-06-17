be0c6759751ac742584a294661d2856276e18b5a
Brenda Wallace <brenda@brenda.nz>
2020-06-17 17:16:16 +1200

    Fix ruby syntax

--- script/check_contributors_md.rb
+++ script/check_contributors_md.rb
@@ -12,7 +12,7 @@ if ENV['TRAVIS'] || ENV['CI']
   if ENV['TRAVIS_PULL_REQUEST'] || ENV['CIRCLE_PULL_REQUEST']
     require 'httparty'
     repo = "#{ENV['TRAVIS_REPO_SLUG']}#{ENV['CIRCLE_PROJECT_REPONAME']}"
-    pr = "#{ENV['TRAVIS_PULL_REQUEST']}#{{ENV['CIRCLE_PR_NUMBER']}"
+    pr = "#{ENV['TRAVIS_PULL_REQUEST']}#{ENV['CIRCLE_PR_NUMBER']}"
     url = "https://api.github.com/repos/#{repo}/pulls/#{pr}"
     response = HTTParty.get(url).parsed_response
     author = response['user']['login'] if response && response['user']
