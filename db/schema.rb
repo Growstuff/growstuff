e4942a66461f211f32e8c0cadb4ed5c727c2ab12
Brenda Wallace <brenda@brenda.nz>
2020-08-15 13:26:49 +1200

    Upgrade to activemedian 0.2.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_210054) do
+ActiveRecord::Schema.define(version: 2020_08_15_012538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
