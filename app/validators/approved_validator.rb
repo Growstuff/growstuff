4985e1ead5ea5f3af732fb92137e470252f40d55
Daniel O'Connor <daniel.oconnor@gmail.com>
2022-03-27 01:33:54 +1030

    Fix warning

--- app/validators/approved_validator.rb
+++ app/validators/approved_validator.rb
@@ -2,6 +2,6 @@
 
 class ApprovedValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, _value)
-    record.errors[attribute] << (options[:message] || 'must be approved') unless record.crop.try(:approved?)
+    record.errors[attribute].add(options[:message] || 'must be approved') unless record.crop.try(:approved?)
   end
 end
