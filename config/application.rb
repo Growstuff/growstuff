89bb30011c91da55a799122a5e858ed020f72493
Daniel O'Connor <daniel.oconnor@gmail.com>
2022-05-01 17:10:05 +0930

    Fixes #3402 (#3043)

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Growstuff
 
     I18n.config.enforce_available_locales = true
 
+    config.active_record.legacy_connection_handling = false
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'UTC'
