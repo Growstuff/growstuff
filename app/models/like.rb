35b08f88f5e95e56012251870910ff25bf3559c3
Daniel O'Connor <daniel.oconnor@gmail.com>
2022-11-06 17:07:06 +1030

    Brakeman complaint

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,4 +5,6 @@ class Like < ApplicationRecord
   belongs_to :likeable, polymorphic: true, counter_cache: true, touch: true
   validates :member, :likeable, presence: true
   validates :member, uniqueness: { scope: :likeable }
+
+  attr_accessible :member_id, :likeable_type, :likeable_id, :categories
 end
