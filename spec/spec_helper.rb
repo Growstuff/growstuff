23d8f174c814415053c1885e6a73b249d4bc2692
Daniel O'Connor <daniel.oconnor@gmail.com>
2022-12-10 09:05:24 +1030

    Raise errors for deprecations, so we can track them down better

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,8 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  config.raise_errors_for_deprecations!
+
   def index_everything
     # reindex models
     Crop.reindex
