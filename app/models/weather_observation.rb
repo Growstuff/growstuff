a081a8d2f541d1a1e36aaccc3de3d2af77faf84b
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-01-07 01:39:58 +0000

    Fix warnings

--- app/models/weather_observation.rb
+++ app/models/weather_observation.rb
@@ -8,11 +8,15 @@ class WeatherObservation < ApplicationRecord
   validates :source, presence: true
   validates :observed_at, presence: true
 
+  attr_accessible :source, :observation_at, :solvar_uv_index, :dew_point_temperature_centigrade,
+                  :air_temperature_centigrade, :relative_humidity, :wind_speed_kmh, :wind_gust_speed_kmh, :garden_id, :wind_direction, :precipitation_probability, :pressure,
+                  :visibility_distance_metres, :weather_type
+
   # Lowest temp on earth: -89.2°C (-128.6°F)
   # Highest:  56.7°C
-  validates :dew_point_temperature_centigrade, numericality: { min: -90, max: 60 }
-  validates :air_temperature_centigrade, numericality: { min: -90, max: 60 }
-  validates :relative_humidity, numericality: { min: 0, max: 100 }
-  validates :wind_speed_kmh, numericality: { min: 0, max: 450 } # Highest 408 km/h
-  validates :wind_gust_speed_kmh, min: 0, max: 450 # Highest 408 km/h
+  validates :dew_point_temperature_centigrade, numericality: { min: -90, max: 60 }, allow_nil: true
+  validates :air_temperature_centigrade, numericality: { min: -90, max: 60 }, allow_nil: true
+  validates :relative_humidity, numericality: { min: 0, max: 100 }, allow_nil: true
+  validates :wind_speed_kmh, numericality: { min: 0, max: 450 }, allow_nil: true # Highest 408 km/h
+  validates :wind_gust_speed_kmh, min: 0, max: 450, allow_nil: true # Highest 408 km/h
 end
