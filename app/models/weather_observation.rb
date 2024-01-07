775986d1dd6524b1f1108da90837ace6398ab27c
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-01-07 01:33:22 +0000

    Fix validators

--- app/models/weather_observation.rb
+++ app/models/weather_observation.rb
@@ -10,9 +10,9 @@ class WeatherObservation < ApplicationRecord
 
   # Lowest temp on earth: -89.2°C (-128.6°F)
   # Highest:  56.7°C
-  validates :dew_point_temperature_centigrade, min: -90, max: 60
-  validates :air_temperature_centigrade, min: -90, max: 60
-  validates :relative_humidity, min: 0, max: 100
-  validates :wind_speed_kmh, min: 0, max: 450 # Highest 408 km/h
+  validates :dew_point_temperature_centigrade, numericality: { min: -90, max: 60 }
+  validates :air_temperature_centigrade, numericality: { min: -90, max: 60 }
+  validates :relative_humidity, numericality: { min: 0, max: 100 }
+  validates :wind_speed_kmh, numericality: { min: 0, max: 450 } # Highest 408 km/h
   validates :wind_gust_speed_kmh, min: 0, max: 450 # Highest 408 km/h
 end
