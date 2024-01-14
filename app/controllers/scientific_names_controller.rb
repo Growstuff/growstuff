1aba3935a0f773dbcd3ef9c6e67bd8d07825c9cd
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-01-14 04:46:22 +0000

    Add autocomplete for GBIF lookup

--- app/controllers/scientific_names_controller.rb
+++ app/controllers/scientific_names_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ScientificNamesController < ApplicationController
-  before_action :authenticate_member!, except: %i(index show)
-  load_and_authorize_resource
+  before_action :authenticate_member!, except: %i(index show gbif_suggest)
+  load_and_authorize_resource except: [:gbif_suggest]
   respond_to :html, :json
   responders :flash
 
@@ -58,7 +58,88 @@ class ScientificNamesController < ApplicationController
 
   def gbif_suggest
     species = Gbif::Species
-    render json: species.name_backbone(name: params[:name])
+
+    # Query the GBIF name autocomplete and discover the scientific name.
+    # [
+    #   {
+    #   "key": 2932942,
+    #   "nameKey": 1970347,
+    #   "kingdom": "Plantae",
+    #   "phylum": "Tracheophyta",
+    #   "order": "Solanales",
+    #   "family": "Solanaceae",
+    #   "genus": "Capsicum",
+    #   "species": "Capsicum chinense",
+    #   "kingdomKey": 6,
+    #   "phylumKey": 7707728,
+    #   "classKey": 220,
+    #   "orderKey": 1176,
+    #   "familyKey": 7717,
+    #   "genusKey": 2932937,
+    #   "speciesKey": 2932942,
+    #   "parent": "Capsicum",
+    #   "parentKey": 2932937,
+    #   "nubKey": 2932942,
+    #   "scientificName": "Capsicum chinense Jacq.",
+    #   "canonicalName": "Capsicum chinense",
+    #   "rank": "SPECIES",
+    #   "status": "ACCEPTED",
+    #   "synonym": false,
+    #   "higherClassificationMap": {
+    #   "6": "Plantae",
+    #   "220": "Magnoliopsida",
+    #   "1176": "Solanales",
+    #   "7717": "Solanaceae",
+    #   "2932937": "Capsicum",
+    #   "7707728": "Tracheophyta"
+    #   },
+    #   "class": "Magnoliopsida"
+    #   },
+    #   {
+    #   "key": 12079498,
+    #   "nameKey": 81778754,
+    #   "kingdom": "Plantae",
+    #   "phylum": "Tracheophyta",
+    #   "order": "Solanales",
+    #   "family": "Solanaceae",
+    #   "genus": "Capsicum",
+    #   "species": "Capsicum chinense",
+    #   "kingdomKey": 6,
+    #   "phylumKey": 7707728,
+    #   "classKey": 220,
+    #   "orderKey": 1176,
+    #   "familyKey": 7717,
+    #   "genusKey": 2932937,
+    #   "speciesKey": 2932942,
+    #   "parent": "Capsicum",
+    #   "parentKey": 2932937,
+    #   "nubKey": 12079498,
+    #   "scientificName": "Capsicum annuum var. chinense (Jacq.) Alef.",
+    #   "canonicalName": "Capsicum annuum chinense",
+    #   "rank": "VARIETY",
+    #   "status": "SYNONYM",
+    #   "synonym": true,
+    #   "higherClassificationMap": {
+    #   "6": "Plantae",
+    #   "220": "Magnoliopsida",
+    #   "1176": "Solanales",
+    #   "7717": "Solanaceae",
+    #   "2932937": "Capsicum",
+    #   "2932942": "Capsicum chinense",
+    #   "7707728": "Tracheophyta"
+    #   },
+    #   "class": "Magnoliopsida"
+    #   }
+    #   ]
+
+    data = species.name_suggest(q: params[:term]).map do |result|
+      result["name"] = result["canonicalName"]
+      result["id"] = result["nameKey"]
+
+      result
+    end
+
+    render json: data
   end
 
   private
