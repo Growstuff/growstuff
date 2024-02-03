b7c7bf62d07ef590b06c63458920c6cc0778bcb6
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-03 04:42:36 +0000

    Attempt to fix rendering

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,11 +2,17 @@
 
 module PostsHelper
   def display_post_truncated(post, length: 300)
-    truncate(strip_tags(post.body), length:,
+    truncate(strip_tags(as_haml(post)), length:,
                                     separator: ' ', omission: '... ') { link_to "Read more", post_path(post) }
   end
 
   def post_stripped_tags(post, length: 300)
-    truncate(strip_tags(post.body), length:)
+    truncate(strip_tags(as_haml(post)), length:)
+  end
+
+  def as_haml(post)
+    locals = {}
+    options = {}
+    Haml::Template.new(options) { strip_tags(post.body) }.render(Object.new, locals)
   end
 end
