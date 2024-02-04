3ff5c75c28da4566ee63f079a7930abfd4f93487
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-04 03:22:30 +0000

    Fix structure to avoid nested paragraph tags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,18 +1,7 @@
 # frozen_string_literal: true
 
 module PostsHelper
-  def display_post_truncated(post, length: 300)
-    truncate(strip_tags(as_haml(post)), length:,
-                                    separator: ' ', omission: '... ') { link_to "Read more", post_path(post) }
-  end
-
   def post_stripped_tags(post, length: 300)
-    truncate(strip_tags(as_haml(post)), length:)
-  end
-
-  def as_haml(post)
-    locals = {}
-    options = {}
-    Haml::Template.new(options) { strip_tags(markdownify(post.body)) }.render(Object.new, locals)
+    truncate(strip_tags(markdownify(post.body)), length:)
   end
 end
