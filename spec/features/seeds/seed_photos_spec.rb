d5c3ca06cc581613769da7fb2d1d5a5f3a9957d1
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-04 04:04:24 +0000

    Fix tests

--- spec/features/seeds/seed_photos_spec.rb
+++ spec/features/seeds/seed_photos_spec.rb
@@ -23,7 +23,10 @@ describe "Seeds", :js do
     #   it { is_expected.to have_content 'no photos' }
     # end
     context 'has one photo' do
-      before { seed.photos = [photo] }
+      before do
+        seed.photos = [photo]
+        visit seed_path(seed)
+      end
 
       let!(:photo) { FactoryBot.create(:photo, title: 'hello photo', owner: seed.owner) }
 
@@ -32,28 +35,30 @@ describe "Seeds", :js do
     end
 
     context 'has 50 photos' do
-      before { 
+      before do
         seed.photos = photos
 
-        visit seed_path(seed) 
-      }
+        visit seed_path(seed)
+      end
 
       let!(:photos) { FactoryBot.create_list(:photo, 10 * 5, owner: seed.owner) }
+      let(:newest_photo) { seed.photos.order(created_at: :desc, id: :desc).first }
+      let(:oldest_photo) { seed.photos.order(created_at: :desc, id: :desc).last }
 
       it "shows newest photo" do
-        expect(subject).to have_xpath("//img[contains(@src,'#{photos.last.thumbnail_url}')]")
+        expect(subject).to have_xpath("//img[contains(@src,'#{newest_photo.thumbnail_url}')]")
       end
 
       it "links to newest photo" do
-        expect(subject).to have_xpath("//a[contains(@href,'#{photo_path(photos.last)}')]")
+        expect(subject).to have_xpath("//a[contains(@href,'#{photo_path(newest_photo)}')]")
       end
 
       it "does not show oldest photo" do
-        expect(subject).to have_no_xpath("//img[contains(@src,'#{photos.first.thumbnail_url}')]")
+        expect(subject).to have_no_xpath("//img[contains(@src,'#{oldest_photo.thumbnail_url}')]")
       end
 
       it "does not link to oldest photo" do
-        expect(subject).to have_no_xpath("//a[contains(@href,'#{photo_path(photos.first)}')]")
+        expect(subject).to have_no_xpath("//a[contains(@href,'#{photo_path(oldest_photo)}')]")
       end
     end
   end
