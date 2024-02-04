2414f368b859bcd5b285644e38fbdbd34b6b405d
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-04 03:42:35 +0000

    Fix specs slightly

--- spec/features/seeds/seed_photos_spec.rb
+++ spec/features/seeds/seed_photos_spec.rb
@@ -13,7 +13,11 @@ describe "Seeds", :js do
     let(:member) { FactoryBot.create(:member)              }
     let!(:seed)  { FactoryBot.create(:seed, owner: member) }
 
-    it { is_expected.to have_content 'Add photo' }
+    it {
+      click_on "Actions"
+
+      expect(subject).to have_content 'Add photo'
+    }
 
     # context 'no photos' do
     #   it { is_expected.to have_content 'no photos' }
@@ -21,16 +25,20 @@ describe "Seeds", :js do
     context 'has one photo' do
       before { seed.photos = [photo] }
 
-      let!(:photo) { FactoryBot.create(:photo, title: 'hello photo') }
+      let!(:photo) { FactoryBot.create(:photo, title: 'hello photo', owner: seed.owner) }
 
       it { is_expected.to have_xpath("//img[contains(@src,'#{photo.thumbnail_url}')]") }
       it { is_expected.to have_xpath("//a[contains(@href,'#{photo_path(photo)}')]") }
     end
 
     context 'has 50 photos' do
-      before { seed.photos = photos }
+      before { 
+        seed.photos = photos
 
-      let!(:photos) { FactoryBot.create_list(:photo, 50) }
+        visit seed_path(seed) 
+      }
+
+      let!(:photos) { FactoryBot.create_list(:photo, 10 * 5, owner: seed.owner) }
 
       it "shows newest photo" do
         expect(subject).to have_xpath("//img[contains(@src,'#{photos.last.thumbnail_url}')]")
@@ -41,11 +49,11 @@ describe "Seeds", :js do
       end
 
       it "does not show oldest photo" do
-        expect(subject).not_to have_xpath("//img[contains(@src,'#{photos.first.thumbnail_url}')]")
+        expect(subject).to have_no_xpath("//img[contains(@src,'#{photos.first.thumbnail_url}')]")
       end
 
       it "does not link to oldest photo" do
-        expect(subject).not_to have_xpath("//a[contains(@href,'#{photo_path(photos.first)}')]")
+        expect(subject).to have_no_xpath("//a[contains(@href,'#{photo_path(photos.first)}')]")
       end
     end
   end
