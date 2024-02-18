e62e4815d57af857bdcdb31b9cba6a20cca60cd5
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-02-18 04:40:22 +0000

    Add factory

---
+++ spec/factories/activity.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Read about factories at https://github.com/thoughtbot/factory_bot
+
+FactoryBot.define do
+    factory :activity do
+      name { "Admire" }
+      description { "Spend 10 minutes admiring your hard work" }
+      category { "General" }
+      owner
+  
+      trait :garden do
+        category { "Soil Conditioning" }
+        description { "Apply compost from winter" }
+        due_date { 3.months.from_now }
+        garden
+      end
+
+      trait :planting do
+        category { "Pruning" }
+        description { "Stake tomato" }
+        planting
+      end
+    end
+  end
+  
