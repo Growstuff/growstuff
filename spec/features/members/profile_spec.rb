9c77fee451f43fb113cab67c598a3d5909f6bac8
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-04-07 10:27:33 +0000

    Update expectations

--- spec/features/members/profile_spec.rb
+++ spec/features/members/profile_spec.rb
@@ -55,10 +55,10 @@ describe "member profile", :js do
       it "with no activity" do
         visit member_path(member)
         expect(page).to have_content "Activity"
-        expect(page).to have_content "0 plantings"
-        expect(page).to have_content "0 harvests"
-        expect(page).to have_content "0 seeds"
-        expect(page).to have_content "0 posts"
+        expect(page).not_to have_content "0 plantings"
+        expect(page).not_to have_content "0 harvests"
+        expect(page).not_to have_content "0 seeds"
+        expect(page).not_to have_content "0 posts"
       end
 
       context "with some activity" do
