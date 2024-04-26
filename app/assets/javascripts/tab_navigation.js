88e458077b34127ebd8ebb3390df50d6ba301072
domthor <domthor@umich.edu>
2024-04-26 17:54:12 -0400

    fix unexpected token linting error

--- app/assets/javascripts/tab_navigation.js
+++ app/assets/javascripts/tab_navigation.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    let url = location.href.replace(/\/$/, "");
+    var url = location.href.replace(/\/$/, "");
 
     if (location.hash) {
         const hash = url.split("#");
