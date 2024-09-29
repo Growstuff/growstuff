b34c50fcf9fded10adac38e27428af9dcf60f547
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-29 06:59:15 +0000

    Add validations

--- app/models/garden_collaborator.rb
+++ app/models/garden_collaborator.rb
@@ -5,6 +5,14 @@ class GardenCollaborator < ApplicationRecord
   belongs_to :garden
 
   validates :member_id, uniqueness: { scope: :garden }
+  validate :not_garden_owner
+
+  def not_garden_owner
+    return unless member
+    return unless garden
+
+    errors.add(:member_id, "cannot be the garden owner") if garden.owner == member
+  end
 
   def member_slug
     @member&.slug
