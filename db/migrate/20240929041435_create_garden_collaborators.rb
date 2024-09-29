570c9f3a926f9818d07db7a9680d07f1aaf04bdf
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-29 05:28:07 +0000

    Update schema

--- db/migrate/20240929041435_create_garden_collaborators.rb
+++ db/migrate/20240929041435_create_garden_collaborators.rb
@@ -1,6 +1,8 @@
 class CreateGardenCollaborators < ActiveRecord::Migration[7.2]
   def change
     create_table :garden_collaborators do |t|
+      t.references :member
+      t.references :garden
       t.timestamps
     end
   end
