0a22e1ca8d886a23e1a7f4079581e39021afb893
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-09-29 05:09:10 +0000

    Typo

--- app/models/harvest.rb
+++ app/models/harvest.rb
@@ -153,7 +153,7 @@ class Harvest < ApplicationRecord
   def owner_must_match_planting
     return if planting.blank? # only check if we are linked to a planting
 
-    errors.add(:owner, "of harvest must be the same as planting, or a collaborator on that garden") unless owner == planting.owner || planting.garden.garden_collaborators.where(member:id: owner).any?
+    errors.add(:owner, "of harvest must be the same as planting, or a collaborator on that garden") unless owner == planting.owner || planting.garden.garden_collaborators.where(member_id: owner).any?
   end
 
   def harvest_must_be_after_planting
