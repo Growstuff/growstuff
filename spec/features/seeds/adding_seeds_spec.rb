7102552bfe3874c9bfa33b8b95ea57b5dbd13213
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-13 22:52:39 +1030

    Update spec/features/seeds/adding_seeds_spec.rb

--- spec/features/seeds/adding_seeds_spec.rb
+++ spec/features/seeds/adding_seeds_spec.rb
@@ -29,7 +29,7 @@ describe "Seeds", :js, :search do
 
     describe "Adding a new seed", :js do
       before do
-        fill_autocomplete "crop", with: "maiz"
+        fill_autocomplete "crop", with: "mai"
         select_from_autocomplete "maize"
         within "form#new_seed" do
           fill_in "Quantity", with: 42
