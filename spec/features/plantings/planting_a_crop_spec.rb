69e764dd63268b7281f0ace235a2b2967dc14d48
Daniel O'Connor <daniel.oconnor@gmail.com>
2024-10-13 09:01:29 +0000

    Fix specs

--- spec/features/plantings/planting_a_crop_spec.rb
+++ spec/features/plantings/planting_a_crop_spec.rb
@@ -62,9 +62,9 @@ describe "Planting a crop", :js, :search do
       before do
         visit new_planting_path
 
-        @a_past_date = 15.days.ago.strftime("%Y-%m-%d")
-        @right_now = Time.zone.today.strftime("%Y-%m-%d")
-        @a_future_date = 1.year.from_now.strftime("%Y-%m-%d")
+        @a_past_date = 15.days.ago
+        @right_now = Time.zone.today
+        @a_future_date = 1.year.from_now
       end
 
       it "shows that it is not planted yet" do
@@ -111,7 +111,7 @@ describe "Planting a crop", :js, :search do
           fill_in "How many?", with: 42
           select "cutting", from: "Planted from"
           select "semi-shade", from: "Sun or shade?"
-          fill_in "When?", with: '2013-03-10'
+          fill_in "When?", with: Time.new(2013, 3, 10)
           fill_in "Tell us more about it", with: "It's rad."
           fill_in "Finished date", with: @a_future_date
           click_button "Save"
