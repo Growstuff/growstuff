4bd322d8cacdde9f2deeddb811ceeb9d336ec107
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-08-10 02:55:34 +0000

    Target a desktop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,6 +22,7 @@ Capybara.register_driver :selenium_chrome_customised_headless do |app|
   options = Selenium::WebDriver::Options.chrome
   options.add_argument("--headless")
   options.add_argument("--no-sandbox")
+  options.add_argument("--window-size=1920,1080")
 
   # driver = Selenium::WebDriver.for :chrome, options: options
 
