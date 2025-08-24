5e77344b1d899ee99fb6a3f9f6ef851c5c079a3b
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-08-24 10:42:05 +0000

    Fix model tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,7 +7,7 @@ describe Comment do
     let(:comment) { FactoryBot.create(:comment) }
 
     it "belongs to a post" do
-      comment.post.should be_an_instance_of Post
+      comment.commentable.should be_an_instance_of Post
     end
 
     it "belongs to an author" do
@@ -26,17 +26,17 @@ describe Comment do
       @c = FactoryBot.create(:comment)
       @n = Notification.first
       @n.sender.should eq @c.author
-      @n.recipient.should eq @c.post.author
+      @n.recipient.should eq @c.commentable.author
       @n.subject.should include 'commented on'
       @n.body.should eq @c.body
-      @n.post.should eq @c.post
+      @n.post.should eq @c.commentable
     end
 
     it "doesn't send notifications to yourself" do
       @m = FactoryBot.create(:member)
       @p = FactoryBot.create(:post, author: @m)
       expect do
-        FactoryBot.create(:comment, post: @p, author: @m)
+        FactoryBot.create(:comment, commentable: @p, author: @m)
       end.not_to change(Notification, :count)
     end
   end
@@ -45,8 +45,8 @@ describe Comment do
     before do
       @m = FactoryBot.create(:member)
       @p = FactoryBot.create(:post, author: @m)
-      @c1 = FactoryBot.create(:comment, post: @p, author: @m)
-      @c2 = FactoryBot.create(:comment, post: @p, author: @m)
+      @c1 = FactoryBot.create(:comment, commentable: @p, author: @m)
+      @c2 = FactoryBot.create(:comment, commentable: @p, author: @m)
     end
 
     it 'has a scope for ASC order for displaying on post page' do
