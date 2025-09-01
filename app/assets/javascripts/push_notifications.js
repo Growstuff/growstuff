5d133b0f581656a49222efec4b23b659e40bc852
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-09-01 22:36:03 +0930

    Update app/assets/javascripts/push_notifications.js

--- app/assets/javascripts/push_notifications.js
+++ app/assets/javascripts/push_notifications.js
@@ -30,7 +30,7 @@ function urlBase64ToUint8Array(base64String) {
   return outputArray;
 }
 
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener('load', () => {
   const pushButton = document.getElementById('enable-push-notifications');
   if (pushButton) {
     pushButton.addEventListener('click', () => {
