514d271638dad4260cb5151c7de9055cc85bf2ec
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-09-07 08:48:02 +0000

    Update rubocop

--- db/migrate/20240810160538_set_default_language_for_existing_alternate_names.rb
+++ db/migrate/20240810160538_set_default_language_for_existing_alternate_names.rb
@@ -2,10 +2,10 @@
 
 class SetDefaultLanguageForExistingAlternateNames < ActiveRecord::Migration[7.2]
   def up
-    AlternateName.update_all(language: 'en')
+    AlternateName.update_all(language: 'en') # rubocop:disable Rails/SkipsModelValidations
   end
 
   def down
-    AlternateName.update_all(language: nil)
+    AlternateName.update_all(language: nil) # rubocop:disable Rails/SkipsModelValidations
   end
 end
