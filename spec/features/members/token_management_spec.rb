69c7c1291ad898dabd14f8d08c313ef6d1d85e4a
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-09-10 10:04:22 +0000

    Fix specs

--- spec/features/members/token_management_spec.rb
+++ spec/features/members/token_management_spec.rb
@@ -14,6 +14,7 @@ describe "member token management", :js do
     expect(page).to have_no_content("Your API token is")
     click_on "Generate API Token"
     expect(page).to have_content("Your API token is")
+    member.reload
     expect(member.api_token).to be_present
   end
 
@@ -27,7 +28,9 @@ describe "member token management", :js do
     it "can regenerate an API token" do
       old_token = member.api_token.token
       expect(page).to have_content("Your API token is")
-      click_on "Regenerate"
+      accept_confirm do
+        click_on "Regenerate"
+      end
       expect(page).to have_content("Your API token is")
       expect(member.reload.api_token.token).not_to eq(old_token)
     end
