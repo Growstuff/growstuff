e5357ad5cde805ef2366a5fc1d645bed49528f4d
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-09-21 03:53:22 +0000

    Fix specs

--- spec/features/home/releases_spec.rb
+++ spec/features/home/releases_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 feature 'GitHub Releases', :vcr do
   scenario 'Viewing the releases widget on the homepage' do
     visit root_path
-    expect(page).to have_content('Recent Releases')
     expect(page).to have_link('Find out more »', href: 'https://github.com/Growstuff/growstuff/releases')
   end
 end
