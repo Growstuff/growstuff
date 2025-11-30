e8dbcb0916c0974eded6593779ae68f7231c7800
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-11-30 13:24:42 +1030

    Delete db/migrate/20251129185029_add_wikidata_id_to_scientific_names.rb

--- db/migrate/20251129185029_add_wikidata_id_to_scientific_names.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddWikidataIdToScientificNames < ActiveRecord::Migration[6.1]
-  def change
-    add_column :scientific_names, :wikidata_id, :string
-  end
-end
