561aa6cd75e6cab8c391f6838aa2c4e0a5dce7a8
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-12-13 03:01:01 +0000

    Mark tests skipped

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,7 +51,7 @@ Capybara.server_port = 8081
 
 # TODO: Find a better home.
 shared_examples 'is accessible' do
-  it "is accessible" do
+  it "is accessible", pending: 'not compatible with playwright' do
     expect(page).to be_axe_clean.skipping('color-contrast', 'heading-order', 'aria-required-children').according_to :wcag2a
   end
 end
