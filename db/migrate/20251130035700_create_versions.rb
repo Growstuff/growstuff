8a70156b619498f213369c64f642de8940b098b6
Daniel O'Connor <daniel.oconnor@gmail.com>
2026-04-23 12:07:56 +0000

    Rubocop: Layout/EmptyLinesAroundClassBody

--- db/migrate/20251130035700_create_versions.rb
+++ db/migrate/20251130035700_create_versions.rb
@@ -1,7 +1,6 @@
 # This migration creates the `versions` table for the Version class.
 # All other migrations PT provides are optional.
 class CreateVersions < ActiveRecord::Migration[7.2]
-
   # The largest text column available in all supported RDBMS is
   # 1024^3 - 1 bytes, roughly one gibibyte.  We specify a size
   # so that MySQL will use `longtext` instead of `text`.  Otherwise,
