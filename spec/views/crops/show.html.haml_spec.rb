91c88c09ce14d4abef88eb1a065a10c515516e23
Daniel O'Connor <daniel.oconnor@gmail.com>
2026-04-23 12:51:05 +0000

    Rubocop: FactoryBot/SyntaxMethods

--- spec/views/crops/show.html.haml_spec.rb
+++ spec/views/crops/show.html.haml_spec.rb
@@ -4,13 +4,13 @@ require 'rails_helper'
 
 describe "crops/show" do
   before do
-    @crop = FactoryBot.create(:maize)
+    @crop = create(:maize)
     @posts = []
     assign(:crop, @crop)
-    @member = FactoryBot.create(:crop_wrangling_member)
+    @member = create(:crop_wrangling_member)
     sign_in @member
     @current_member = @member
-    @harvest = FactoryBot.create(:harvest, owner: @member)
+    @harvest = create(:harvest, owner: @member)
     controller.stub(:current_user) { @member }
     assign(:version_members, {})
   end
@@ -21,7 +21,7 @@ describe "crops/show" do
   end
 
   it "has sunniness block if sunny planting" do
-    FactoryBot.create(:sunny_planting, crop: @crop)
+    create(:sunny_planting, crop: @crop)
     render
     expect(rendered).to have_content "Sunniness"
   end
@@ -32,7 +32,7 @@ describe "crops/show" do
   end
 
   it "has planted from block if seed planting" do
-    FactoryBot.create(:seed_planting, crop: @crop)
+    create(:seed_planting, crop: @crop)
     render
     expect(rendered).to have_content "Planted from"
   end
