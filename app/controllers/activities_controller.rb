c0e1011fb51cb538ac44fd19cada0ad4179e0477
Daniel O'Connor <daniel.oconnor@gmail.com>
2026-04-28 08:20:25 +0000

    Less eager loading

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,12 +4,12 @@ class ActivitiesController < DataController
   def index
     @show_all = params[:all] == '1'
 
-    @activities = Activity.includes(:owner, :garden, planting: :crop).order(created_at: :desc)
+    @activities = Activity.includes(:owner).order(created_at: :desc)
     @activities = @activities.active unless @show_all
 
     if params[:member_slug].present?
       @owner = Member.find_by!(slug: params[:member_slug])
-      where['owner_id'] = @owner.id
+      @activities = @activities.where(owner_id: @owner.id)
     end
 
     @activities = @activities.paginate(page: params[:page], per_page: 30)
