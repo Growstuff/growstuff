555d5ddf15fa3440991cda3dff3359ce96e44c99
Daniel O'Connor <daniel.oconnor@gmail.com>
2026-05-04 18:15:17 +0930

    Revert "Replace Sidekiq with Solid Queue (#4619)" (#4620)
    This reverts commit 4659ac5464f74d8b8f8bd3af4c188e5cac1cc97f.

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# config/initializers/sidekiq.rb
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://localhost:6379/0' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://localhost:6379/0' }
+end
