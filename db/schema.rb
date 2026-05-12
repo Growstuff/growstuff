0365bed42fc0c0d1da8b17b7913667eb6d82d53f
Daniel O'Connor <daniel.oconnor@gmail.com>
2026-05-12 08:08:06 +0000

    Fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_11_035341) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_29_132911) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
